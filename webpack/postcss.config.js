a8ac46bfe913055f7efb0014a09b086d8d174a3a
ctestama <ctestama@asu.edu>
2018-08-28 16:39:46 -0700

    updated to cssnext-preset-env stage 2 and fixed nesting

--- webpack/postcss.config.js
+++ webpack/postcss.config.js
@@ -1,13 +1,17 @@
-
 const path = require('path');
+
 
 module.exports = {
     plugins: {
-        'postcss-cssnext': {},
         'postcss-import': {
             root: path.join(__dirname, '../'),
             path: [path.join(__dirname, '../src')]
         },
-        'postcss-nesting': {},
+        'postcss-preset-env': {
+            stage: 2,
+            features: {
+                'nesting-rules': true
+            }
+        }
     }
 }
