a31e691d951529657af2bad0139d38eb3f07824b
Nathan Rollins <nathan.rollins@asu.edu>
2019-07-09 10:23:17 -0700

    Merge pull request #4 from gios-asu/2-setup-build-tools
    Setup development and build tools

---
+++ packages/asu-web-standards-bootstrap4/.storybook/config.js
@@ -0,0 +1,39 @@
+import { configure, addDecorator, addParameters } from '@storybook/html';
+import { withA11y } from '@storybook/addon-a11y';
+import { addReadme } from 'storybook-readme/html';
+import 'happo-plugin-storybook/register';
+import centered from '@storybook/addon-centered/react';
+
+import WebFont from 'webfontloader';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '!style-loader!css-loader!sass-loader!./scss-loader.scss';
+
+WebFont.load({
+  google: {
+    families: ['Roboto:300,400,700', 'sans-serif']
+  }
+});
+
+addDecorator(withA11y)
+addDecorator(centered);
+
+addParameters({
+  options: {
+    showPanel: true,
+    panelPosition: 'right',
+    // theme: basicTheme,
+    // theme: themes.dark,
+  },
+  readme: {
+    codeTheme: 'github',
+  },
+});
+addDecorator(addReadme);
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../stories', true, /\.stories\.js$/);
+const loadStories = () => {
+  req.keys().forEach(req);
+}
+
+configure(loadStories, module);
