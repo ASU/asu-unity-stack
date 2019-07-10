f8dad5d3e4e0a9195552ada9bd5f83858f3c2531
Nathan Rollins <nathan.rollins@asu.edu>
2019-07-10 11:42:44 -0700

    Merge pull request #9 from gios-asu/nrollins-update-button-tests
    Update button tests

--- packages/asu-web-standards-bootstrap4/stories/buttons/buttons.stories.js
+++ packages/asu-web-standards-bootstrap4/stories/buttons/buttons.stories.js
@@ -10,6 +10,7 @@ storiesOf('Buttons', module)
       sidebar: ButtonsReadme,
     },
   })
+
   .add('Examples', () => `<button type="button" class="btn btn-primary">Primary</button>
 <button type="button" class="btn btn-secondary">Secondary</button>
 <button type="button" class="btn btn-success">Success</button>
@@ -19,11 +20,13 @@ storiesOf('Buttons', module)
 <button type="button" class="btn btn-light">Light</button>
 <button type="button" class="btn btn-dark">Dark</button>
 <button type="button" class="btn btn-link">Link</button>`)
+
   .add('Button tags', () => `<a class="btn btn-primary" href="#" role="button">Link</a>
 <button class="btn btn-primary" type="submit">Button</button>
 <input class="btn btn-primary" type="button" value="Input">
 <input class="btn btn-primary" type="submit" value="Submit">
 <input class="btn btn-primary" type="reset" value="Reset">`)
+
   .add('Outline buttons', () => `<button type="button" class="btn btn-outline-primary">Primary</button>
 <button type="button" class="btn btn-outline-secondary">Secondary</button>
 <button type="button" class="btn btn-outline-success">Success</button>
@@ -32,18 +35,23 @@ storiesOf('Buttons', module)
 <button type="button" class="btn btn-outline-info">Info</button>
 <button type="button" class="btn btn-outline-light">Light</button>
 <button type="button" class="btn btn-outline-dark">Dark</button>`)
+
   .add('Sizes', () => `<button type="button" class="btn btn-primary btn-lg">Large button</button>
 <button type="button" class="btn btn-secondary btn-lg">Large button</button><hr/><button type="button" class="btn btn-primary btn-sm">Small button</button>
 <button type="button" class="btn btn-secondary btn-sm">Small button</button><hr/><button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
 <button type="button" class="btn btn-secondary btn-lg btn-block">Block level button</button>`)
+
   .add('Active state', () => `<a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
 <a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>`)
+
   .add('Disabled state', () => `<button type="button" class="btn btn-lg btn-primary" disabled>Primary button</button>
 <button type="button" class="btn btn-secondary btn-lg" disabled>Button</button><hr/><a href="#" class="btn btn-primary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Primary link</a>
 <a href="#" class="btn btn-secondary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Link</a>`)
+
   .add('Button plugin - toggle state', () => `<button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
   Single toggle
 </button>`)
+
   .add('Checkbox and radio buttons', () => `<div class="btn-group-toggle" data-toggle="buttons">
   <label class="btn btn-secondary active">
     <input type="checkbox" checked autocomplete="off"> Checked
--- packages/asu-web-standards-bootstrap4/stories/buttons/buttons.test.js
+++ packages/asu-web-standards-bootstrap4/stories/buttons/buttons.test.js
@@ -1,128 +1,73 @@
 describe('Buttons', () => {
-  it('default button variant visually looks correct', async () => {
+  it('examples visually look correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--default');
+    await page.goto('http://localhost:9009/iframe.html?id=buttons--examples');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('primary button variant visually looks correct', async () => {
+  it('button tags visually look correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--primary');
+    await page.goto('http://localhost:9009/iframe.html?id=buttons--button-tags');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('secondary button variant visually looks correct', async () => {
+  it('outline buttons visually look correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--secondary');
+    await page.goto('http://localhost:9009/iframe.html?id=buttons--outline-buttons');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('gold button variant visually looks correct', async () => {
+  it('button sizes visually look correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--gold');
+    await page.goto('http://localhost:9009/iframe.html?id=buttons--sizes');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('blue button variant visually looks correct', async () => {
+  it('active button state visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--blue');
+    await page.goto('http://localhost:9009/iframe.html?id=buttons--active-state');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('success button variant visually looks correct', async () => {
+  it('disabled button state visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--success');
+    await page.goto('http://localhost:9009/iframe.html?id=buttons--disabled-state');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('info button variant visually looks correct', async () => {
+  it('button plugin for toggle state visually looks correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--info');
+    await page.goto('http://localhost:9009/iframe.html?id=buttons--button-plugin-toggle-state');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
 
-  it('warning button variant visually looks correct', async () => {
+  it('checkbox and radio buttons visually look correct', async () => {
     // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--warning');
+    await page.goto('http://localhost:9009/iframe.html?id=buttons--checkbox-and-radio-buttons');
     const image = await page.screenshot();
 
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
-
-  it('danger button variant visually looks correct', async () => {
-    // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--danger');
-    const image = await page.screenshot();
-
-    // API from jest-image-snapshot
-    expect(image).toMatchImageSnapshot();
-  });
-
-  it('link button variant visually looks correct', async () => {
-    // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-alternatives--link');
-    const image = await page.screenshot();
-
-    // API from jest-image-snapshot
-    expect(image).toMatchImageSnapshot();
-  });
-
-  it('button block variant visually looks correct', async () => {
-    // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-button-blocks--default');
-    const image = await page.screenshot();
-
-    // API from jest-image-snapshot
-    expect(image).toMatchImageSnapshot();
-  });
-
-  it('large button variant visually looks correct', async () => {
-    // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-button-sizes--large');
-    const image = await page.screenshot();
-
-    // API from jest-image-snapshot
-    expect(image).toMatchImageSnapshot();
-  });
-
-  it('small button variant visually looks correct', async () => {
-    // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-button-sizes--small');
-    const image = await page.screenshot();
-
-    // API from jest-image-snapshot
-    expect(image).toMatchImageSnapshot();
-  });
-
-  it('xsmall button variant visually looks correct', async () => {
-    // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=buttons-button-sizes--xsmall');
-    const image = await page.screenshot();
-
-    // API from jest-image-snapshot
-    expect(image).toMatchImageSnapshot();
-  });
-
 });
