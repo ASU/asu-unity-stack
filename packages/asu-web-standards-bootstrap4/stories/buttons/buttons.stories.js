5d8b96e35d64e319753d5fb35e1176495a6a731e
Nathan Rollins <nathan.rollins@asu.edu>
2019-07-10 10:46:12 -0700

    Merge pull request #8 from gios-asu/nrollins-update-button
    Update Button component folder with Bootstrap 4 docs and stories

--- packages/asu-web-standards-bootstrap4/stories/buttons/buttons.stories.js
+++ packages/asu-web-standards-bootstrap4/stories/buttons/buttons.stories.js
@@ -10,33 +10,52 @@ storiesOf('Buttons', module)
       sidebar: ButtonsReadme,
     },
   })
-  .add('Types', () => `<div>
-  <a class="btn btn-default" href="#" role="button">Link</a>
-  <button class="btn btn-default" type="submit">Button</button>
-  <input class="btn btn-default" type="button" value="Input">
-  <input class="btn btn-default" type="submit" value="Submit">
+  .add('Examples', () => `<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+<button type="button" class="btn btn-link">Link</button>`)
+  .add('Button tags', () => `<a class="btn btn-primary" href="#" role="button">Link</a>
+<button class="btn btn-primary" type="submit">Button</button>
+<input class="btn btn-primary" type="button" value="Input">
+<input class="btn btn-primary" type="submit" value="Submit">
+<input class="btn btn-primary" type="reset" value="Reset">`)
+  .add('Outline buttons', () => `<button type="button" class="btn btn-outline-primary">Primary</button>
+<button type="button" class="btn btn-outline-secondary">Secondary</button>
+<button type="button" class="btn btn-outline-success">Success</button>
+<button type="button" class="btn btn-outline-danger">Danger</button>
+<button type="button" class="btn btn-outline-warning">Warning</button>
+<button type="button" class="btn btn-outline-info">Info</button>
+<button type="button" class="btn btn-outline-light">Light</button>
+<button type="button" class="btn btn-outline-dark">Dark</button>`)
+  .add('Sizes', () => `<button type="button" class="btn btn-primary btn-lg">Large button</button>
+<button type="button" class="btn btn-secondary btn-lg">Large button</button><hr/><button type="button" class="btn btn-primary btn-sm">Small button</button>
+<button type="button" class="btn btn-secondary btn-sm">Small button</button><hr/><button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
+<button type="button" class="btn btn-secondary btn-lg btn-block">Block level button</button>`)
+  .add('Active state', () => `<a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Primary link</a>
+<a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>`)
+  .add('Disabled state', () => `<button type="button" class="btn btn-lg btn-primary" disabled>Primary button</button>
+<button type="button" class="btn btn-secondary btn-lg" disabled>Button</button><hr/><a href="#" class="btn btn-primary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Primary link</a>
+<a href="#" class="btn btn-secondary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Link</a>`)
+  .add('Button plugin - toggle state', () => `<button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off">
+  Single toggle
+</button>`)
+  .add('Checkbox and radio buttons', () => `<div class="btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-secondary active">
+    <input type="checkbox" checked autocomplete="off"> Checked
+  </label>
+</div><hr/><div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-secondary active">
+    <input type="radio" name="options" id="option1" autocomplete="off" checked> Active
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option2" autocomplete="off"> Radio
+  </label>
+  <label class="btn btn-secondary">
+    <input type="radio" name="options" id="option3" autocomplete="off"> Radio
+  </label>
 </div>`);
-
-storiesOf('Buttons/Alternatives', module)
-  .addParameters({ happo: false })
-  .add('default', () => `<button type="button" class="btn btn-default">btn-default</button>`)
-  .add('primary', () => `<button type="button" class="btn btn-primary">btn-primary</button>`)
-  .add('secondary', () => `<button type="button" class="btn btn-secondary">btn-secondary</button>`)
-  .add('gold', () => `<button type="button" class="btn btn-gold">btn-gold</button>`)
-  .add('blue', () => `<button type="button" class="btn btn-blue">btn-blue</button>`)
-  .add('success', () => `<button type="button" class="btn btn-success">btn-success</button>`)
-  .add('info', () => `<button type="button" class="btn btn-info">btn-info</button>`)
-  .add('warning', () => `<button type="button" class="btn btn-warning">btn-warning</button>`)
-  .add('danger', () => `<button type="button" class="btn btn-danger">btn-danger</button>`)
-  .add('link', () => `<button type="button" class="btn btn-link">btn-link</button>`);
-
-storiesOf('Buttons/Button Blocks', module)
-  .addParameters({ happo: false })
-  .add('default', () => `<button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>`);
-
-storiesOf('Buttons/Button Sizes', module)
-  .addParameters({ happo: false })
-  .add('Default', () => `<p><button type="button" class="btn btn-primary">Default Size Button</button><button type="button" class="btn btn-default">Default Button</button></p>`)
-  .add('Large', () => `<p><button type="button" class="btn btn-primary btn-lg">Large Button</button><button type="button" class="btn btn-default btn-lg">Large Button</button></p>`)
-  .add('Small', () => `<p><button type="button" class="btn btn-primary btn-sm">Small Button</button><button type="button" class="btn btn-default btn-sm">Small Button</button></p>`)
-  .add('XSmall', () => `<p><button type="button" class="btn btn-primary btn-xs">XS Button</button><button type="button" class="btn btn-default btn-xs">XS Button</button></p>`);
