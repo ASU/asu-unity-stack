2d076465d7a6b0866cfb26d898b1d23392c144e0
Nathan Rollins <nathan.rollins@asu.edu>
2019-07-19 09:35:59 -0700

    Merge pull request #31 from gios-asu/nrollins-disable-readme-plugin
    Remove Readme plugin from Storybook setup

--- packages/asu-web-standards-bootstrap4/.storybook/addons.js
+++ packages/asu-web-standards-bootstrap4/.storybook/addons.js
@@ -1,6 +1,5 @@
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-viewport/register';
-import 'storybook-readme/register';
 import '@storybook/addon-google-analytics/register';
 // TODO set env var: window.STORYBOOK_GA_ID = UA-000000-01
 
--- packages/asu-web-standards-bootstrap4/.storybook/config.js
+++ packages/asu-web-standards-bootstrap4/.storybook/config.js
@@ -1,6 +1,5 @@
 import { configure, addDecorator, addParameters } from '@storybook/html';
 import { withA11y } from '@storybook/addon-a11y';
-import { addReadme } from 'storybook-readme/html';
 import 'happo-plugin-storybook/register';
 
 import WebFont from 'webfontloader';
@@ -13,19 +12,6 @@ WebFont.load({
 });
 
 addDecorator(withA11y)
-
-addParameters({
-  options: {
-    showPanel: true,
-    panelPosition: 'bottom',
-    // theme: basicTheme,
-    // theme: themes.dark,
-  },
-  readme: {
-    codeTheme: 'github',
-  },
-});
-addDecorator(addReadme);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
