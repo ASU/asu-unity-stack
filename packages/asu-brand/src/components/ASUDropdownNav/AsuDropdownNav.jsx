3446369fec1775ceff3c6ce366cb502b9e66eb2a
Colton Testamarck <colt@asu.edu>
2019-09-13 12:41:05 -0700

    dropdown nav with useState and styles

--- packages/asu-brand/src/components/ASUDropdownNav/AsuDropdownNav.jsx
+++ packages/asu-brand/src/components/ASUDropdownNav/AsuDropdownNav.jsx
@@ -1,43 +1,64 @@
-import React, {useEffect, useState} from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./AsuDropdownNav.css";
+import classNames from "classnames";
 
-const AsuDropdownNav = (props) => {
+const AsuDropdownNav = props => {
+  const navClass = styles.asuNavItemTop;
+  const [open, setOpen] = useState(false);
+  const subStyles = classNames(
+    styles.subMenu,
+    open ? styles.subSlideIn : styles.subSlideOut
+  );
 
   let linkRefs = [];
 
-  for (let i=0; i < props.items.length; i++) {
+  for (let i = 0; i < props.items.length; i++) {
     let newRef = React.createRef();
     linkRefs.push(newRef);
   }
 
-  return(
-    <div>
-      <div role="navigation">
-        <button>{props.title}</button>
+  return (
+    <div role="navigation" className={navClass}>
 
-        <ul>
-          { props.items.map((item, index) => {
-              return (
-                <li key={index}>
-                  <a href={item.href} ref={linkRefs[index]} tabIndex="0">{item.value}</a>
-                </li>
-              );
-            })
-          }
+          <a className={styles.asuNavA} href={props.href} target={props.target}>
+            {props.title}
+          </a>
+          {props.items ? (
+            <span
+              className={open ? styles.icoSortUp : styles.icoSortDown}
+              onClick={() => {
+                setOpen(!open);
+              }}
+            ></span>
+          ) : (
+            ""
+          )}
+        <ul className={subStyles}>
+          {props.items.map((item, index) => {
+            return (
+              <li className={styles.asuNavItem} key={index}>
+                <a href={item.href} ref={linkRefs[index]} tabIndex="0">
+                  {item.value}
+                </a>
+              </li>
+            );
+          })}
         </ul>
-      </div>
-    </div>
-  )
-};
 
+    </div>
+  );
+};
 
 AsuDropdownNav.propTypes = {
   title: PropTypes.string,
-  items: PropTypes.arrayOf(PropTypes.object)
+  items: PropTypes.arrayOf(PropTypes.object),
+  href: PropTypes.string,
+  target: PropTypes.string
 };
 
 AsuDropdownNav.defaultProps = {
-  title: 'test'
+  title: "test"
 };
 
 export default AsuDropdownNav;
