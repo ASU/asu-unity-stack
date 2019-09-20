a29726ef98114e82c93f48ce9f82c513a0c295b4
Colton Testamarck <colt@asu.edu>
2019-09-20 14:48:56 -0700

    bootstrap applied modularly to reactstrap component

---
+++ packages/bs-react-carousel/src/components/BsReactCarousel/BsReactCarousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import { Test } from './BsReactCarousel.styles';
+import { UncontrolledCarousel, Util} from 'reactstrap';
+import bootstrap from '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+Util.setGlobalCssModule(bootstrap);
+
+const BsReactCarousel = (props) => (
+  <UncontrolledCarousel cssModule={bootstrap} items={props.items}/>
+);
+
+BsReactCarousel.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object)
+};
+
+BsReactCarousel.defaultProps = {
+};
+
+export default BsReactCarousel;
---
+++ packages/bs-react-carousel/webpack/webpack.production.js
@@ -0,0 +1,38 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const devWebpack = require('./webpack.development.js');
+
+module.exports = devWebpack;
+
+module.exports.push(merge(common, {
+    mode: 'production',
+    entry: {
+      'asu-brand': './index.js'
+    },
+    output: {
+      path: path.resolve(__dirname, '..', 'dist'),
+      filename: "[name].production.js",
+      libraryTarget: 'umd',
+      library: '',
+      umdNamedDefine: true,
+      globalObject: "typeof self !== 'undefined' ? self : this"
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].production.css",
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { modules: true, importLoaders: 1, localIdentName: '[name]__[local]___[hash:base64:5]'} }
+                ]
+            }
+        ]
+    }
+}));
