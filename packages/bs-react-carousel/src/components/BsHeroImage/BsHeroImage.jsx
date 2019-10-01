a51dbe37cc23c6a26b4253a3c47b3ea9cc1ee843
Colton Testamarck <colt@asu.edu>
2019-10-01 16:39:10 -0700

    added more carousel classes

--- packages/bs-react-carousel/src/components/BsHeroImage/BsHeroImage.jsx
+++ packages/bs-react-carousel/src/components/BsHeroImage/BsHeroImage.jsx
@@ -1,64 +1,71 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Jumbotron, Button, Util } from 'reactstrap';
-import bootstrap from '../../styles/bs4/css/bootstrap-asu.min.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { Jumbotron, Button, Util } from "reactstrap";
+import bootstrap from "../../styles/bs4/css/bootstrap-asu.min.css";
 
 // Needs to be set for CSS modules.
 // See https://github.com/reactstrap/reactstrap/issues/1049
 Util.setGlobalCssModule(bootstrap);
 
+const heroButtons = buttons => {
+  return buttons.map((item, index) => {
+    if (!item.text || !item.url) {
+      return "";
+    }
 
-const heroButtons = (buttons) => {
+    let bcolor = "primary";
 
+    if (item.color == "blue") {
+      bcolor = "secondary";
+    }
 
+    return (
+      <Button key={index} tag="a" href={item.url} color={bcolor}>
+        {item.text}
+      </Button>
+    );
+  });
+};
 
-
-    return buttons.map((item, index) => {
-
-      if (!item.text || !item.url) {
-        return '';
-      }
-
-      let bcolor = 'primary';
-
-      if (item.color == 'blue') {
-        bcolor = 'secondary';
-      }
-
-      return (<Button key={index} tag='a' href={item.url} color={bcolor}>{item.text}</Button>)
-    })
-}
-
-const BsHeroImage = (props) => {
-
+const BsHeroImage = props => {
   const bgStyles = {
-    color: 'white',
-    backgroundImage: 'url(' + props.src + ')',
-    backgroundPosition: 'center',
-    backgroundRepeat: 'norepeat',
-    backgroundSize: 'cover',
-    height: '100vh'
+    color: "white",
+    backgroundImage: "url(" + props.src + ")",
+    backgroundPosition: "center",
+    backgroundRepeat: "norepeat",
+    backgroundSize: "cover",
+    height: "100vh"
   };
 
-  const buttons = props.buttons ? heroButtons(props.buttons) : '';
+  const buttons = props.buttons ? heroButtons(props.buttons) : "";
+  let title = <h2 className={bootstrap["display-4"]}>{props.title}</h2>;
+
+  if (props.titleLink) {
+    title = <a href={props.titleLink}>{title}</a>
+  }
 
   return (
     <Jumbotron cssModule={bootstrap} fluid style={bgStyles}>
-      <img src={props.src} alt={props.altText} className={bootstrap['sr-only']}/>
+      <img
+        src={props.src}
+        alt={props.altText}
+        className={bootstrap["sr-only"]}
+      />
       <div className={bootstrap.container}>
-        <h2 className={bootstrap["display-4"]}>{props.title}</h2>
+        {title}
         <p>{props.tagline}</p>
         {buttons}
       </div>
     </Jumbotron>
-  )
+  );
 };
 
 BsHeroImage.propTypes = {
   src: PropTypes.string,
   altText: PropTypes.string,
   tagline: PropTypes.string,
-  title: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  titleLink: PropTypes.string,
   buttons: PropTypes.arrayOf(PropTypes.object)
 };
 
