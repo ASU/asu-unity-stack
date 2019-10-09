afd555c97ad8f0e8227ceb798bc5c8dcb3d23bce
Colton Testamarck <colt@asu.edu>
2019-10-09 16:11:37 -0700

    rename

--- packages/asu-brand/src/components/AsuDropdownNav/AsuDropdownNav.stories.js
+++ packages/asu-brand/src/components/AsuDropdownNav/AsuDropdownNav.stories.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from './node_modules/react';
+import { storiesOf } from './node_modules/@storybook/react';
 import AsuDropdownNav from './index.js';
 
 const items = [
