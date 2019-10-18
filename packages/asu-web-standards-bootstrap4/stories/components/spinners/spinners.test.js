d5d6912bed73f0bed5c3902620cba9bde83f5894
Nathan Rollins <nathan.rollins@asu.edu>
2019-10-18 16:59:31 -0700

    Drop extraneous jest tests in bootstrap4

--- packages/asu-web-standards-bootstrap4/stories/components/spinners/spinners.test.js
+++ packages/asu-web-standards-bootstrap4/stories/components/spinners/spinners.test.js
@@ -88,23 +88,4 @@ describe('Spinners', () => {
     // API from jest-image-snapshot
     expect(image).toMatchImageSnapshot();
   });
-
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
-    // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-spinners--nAMEOFCOMPONENT');
-    const image = await page.screenshot();
-
-    // API from jest-image-snapshot
-    expect(image).toMatchImageSnapshot();
-  });
-
-  it('nAMEOFCOMPONENT visually looks correct', async () => {
-    // APIs from jest-puppeteer
-    await page.goto('http://localhost:9009/iframe.html?id=components-spinners--nAMEOFCOMPONENT');
-    const image = await page.screenshot();
-
-    // API from jest-image-snapshot
-    expect(image).toMatchImageSnapshot();
-  });
-
 }); //close
