7f60e35837f4922146f06df30c27ea42252e9cc5
Colton Testamarck <colt@asu.edu>
2020-02-17 14:22:44 -0700

    puppeteer image test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   launch: {
     headless: process.env.HEADLESS !== 'false',
-    slowMo: (process.env.HEADLESS === 'false') ? 150 : false
+    slowMo: (process.env.HEADLESS === 'false') ? 150 : false,
+    args: ['--no-sandbox', '--disable-dev-shm-usage']
   }
 }
