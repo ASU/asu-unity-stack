8c9c155234578334875d229fb8536643653545a9
msingh45 <msingh45@asu.edu>
2020-04-29 07:38:22 -0700

    Refactored stories and added knobs

--- packages/asu-typography/src/components/ASUHeadings/ASUHeading.stories.js
+++ packages/asu-typography/src/components/ASUHeadings/ASUHeading.stories.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { withKnobs, boolean, text, number } from "@storybook/addon-knobs";
+import { withKnobs, select, text } from "@storybook/addon-knobs";
 import { withA11y } from "@storybook/addon-a11y";
 
 import { ASUHeadingOne, ASUHeadingTwo, ASUHeadingThree, ASUHeadingFour, ASUHeadingFive } from "./ASUHeading";
 
+const highlightColors = {
+  White: 'white',
+  Gold: 'gold',
+  Black: 'black'
+};
+
 storiesOf("ASU Typography", module)
   .add("ASU Headings", () => (
     <div>
-        <ASUHeadingOne>Heading 1 mobile</ASUHeadingOne>
-        <ASUHeadingTwo>ASU Heading Two</ASUHeadingTwo>
-        <ASUHeadingThree>ASU Heading Three</ASUHeadingThree>
-        <ASUHeadingFour>ASU Heading Four</ASUHeadingFour>
-        <ASUHeadingFive>ASU Heading Five</ASUHeadingFive>
+          <ASUHeadingOne>{text("h1", "Heading 1")}</ASUHeadingOne>
+          <ASUHeadingTwo>{text("h2", "Heading 2")}</ASUHeadingTwo>
+          <ASUHeadingThree>{text("h3", "Heading 3")}</ASUHeadingThree>
+          <ASUHeadingFour>{text("h4", "Heading 4")}</ASUHeadingFour>
+          <ASUHeadingFive>{text("h5", "Heading 5")}</ASUHeadingFive>
     </div>
   ))
   .add("ASU Headings - Highlighted", () => (
     <div>
-        <ASUHeadingOne highlight={"gold"}>Heading 1 mobile</ASUHeadingOne>
-        <ASUHeadingTwo highlight={"black"}>ASU Heading Two</ASUHeadingTwo>
-        <ASUHeadingThree highlight={"gold"}>ASU Heading Three</ASUHeadingThree>
-        <ASUHeadingFour highlight={"black"}>ASU Heading Four</ASUHeadingFour>
-        <ASUHeadingFive highlight={"gold"}>ASU Heading Five</ASUHeadingFive>
+        <ASUHeadingOne highlight={select("h1 Highlight", highlightColors, 'gold', "Heading 1")}>{text("h1", "Heading 1", "Heading 1")}</ASUHeadingOne>
+        <ASUHeadingTwo highlight={select("h2 Highlight", highlightColors, 'black', "Heading 2")}>{text("h2", "Heading 2", "Heading 2")}</ASUHeadingTwo>
+        <ASUHeadingThree highlight={select("h3 Highlight", highlightColors, 'gold', "Heading 3")}>{text("h3", "Heading 3", "Heading 3")}</ASUHeadingThree>
+        <ASUHeadingFour highlight={select("h4 Highlight", highlightColors, 'black', "Heading 4")}>{text("h4", "Heading 4", "Heading 4")}</ASUHeadingFour>
+        <ASUHeadingFive highlight={select("h5 Highlight", highlightColors, 'gold', "Heading 5")}>{text("h5", "Heading 5", "Heading 5")}</ASUHeadingFive>
     </div>
   ))
   .addDecorator(withKnobs, withA11y);
