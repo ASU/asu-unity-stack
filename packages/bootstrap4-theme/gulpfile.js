032179ad2fc3a3f6f92e624d4681b0b6fecc78ec
Nathan Rollins <nathan.rollins@asu.edu>
2020-05-24 14:15:21 -0700

    Format gulpfile js

--- packages/bootstrap4-theme/gulpfile.js
+++ packages/bootstrap4-theme/gulpfile.js
@@ -23,28 +23,29 @@ const paths = cfg.paths;
 // gulp sass
 // Compiles SCSS files into CSS
 gulp.task('sass', function() {
-	const stream = gulp
-		.src(paths.sass + '/bootstrap-asu.scss')
-		.pipe(
-			plumber({
-				errorHandler: function(err) {
-					console.log(err);
-					this.emit('end');
-				}
-			})
-		)
-		// .pipe(sourcemaps.init({ loadMaps: true }))
-		.pipe(sass({ outputStyle: 'expanded', errLogToConsole: true }))
-		// .pipe(postcss([autoprefixer()]))
-		// .pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(paths.css));
-	return stream;
+  const stream = gulp
+    .src(paths.sass + '/bootstrap-asu.scss')
+    .pipe(
+      plumber({
+        errorHandler: function(err) {
+          console.log(err);
+          this.emit('end');
+        },
+      })
+    )
+    // .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sass({ outputStyle: 'expanded', errLogToConsole: true }))
+    // .pipe(postcss([autoprefixer()]))
+    // .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.css));
+  return stream;
 });
 
 // Run:
 // gulp eslint
-gulp.task('eslint', function () {
-  return gulp.src(`${paths.js}/*.js`)
+gulp.task('eslint', function() {
+  return gulp
+    .src(`${paths.js}/*.js`)
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
@@ -54,100 +55,97 @@ gulp.task('eslint', function () {
 // gulp sasslint
 // Lint SCSS files
 gulp.task('sasslint', function() {
-	const stream = gulp
-		.src(paths.sass + '/**/*.scss')
-		.pipe(
-			plumber({
-				errorHandler: function(err) {
-					console.log(err);
-					this.emit('end');
-				}
-			})
-		)
+  const stream = gulp
+    .src(paths.sass + '/**/*.scss')
+    .pipe(
+      plumber({
+        errorHandler: function(err) {
+          console.log(err);
+          this.emit('end');
+        },
+      })
+    )
     .pipe(sassLint())
     .pipe(sassLint.format())
-    .pipe(sassLint.failOnError())
-	return stream;
+    .pipe(sassLint.failOnError());
+  return stream;
 });
 
 // Run:
 // gulp imagemin
 // Running image optimizing task
 gulp.task('imagemin', function() {
-	return gulp
-		.src(`${paths.imgsrc}/**`)
-		.pipe(imagemin())
-		.pipe(gulp.dest(paths.img));
+  return gulp
+    .src(`${paths.imgsrc}/**`)
+    .pipe(imagemin())
+    .pipe(gulp.dest(paths.img));
 });
 
 gulp.task('minifycss', function() {
-	return gulp
-		.src(`${paths.css}/bootstrap-asu.css`)
-		.pipe(sourcemaps.init({ loadMaps: true }))
-		.pipe(cleanCSS({ compatibility: '*' }))
-		.pipe(
-			plumber({
-				errorHandler: function(err) {
-					console.log(err);
-					this.emit('end');
-				}
-			})
-		)
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(paths.css));
+  return gulp
+    .src(`${paths.css}/bootstrap-asu.css`)
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(cleanCSS({ compatibility: '*' }))
+    .pipe(
+      plumber({
+        errorHandler: function(err) {
+          console.log(err);
+          this.emit('end');
+        },
+      })
+    )
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.css));
 });
 
 gulp.task('styles', function(callback) {
-	gulp.series('sass', 'minifycss')(callback);
+  gulp.series('sass', 'minifycss')(callback);
 });
 
 // Run:
 // gulp scripts.
 // Uglifies and concat all JS files into one
 gulp.task('scripts', function() {
-	const scripts = [
-    'src/js/_license.js',
-    'node_modules/fastclick/lib/fastclick.js',
-    'src/js/_fastclick.js',
-    'node_modules/smooth-scroll/dist/js/smooth-scroll.js',
-    'src/js/_smoothscroll.js'
-	];
-	gulp
-		.src(scripts, { allowEmpty: true })
-		.pipe(sourcemaps.init({ loadMaps: true }))
-		.pipe(babel())
-		.pipe(concat('bootstrap-asu.min.js'))
-		.pipe(uglify())
-		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(paths.dist + '/js'));
+  const scripts = ['src/js/_license.js'];
+  gulp
+    .src(scripts, { allowEmpty: true })
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(babel())
+    .pipe(concat('bootstrap-asu.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.dist + '/js'));
 
-	return gulp
-		.src(scripts, { allowEmpty: true })
-		.pipe(babel())
-		.pipe(concat('bootstrap-asu.js'))
-		.pipe(gulp.dest(paths.dist + '/js'));
+  return gulp
+    .src(scripts, { allowEmpty: true })
+    .pipe(babel())
+    .pipe(concat('bootstrap-asu.js'))
+    .pipe(gulp.dest(paths.dist + '/js'));
 });
 
 // Copy all assets needed for ASU Web Standards
 gulp.task('copy-assets', function(done) {
-	// Copy Font Awesome Fonts
-	gulp
-		.src(`${paths.node}font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}`)
-		.pipe(gulp.dest('./dist/fonts'));
+  // Copy Font Awesome Fonts
+  gulp
+    .src(`${paths.node}font-awesome/fonts/**/*.{ttf,woff,woff2,eot,svg}`)
+    .pipe(gulp.dest('./dist/fonts'));
 
-	done();
+  done();
 });
 
 // Deleting any file inside the /dist folder
 gulp.task('clean-dist', function() {
-	return del([paths.dist + '/**']);
+  return del([paths.dist + '/**']);
 });
 
 // Run
 // gulp compile
 // Compiles the styles, scripts, and assets into the dist folder
-gulp.task('compile', gulp.series('styles', 'scripts', 'imagemin', 'copy-assets'));
+gulp.task(
+  'compile',
+  gulp.series('styles', 'scripts', 'imagemin', 'copy-assets')
+);
 
 // Run
 // gulp validate
