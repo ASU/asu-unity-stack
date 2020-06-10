63f27732c23ef7a0532f31c16b1b53a88cc6d10c
Steve Ryan <steve.ryan@asu.edu>
2020-06-10 15:26:06 -0700

    Updated list component to restrict styles to specific use cases.

--- packages/bootstrap4-theme/stories/components/list/list.stories.js
+++ packages/bootstrap4-theme/stories/components/list/list.stories.js
@@ -6,7 +6,7 @@ storiesOf('Components/List', module)
     happo: false,
   })
 
-.add('Unodered list', () => `
+.add('Unordered list', () => `
 <ul>
   <li>Lorem ipsum dolor sit amet</li>
   <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
@@ -15,12 +15,53 @@ storiesOf('Components/List', module)
 </ul>
 `)
 
-.add('Unodered list, darkmode', () => `
+.add('Unordered list, Maroon', () => `
+<ul class="maroon">
+  <li>Lorem ipsum dolor sit amet</li>
+  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
+  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
+</ul>
+`)
+
+.add('Unordered list, Smoke', () => `
+<ul class="smokemode">
+  <li>Lorem ipsum dolor sit amet</li>
+  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
+  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
+</ul>
+`)
+
+.add('Unordered list, Dark', () => `
 <ul class="darkmode">
   <li>Lorem ipsum dolor sit amet</li>
   <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
   <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
   <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
+</ul>
+`)
+
+.add('Unordered list, Multi-level', () => `
+<ul>
+  <li>Lorem ipsum dolor sit amet
+    <ul>
+      <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <ul>
+              <li>Lorem ipsum dolor sit amet
+              <ul>
+                <li>Lorem ipsum dolor sit amet</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </ul>
+            </li>
+          </ul>
+        </li>
+      <li>Lorem ipsum dolor sit amet</li>
+    </ul>
+  </li>
+  <li>Lorem ipsum dolor sit amet</li>
 </ul>
 `)
 
@@ -41,20 +82,55 @@ storiesOf('Components/List', module)
 </ol>
 `)
 
-.add('Ordered list, smokemode', () => `
+.add('Ordered list, Maroon', () => `
+<ol class="maroon">
+  <li>Lorem ipsum dolor sit amet</li>
+  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
+  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
+</ol>
+`)
+
+.add('Ordered list, Smoke', () => `
 <ol class="smokemode">
   <li>Lorem ipsum dolor sit amet</li>
   <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
   <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
   <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
+</ol>
+`)
+
+.add('Ordered list, Dark', () => `
+<ol class="darkmode">
   <li>Lorem ipsum dolor sit amet</li>
-  <li>Eiusmod tempor lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
+  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
+</ol>
+`)
+
+.add('Ordered List, Multi-Level', () => `
+<ol>
+  <li>Lorem ipsum dolor sit amet
+      <ol>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <ol>
+                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      <ol>
+                          <li>Lorem ipsum dolor sit amet
+                              <ol>
+                                  <li>Lorem ipsum dolor sit amet</li>
+                                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                              </ol>
+                          </li>
+                      </ol>
+                  </li>
+                  <li>Lorem ipsum dolor sit amet</li>
+              </ol>
+          </li>
+      </ol>
+  </li>
   <li>Lorem ipsum dolor sit amet</li>
-  <li>Adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Ipsum dolor sit amet</li>
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Dolor sit amet, consectetur adipiscing elit.</li>
 </ol>
 `)
 
@@ -67,23 +143,6 @@ storiesOf('Components/List', module)
 </ul>
 `)
 
-.add('Unodered list, maroon', () => `
-<ul class="maroon">
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-</ul>
-`)
-
-.add('Ordered list, maroon', () => `
-<ol class="maroon">
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-</ol>
-`)
 
 .add('Icon list, maroon', () => `
 <ul class="fa-ul maroon">
@@ -112,52 +171,7 @@ storiesOf('Components/List', module)
 </ul>
 `)
 
-.add('Multilevel unordered list', () => `
-<ul>
-  <li>Lorem ipsum dolor sit amet
-      <ul>
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              <ul>
-                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      <ul>
-                          <li>Lorem ipsum dolor sit amet
-                              <ul>
-                                  <li>Lorem ipsum dolor sit amet</li>
-                              </ul>
-                          </li>
-                      </ul>
-                  </li>
-              </ul>
-          </li>
-      </ul>
-  </li>
-</ul>
-`)
 
-.add('Multilevel ordered list', () => `
-<ol>
-  <li>Lorem ipsum dolor sit amet
-      <ol>
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              <ol>
-                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      <ol>
-                          <li>Lorem ipsum dolor sit amet
-                              <ol>
-                                  <li>Lorem ipsum dolor sit amet</li>
-                                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                              </ol>
-                          </li>
-                      </ol>
-                  </li>
-                  <li>Lorem ipsum dolor sit amet</li>
-              </ol>
-          </li>
-      </ol>
-  </li>
-  <li>Lorem ipsum dolor sit amet</li>
-</ol>
-`)
 
 // Should this be implemented as a list-group?
 .add('Step list', () => `
