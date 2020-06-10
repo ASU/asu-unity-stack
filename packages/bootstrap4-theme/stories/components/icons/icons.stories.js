f3bb3e24c3cfff10f9066be14dee5a173f83ee22
msingh45 <msingh45@asu.edu>
2020-06-10 15:24:52 -0700

    Changed icon design tokens and updated markup in stories

--- packages/bootstrap4-theme/stories/components/icons/icons.stories.js
+++ packages/bootstrap4-theme/stories/components/icons/icons.stories.js
@@ -10,36 +10,44 @@ storiesOf('Components/Icons', module)
 <div class="container-fluid">
   <div class="row">
     <div class="col col-sm-12 col-md-4 p-4">
-      <i class="fas fa-2x fa-home"></i>
+      <h4>fa-home</h4>
+      <i class="fas icon-base fa-home"></i>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
-      <i class="fas fa-2x fa-bars"></i>
+      <h4>fa-bars</h4>
+      <i class="fas icon-base fa-bars"></i>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
-      <i class="fas fa-2x fa-times"></i>
+      <h4>fa-times</h4>
+      <i class="fas icon-large fa-times"></i>
     </div>
   </div>
-    <div class="row">
-      <div class="col col-sm-12 col-md-4 p-4">
-        <i class="fas fa-2x fa-chevron-up"></i>
-        <i class="fas fa-2x fa-chevron-down"></i>
-        <i class="fas fa-2x fa-chevron-left"></i>
-        <i class="fas fa-2x fa-chevron-right"></i>
-      </div>
-      <div class="col col-sm-12 col-md-4 p-4">
-        <i class="fas fa-2x fa-arrow-right"></i>
-      </div>
-      <div class="col col-sm-12 col-md-4 p-4">
-        <i class="fas fa-2x fa-play"></i>
-        <i class="fas fa-2x fa-pause"></i>
-      </div>
+  <div class="row">
+    <div class="col col-sm-12 col-md-4 p-4">
+      <h4>fa-chevron</h4>
+      <i class="fas icon-small fa-chevron-up"></i>
+      <i class="fas icon-small fa-chevron-down"></i>
+      <i class="fas icon-small fa-chevron-left"></i>
+      <i class="fas icon-small fa-chevron-right"></i>
     </div>
-    <div class="row">
-      <div class="col col-sm-12 col-md-4 p-4">
-        <i class="fas fa-2x fa-search"></i>
-      </div>
-      <div class="col col-sm-12 col-md-4 p-4">
-        <i class="fas fa-2x fa-external-link-alt"></i>
-      </div>
+    <div class="col col-sm-12 col-md-4 p-4">
+      <h4>fa-arrow-right</h4>
+      <i class="fas icon-xl fa-arrow-right"></i>
     </div>
-  </div>`)
+    <div class="col col-sm-12 col-md-4 p-4">
+      <h4>fa-play   fa-pause</h4>
+      <i class="fas icon-xxl fa-play"></i>
+      <i class="fas icon-xxl fa-pause"></i>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col col-sm-12 col-md-4 p-4">
+      <h4>fa-search</h4>
+      <i class="fas icon-base fa-search"></i>
+    </div>
+    <div class="col col-sm-12 col-md-4 p-4">
+      <h4>fa-external-link-alt</h4>
+      <i class="fas icon-large fa-external-link-alt"></i>
+    </div>
+  </div>
+</div>`)
