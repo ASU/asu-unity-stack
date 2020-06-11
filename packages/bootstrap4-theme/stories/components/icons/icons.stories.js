fed3a7e32bf0fe2e5bf52e3f4f2aafeaf30c2ba5
msingh45 <msingh45@asu.edu>
2020-06-11 16:38:53 -0700

    Fixed accessibility issues within icon stories

--- packages/bootstrap4-theme/stories/components/icons/icons.stories.js
+++ packages/bootstrap4-theme/stories/components/icons/icons.stories.js
@@ -11,27 +11,27 @@ storiesOf('Components/Icons', module)
   <div class="row">
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>Email</em></h4>
-      <i class="fas icon-small fa-envelope"></i>
-      <i class="fas icon-base fa-envelope"></i>
-      <i class="fas icon-large fa-envelope"></i>
-      <i class="fas icon-xl fa-envelope"></i>
-      <i class="fas icon-xxl fa-envelope"></i>
+      <span class="fas icon-small fa-envelope" alt="Email Icon"></span>
+      <span class="fas icon-base fa-envelope" alt="Email Icon"></span>
+      <span class="fas icon-large fa-envelope" alt="Email Icon"></span>
+      <span class="fas icon-xl fa-envelope" alt="Email Icon"></span>
+      <span class="fas icon-xxl fa-envelope" alt="Email Icon"></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>Phone</em></h4>
-      <i class="fas icon-small fa-phone"></i>
-      <i class="fas icon-base fa-phone"></i>
-      <i class="fas icon-large fa-phone"></i>
-      <i class="fas icon-xl fa-phone"></i>
-      <i class="fas icon-xxl fa-phone"></i>
+      <span class="fas icon-small fa-phone" alt="Phone Icon"></span>
+      <span class="fas icon-base fa-phone" alt="Phone Icon"></span>
+      <span class="fas icon-large fa-phone" alt="Phone Icon"></span>
+      <span class="fas icon-xl fa-phone" alt="Phone Icon"></span>
+      <span class="fas icon-xxl fa-phone" alt="Phone Icon"></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>Address</em></h4>
-      <i class="fas icon-small fa-map-marker-alt"></i>
-      <i class="fas icon-base fa-map-marker-alt"></i>
-      <i class="fas icon-large fa-map-marker-alt"></i>
-      <i class="fas icon-xl fa-map-marker-alt"></i>
-      <i class="fas icon-xxl fa-map-marker-alt"></i>
+      <span class="fas icon-small fa-map-marker-alt" alt="Map Marker Icon"></span>
+      <span class="fas icon-base fa-map-marker-alt" alt="Map Marker Icon"></span>
+      <span class="fas icon-large fa-map-marker-alt" alt="Map Marker Icon"></span>
+      <span class="fas icon-xl fa-map-marker-alt" alt="Map Marker Icon"></span>
+      <span class="fas icon-xxl fa-map-marker-alt" alt="Map Marker Icon"></span>
     </div>
   </div>
 </div>`)
