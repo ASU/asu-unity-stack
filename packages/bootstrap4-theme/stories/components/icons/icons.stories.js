6fc08b7a4a7c70a0dac355ce1950d26e206e39af
msingh45 <msingh45@asu.edu>
2020-06-12 15:22:45 -0700

    Fixed accessibility issues

--- packages/bootstrap4-theme/stories/components/icons/icons.stories.js
+++ packages/bootstrap4-theme/stories/components/icons/icons.stories.js
@@ -11,27 +11,27 @@ storiesOf('Components/Icons', module)
   <div class="row">
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>Email</em></h4>
-      <span class="fas icon-small fa-envelope" alt="Email Icon"></span>
-      <span class="fas icon-base fa-envelope" alt="Email Icon"></span>
-      <span class="fas icon-large fa-envelope" alt="Email Icon"></span>
-      <span class="fas icon-xl fa-envelope" alt="Email Icon"></span>
-      <span class="fas icon-xxl fa-envelope" alt="Email Icon"></span>
+      <span class="fas icon-small fa-envelope" alt="Email Icon"><span class="sr-only">Email</span></span>
+      <span class="fas icon-base fa-envelope" alt="Email Icon"><span class="sr-only">Email</span></span>
+      <span class="fas icon-large fa-envelope" alt="Email Icon"><span class="sr-only">Email</span></span>
+      <span class="fas icon-xl fa-envelope" alt="Email Icon"><span class="sr-only">Email</span></span>
+      <span class="fas icon-xxl fa-envelope" alt="Email Icon"><span class="sr-only">Email</span></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>Phone</em></h4>
-      <span class="fas icon-small fa-phone" alt="Phone Icon"></span>
-      <span class="fas icon-base fa-phone" alt="Phone Icon"></span>
-      <span class="fas icon-large fa-phone" alt="Phone Icon"></span>
-      <span class="fas icon-xl fa-phone" alt="Phone Icon"></span>
-      <span class="fas icon-xxl fa-phone" alt="Phone Icon"></span>
+      <span class="fas icon-small fa-phone" alt="Phone Icon"><span class="sr-only">Phone</span></span>
+      <span class="fas icon-base fa-phone" alt="Phone Icon"><span class="sr-only">Phone</span></span>
+      <span class="fas icon-large fa-phone" alt="Phone Icon"><span class="sr-only">Phone</span></span>
+      <span class="fas icon-xl fa-phone" alt="Phone Icon"><span class="sr-only">Phone</span></span>
+      <span class="fas icon-xxl fa-phone" alt="Phone Icon"><span class="sr-only">Phone</span></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>Address</em></h4>
-      <span class="fas icon-small fa-map-marker-alt" alt="Map Marker Icon"></span>
-      <span class="fas icon-base fa-map-marker-alt" alt="Map Marker Icon"></span>
-      <span class="fas icon-large fa-map-marker-alt" alt="Map Marker Icon"></span>
-      <span class="fas icon-xl fa-map-marker-alt" alt="Map Marker Icon"></span>
-      <span class="fas icon-xxl fa-map-marker-alt" alt="Map Marker Icon"></span>
+      <span class="fas icon-small fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address</span></span>
+      <span class="fas icon-base fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address</span></span>
+      <span class="fas icon-large fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address</span></span>
+      <span class="fas icon-xl fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address</span></span>
+      <span class="fas icon-xxl fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address</span></span>
     </div>
   </div>
 </div>`)
