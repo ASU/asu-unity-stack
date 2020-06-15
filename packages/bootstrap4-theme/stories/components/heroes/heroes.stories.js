c247e3581676966e9a5798c93c85b7746f15aa89
Michael Samuelson <mlsamuel@asu.edu>
2020-06-15 15:13:12 -0700

    UDS-148 Update namespacing from hero to heroes. In progress css changes.

--- packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
+++ packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
@@ -1,15 +1,17 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
+import bkgPlaceholderImg from './Placeholder_Img_Large-5@2x.png'
 
-storiesOf('Components/Hero', module)
+
+storiesOf('Components/Heroes', module)
   .addParameters({
     happo: false,
   })
 
 .add('Hero, small', () => `
 <section class="uds-hero uds-hero-sm">
-  <img src="https://www.asu.edu/asuthemes/4.9/assets/arizona-state-university-logo.png" alt="" role="presentation" class="uds-hero-img">
-  <div class="container">
+  <img src=${bkgPlaceholderImg} alt="" role="presentation" class="uds-hero-img">
+  <div class="container uds-hero-container">
     <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
   </div>
 </section>
@@ -17,12 +19,11 @@ storiesOf('Components/Hero', module)
 
 .add('Hero, medium', () => `
 <section class="uds-hero uds-hero-md">
-  <img src="https://www.asu.edu/asuthemes/4.9/assets/arizona-state-university-logo.png" alt="" role="presentation" class="uds-hero-img">
-  <div class="container">
+  <img src=${bkgPlaceholderImg} alt="" role="presentation" class="uds-hero-img">
+  <div class="container uds-hero-container">
     <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
     <div class="uds-hero-text col-6">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <!-- TODO Is there a better call to action button? -->
       <button class="btn btn-gold" type="submit">Call to action button</button>
     </div>
   </div>
@@ -31,12 +32,11 @@ storiesOf('Components/Hero', module)
 
 .add('Hero, large', () => `
 <section class="uds-hero uds-hero-lg">
-  <img src="https://www.asu.edu/asuthemes/4.9/assets/arizona-state-university-logo.png" alt="" role="presentation" class="uds-hero-img">
-  <div class="container">
+  <img src=${bkgPlaceholderImg} alt="" role="presentation" class="uds-hero-img">
+  <div class="container uds-hero-container">
     <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
     <div class="uds-hero-text col-6">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <!-- TODO Is there a better call to action button? -->
       <button class="btn btn-gold" type="submit">Call to action button</button>
     </div>
   </div>
