3de04831925678bed907bb9cc3ce5b3908a5f239
msingh45 <msingh45@asu.edu>
2020-06-15 14:10:37 -0700

    Removed alt attributes in markup for a11y

--- packages/bootstrap4-theme/stories/components/icons/icons.stories.js
+++ packages/bootstrap4-theme/stories/components/icons/icons.stories.js
@@ -11,27 +11,27 @@ storiesOf('Components/Icons', module)
   <div class="row">
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>Email Icon</em></h4>
-      <span class="fas icon-small fa-envelope" alt="Email Icon"><span class="sr-only">Email Icon</span></span>
-      <span class="fas icon-base fa-envelope" alt="Email Icon"><span class="sr-only">Email Icon</span></span>
-      <span class="fas icon-large fa-envelope" alt="Email Icon"><span class="sr-only">Email Icon</span></span>
-      <span class="fas icon-xl fa-envelope" alt="Email Icon"><span class="sr-only">Email Icon</span></span>
-      <span class="fas icon-xxl fa-envelope" alt="Email Icon"><span class="sr-only">Email Icon</span></span>
+      <span class="fas icon-small fa-envelope"><span class="sr-only">Email Icon</span></span>
+      <span class="fas icon-base fa-envelope"><span class="sr-only">Email Icon</span></span>
+      <span class="fas icon-large fa-envelope"><span class="sr-only">Email Icon</span></span>
+      <span class="fas icon-xl fa-envelope"><span class="sr-only">Email Icon</span></span>
+      <span class="fas icon-xxl fa-envelope"><span class="sr-only">Email Icon</span></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>Phone Icon</em></h4>
-      <span class="fas icon-small fa-phone" alt="Phone Icon"><span class="sr-only">Phone Icon</span></span>
-      <span class="fas icon-base fa-phone" alt="Phone Icon"><span class="sr-only">Phone Icon</span></span>
-      <span class="fas icon-large fa-phone" alt="Phone Icon"><span class="sr-only">Phone Icon</span></span>
-      <span class="fas icon-xl fa-phone" alt="Phone Icon"><span class="sr-only">Phone Icon</span></span>
-      <span class="fas icon-xxl fa-phone" alt="Phone Icon"><span class="sr-only">Phone Icon</span></span>
+      <span class="fas icon-small fa-phone"><span class="sr-only">Phone Icon</span></span>
+      <span class="fas icon-base fa-phone"><span class="sr-only">Phone Icon</span></span>
+      <span class="fas icon-large fa-phone"><span class="sr-only">Phone Icon</span></span>
+      <span class="fas icon-xl fa-phone"><span class="sr-only">Phone Icon</span></span>
+      <span class="fas icon-xxl fa-phone"><span class="sr-only">Phone Icon</span></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>Address Icon</em></h4>
-      <span class="fas icon-small fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address Icon</span></span>
-      <span class="fas icon-base fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address Icon</span></span>
-      <span class="fas icon-large fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address Icon</span></span>
-      <span class="fas icon-xl fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address Icon</span></span>
-      <span class="fas icon-xxl fa-map-marker-alt" alt="Map Marker Icon"><span class="sr-only">Address Icon</span></span>
+      <span class="fas icon-small fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
+      <span class="fas icon-base fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
+      <span class="fas icon-large fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
+      <span class="fas icon-xl fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
+      <span class="fas icon-xxl fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
     </div>
   </div>
 </div>`)
