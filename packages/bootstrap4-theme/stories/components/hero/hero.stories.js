3aae3aa65ee6bfc827b3515a7ff6e11e8376e041
Michael Samuelson <mlsamuel@asu.edu>
2020-06-15 12:58:12 -0700

    UDS-148 Further finishing up on SCSS for heroes.

--- packages/bootstrap4-theme/stories/components/hero/hero.stories.js
+++ packages/bootstrap4-theme/stories/components/hero/hero.stories.js
@@ -20,7 +20,7 @@ storiesOf('Components/Hero', module)
   <img src="https://www.asu.edu/asuthemes/4.9/assets/arizona-state-university-logo.png" alt="" role="presentation" class="uds-hero-img">
   <div class="container">
     <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
-    <div class="col-md-6">
+    <div class="uds-hero-text col-6">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <!-- TODO Is there a better call to action button? -->
       <button class="btn btn-gold" type="submit">Call to action button</button>
@@ -34,7 +34,7 @@ storiesOf('Components/Hero', module)
   <img src="https://www.asu.edu/asuthemes/4.9/assets/arizona-state-university-logo.png" alt="" role="presentation" class="uds-hero-img">
   <div class="container">
     <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
-    <div class="col-md-6">
+    <div class="uds-hero-text col-6">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <!-- TODO Is there a better call to action button? -->
       <button class="btn btn-gold" type="submit">Call to action button</button>
