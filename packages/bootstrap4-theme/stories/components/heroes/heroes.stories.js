6f0f1a6f3dfa51956412450788ff437fd267b59f
Michael Samuelson <mlsamuel@asu.edu>
2020-06-16 16:02:26 -0700

    UDS-148 Complete styling. Pending tokenizing and some cleanup.

--- packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
+++ packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
@@ -8,21 +8,22 @@ storiesOf('Components/Heroes', module)
     happo: false,
   })
 
+  // TODO Keep the Unsplash images, OR use url(${bkgPlaceholderImg})
+  // TODO if not using bkgPlaceholderImg, remove that file from component.
+
 .add('Hero, small', () => `
-<section class="uds-hero uds-hero-sm">
-  <img src=${bkgPlaceholderImg} alt="" role="presentation" class="uds-hero-img">
-  <div class="container uds-hero-container">
-    <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
+<section class="uds-hero uds-hero-sm" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://unsplash.it/1500?random');">
+  <div class="container container-center uds-hero-container">
+    <h1 class="heading heading-one col-8-md"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
   </div>
 </section>
 `)
 
 .add('Hero, medium', () => `
-<section class="uds-hero uds-hero-md">
-  <img src=${bkgPlaceholderImg} alt="" role="presentation" class="uds-hero-img">
-  <div class="container uds-hero-container">
-    <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
-    <div class="uds-hero-text col-6">
+<section class="uds-hero uds-hero-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://unsplash.it/1500?random');">
+  <div class="container container-center uds-hero-container">
+    <h1 class="heading heading-one col-8-md"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
+    <div class="uds-hero-text col-sm-12 col-md-7">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <button class="btn btn-gold" type="submit">Call to action button</button>
     </div>
@@ -31,11 +32,10 @@ storiesOf('Components/Heroes', module)
 `)
 
 .add('Hero, large', () => `
-<section class="uds-hero uds-hero-lg">
-  <img src=${bkgPlaceholderImg} alt="" role="presentation" class="uds-hero-img">
-  <div class="container uds-hero-container">
+<section class="uds-hero uds-hero-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://unsplash.it/1500?random');">
+  <div class="container container-center uds-hero-container">
     <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
-    <div class="uds-hero-text col-6">
+    <div class="uds-hero-text col-sm-12 col-md-7">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <button class="btn btn-gold" type="submit">Call to action button</button>
     </div>
