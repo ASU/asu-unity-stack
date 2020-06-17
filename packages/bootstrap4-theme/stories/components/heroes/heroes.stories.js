576fded6a3f42ae3879c7f801dd700dd95f32b49
Michael Samuelson <mlsamuel@asu.edu>
2020-06-17 16:25:18 -0700

    UDS-148 Add breakpoints tokens, tokenize Heroes.

--- packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
+++ packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
@@ -10,7 +10,7 @@ storiesOf('Components/Heroes', module)
 
 .add('Hero, small', () => `
 <section class="uds-hero uds-hero-sm" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="container container-center uds-hero-container">
+  <div class="container uds-hero-container">
     <h1 class="heading heading-one col-8-md"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
   </div>
 </section>
@@ -18,7 +18,7 @@ storiesOf('Components/Heroes', module)
 
 .add('Hero, medium', () => `
 <section class="uds-hero uds-hero-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="container container-center uds-hero-container">
+  <div class="container uds-hero-container">
     <h1 class="heading heading-one col-8-md"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
     <div class="uds-hero-text col-sm-12 col-md-7">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -30,7 +30,7 @@ storiesOf('Components/Heroes', module)
 
 .add('Hero, large', () => `
 <section class="uds-hero uds-hero-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="container container-center uds-hero-container">
+  <div class="container uds-hero-container">
     <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
     <div class="uds-hero-text col-sm-12 col-md-7">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
