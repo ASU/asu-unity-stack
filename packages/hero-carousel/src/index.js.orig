d00b659046c9d229d69621d4212db4f989042cae
Colton Testamarck <colt@asu.edu>
2020-06-18 16:34:01 -0700

    renamed packages

--- packages/hero-carousel/src/index.js.orig
+++
@@ -1,8 +0,0 @@
-import AsuCarousel from './components/AsuCarousel';
-import {AsuHeroImage, AsuBgHeroImage} from './components/AsuHeroImage';
-
-export {
-  AsuCarousel,
-  AsuHeroImage,
-  AsuBgHeroImage
-}
--- packages/react-library/webpack/webpack.common.js
+++ packages/react-library/webpack/webpack.common.js
@@ -5,10 +5,10 @@ const path = require("path");
 const packageInfo = require("../package.json");
 
 module.exports = {
+  context: path.join(__dirname, "..", "src"),
   entry: {
     [packageInfo.name]: "./index.js",
   },
-  context: path.join(__dirname, "..", "src"),
   module: {
     rules: [
       {
