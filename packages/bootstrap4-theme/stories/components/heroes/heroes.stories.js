cd34adc447613f1f126f865226cb3c76d23304c2
Michael Samuelson <mlsamuel@asu.edu>
2020-06-19 11:03:08 -0700

    UDS-148 Swap out section for div.

--- packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
+++ packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
@@ -9,15 +9,15 @@ storiesOf('Components/Heroes', module)
   })
 
 .add('Hero, small', () => `
-<section class="uds-hero uds-hero-sm" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+<div class="uds-hero uds-hero-sm" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
   <div class="container uds-hero-container">
     <h1 class="heading heading-one col-8-md"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
   </div>
-</section>
+</div>
 `)
 
 .add('Hero, medium', () => `
-<section class="uds-hero uds-hero-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+<div class="uds-hero uds-hero-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
   <div class="container uds-hero-container">
     <h1 class="heading heading-one col-8-md"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
     <div class="uds-hero-text col-sm-12 col-md-7">
@@ -25,11 +25,11 @@ storiesOf('Components/Heroes', module)
       <button class="btn btn-gold" type="submit">Call to action button</button>
     </div>
   </div>
-</section>
+</div>
 `)
 
 .add('Hero, large', () => `
-<section class="uds-hero uds-hero-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+<div class="uds-hero uds-hero-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
   <div class="container uds-hero-container">
     <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
     <div class="uds-hero-text col-sm-12 col-md-7">
@@ -37,7 +37,7 @@ storiesOf('Components/Heroes', module)
       <button class="btn btn-gold" type="submit">Call to action button</button>
     </div>
   </div>
-</section>
+</div>
 `)
 
 ; // close
