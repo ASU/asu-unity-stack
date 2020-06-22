4f6baa0d74aac1b356d2cfb03b53251a4e552571
Colton Testamarck <colt@asu.edu>
2020-06-22 11:35:07 -0700

    fix(components-library): test publish

--- packages/components-library/src/helpers.js
+++ packages/components-library/src/helpers.js
@@ -1,16 +1,13 @@
 /** @jsx h */
 /* eslint-disable react/prop-types */
-import { h, hydrate, render} from "preact";
-// test comment
+import { h, hydrate, render } from "preact";
 
 const HydratePreact = (component, props, target, children) => {
-
   return hydrate(h(component, props), target);
-}
+};
 
 const RenderPreact = (component, props, target, children) => {
-
   return render(h(component, props), target);
-}
+};
 
-export {HydratePreact, RenderPreact};
+export { HydratePreact, RenderPreact };
