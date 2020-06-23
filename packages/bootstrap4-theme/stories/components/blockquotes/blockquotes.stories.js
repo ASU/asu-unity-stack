c887de9a46a711b077ebec2169f507bd9a8ec12b
Bryan R <aubjr@asu.edu>
2020-06-23 12:04:25 -0700

    UDS-123 - Blockquotes framework added to Storybook (SASS, stories JS, etc.)

--- packages/bootstrap4-theme/stories/components/blockquotes/blockquotes.stories.js
+++ packages/bootstrap4-theme/stories/components/blockquotes/blockquotes.stories.js
@@ -1,136 +1,116 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 
-storiesOf('Components/List', module)
+storiesOf('Components/Blockquotes', module)
   .addParameters({
     happo: false,
   })
 
-.add('Unordered list', () => `
-<ul>
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-</ul>
-`)
+  .add('Blockquote (Default)', () => `
+    <blockquote>
+      <p>Blockquote default<br>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+      euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+      tation ullamcorper suscipit lobortis nisl ut <a href="some-link" title="Title!" class="class1">aliquip ex ea commodo
+      consequat</a>. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
+      dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+      delenit augue duis dolore te feugait nulla facilisi.</p>
+      <p class="ws2-sig-title">Signature</p>
+      <p class="ws2-sig-byline">Sig byline</p>
+    </blockquote>
+    <hr>
+    <blockquote class="ws2-element-maroon ws2-element-style">
+      <p>Blockquote maroon.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+      quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues.
+      It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
+      Angles, quam un skeptic Cambridge amico dit me que Occidental es. END TESTIMONIAL</p>
+      <p class="sig-title ws2-sig-title">Signature name</p>
+      <p class="sig-byline ws2-sig-byline">Signature byline</p>
+    </blockquote>
+    <hr>
+    <blockquote class="ws2-element-gold ws2-element-style">
+      <p>Blockquote Gold.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam
+      ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It
+      va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
+      Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
+    </blockquote>
+    <hr>
+    <blockquote class="ws2-bq ws2-bq-image ws2-element-gray ws2-element-style">
+      <p>
+        <img alt="one eighty alt" title="one eighty title" class="media-element file-default panopoly-image-original"
+        data-delta="1" src="/sites/default/files/styles/panopoly_image_original/public/180x180.png?itok=VfkmZBz7">
+        Blockquote Image.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+        quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan
+        lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un
+        simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
+      <p class="ws2-sig-title">Signature name</p>
+      <p class="ws2-sig-byline">Signature byline</p>
+    </blockquote>
+  `)
 
-.add('Unordered list, Maroon', () => `
-<ul class="maroon">
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-</ul>
-`)
+  .add('Blockquote (Alt Gray)', () => `
+    <blockquote class="ws2-element-alt ws2-element-alt-gray ws2-element-alt-no-padding">
+      <p>Blockquote default<br>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+      euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+      tation ullamcorper suscipit lobortis nisl ut <a href="some-link" title="Title!" class="class1">aliquip ex ea commodo
+      consequat</a>. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
+      dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+      delenit augue duis dolore te feugait nulla facilisi.</p>
+      <p class="ws2-sig-title">Signature</p>
+      <p class="ws2-sig-byline">Sig byline</p>
+    </blockquote>
+    <hr>
+      <blockquote class="ws2-element-maroon ws2-element-style ws2-element-alt ws2-element-alt-gray">
+      <p>Blockquote maroon.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+      quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues.
+      It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
+      Angles, quam un skeptic Cambridge amico dit me que Occidental es. END TESTIMONIAL</p>
+      <p class="sig-title ws2-sig-title">Signature name</p>
+      <p class="sig-byline ws2-sig-byline">Signature byline</p>
+    </blockquote>
+    <hr>
+    <blockquote class="ws2-bq ws2-bq-image ws2-element-style ws2-element-gray ws2-element-alt ws2-element-alt-gray">
+      <p>
+        <img alt="one eighty alt" title="one eighty title" class="media-element file-default panopoly-image-original"
+        data-delta="1" src="/sites/default/files/styles/panopoly_image_original/public/180x180.png?itok=VfkmZBz7">
+        Blockquote Image.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+        quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan
+        lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un
+        simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
+      <p class="ws2-sig-title">Signature name</p>
+      <p class="ws2-sig-byline">Signature byline</p>
+    </blockquote>
+  `)
 
-.add('Unordered list, Smoke', () => `
-<ul class="smokemode">
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-</ul>
-`)
+  .add('Blockquote (Alt Black)', () => `
+    <blockquote class="ws2-element-alt ws2-element-alt-black ws2-element-style ws2-element-white">
+      <p>Blockquote default<br>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+      euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
+      tation ullamcorper suscipit lobortis nisl ut <a href="some-link" title="Title!" class="class1">aliquip ex ea commodo
+      consequat</a>. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
+      dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
+      delenit augue duis dolore te feugait nulla facilisi.</p>
+      <p class="ws2-sig-title">Signature</p>
+      <p class="ws2-sig-byline">Sig byline</p>
+    </blockquote>
+    <hr>
+    <blockquote class="ws2-element-gold ws2-element-style ws2-element-alt ws2-element-alt-black">
+      <p>Blockquote Gold.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam
+      ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It
+      va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
+      Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
+    </blockquote>
+    <hr>
+    <blockquote class="ws2-bq ws2-bq-image ws2-element-style ws2-element-gray ws2-element-alt ws2-element-alt-black">
+      <p>
+        <img alt="one eighty alt" title="one eighty title" class="media-element file-default panopoly-image-original"
+        data-delta="1" src="/sites/default/files/styles/panopoly_image_original/public/180x180.png?itok=VfkmZBz7">
+        Blockquote Image.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+        quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan
+        lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un
+        simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
+      <p class="ws2-sig-title">Signature name</p>
+      <p class="ws2-sig-byline">Signature byline</p>
+    </blockquote>
+  `)
 
-.add('Unordered list, Darkmode', () => `
-<ul class="darkmode">
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-</ul>
-`)
-
-.add('Unordered list, Multi-level', () => `
-<ul>
-  <li>Lorem ipsum dolor sit amet
-    <ul>
-      <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        <ul>
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            <ul>
-              <li>Lorem ipsum dolor sit amet
-              <ul>
-                <li>Lorem ipsum dolor sit amet</li>
-                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </ul>
-            </li>
-          </ul>
-        </li>
-      <li>Lorem ipsum dolor sit amet</li>
-    </ul>
-  </li>
-  <li>Lorem ipsum dolor sit amet</li>
-</ul>
-`)
-
-.add('Ordered list', () => `
-<ol>
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Eiusmod tempor lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Ipsum dolor sit amet</li>
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Dolor sit amet, consectetur adipiscing elit.</li>
-</ol>
-`)
-
-.add('Ordered list, Maroon', () => `
-<ol class="maroon">
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-</ol>
-`)
-
-.add('Ordered list, Smoke', () => `
-<ol class="smokemode">
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-</ol>
-`)
-
-.add('Ordered list, Darkmode', () => `
-<ol class="darkmode">
-  <li>Lorem ipsum dolor sit amet</li>
-  <li>Consectetur adipiscing lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
-  <li>Ipsum dolor sit amet, consectetur adipiscing elit.</li>
-  <li>Sed do eiusmod tempor incididunt ut lorem ipsum dolor sit amet</li>
-</ol>
-`)
-
-.add('Ordered List, Multi-Level', () => `
-<ol>
-  <li>Lorem ipsum dolor sit amet
-      <ol>
-          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              <ol>
-                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      <ol>
-                          <li>Lorem ipsum dolor sit amet
-                              <ol>
-                                  <li>Lorem ipsum dolor sit amet</li>
-                                  <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                              </ol>
-                          </li>
-                      </ol>
-                  </li>
-                  <li>Lorem ipsum dolor sit amet</li>
-              </ol>
-          </li>
-      </ol>
-  </li>
-  <li>Lorem ipsum dolor sit amet</li>
-</ol>
-`)
 ; //close
