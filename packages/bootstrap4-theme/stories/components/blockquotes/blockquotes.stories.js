c2139d61064d1ed92b67958ceb8ffc4fcb2e2da7
Bryan R <aubjr@asu.edu>
2020-06-23 19:40:35 -0700

    UDS-123 - Added image for blockquote; Updated top/bottom spacing

--- packages/bootstrap4-theme/stories/components/blockquotes/blockquotes.stories.js
+++ packages/bootstrap4-theme/stories/components/blockquotes/blockquotes.stories.js
@@ -1,5 +1,6 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
+import blockquoteImage from './180x180.png';
 
 storiesOf('Components/Blockquotes', module)
   .addParameters({
@@ -7,7 +8,7 @@ storiesOf('Components/Blockquotes', module)
   })
 
   .add('Blockquote (Default)', () => `
-    <blockquote>
+    <blockquote class="ws2-element-spacing-entity">
       <p>Blockquote default<br>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
       euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
       tation ullamcorper suscipit lobortis nisl ut <a href="some-link" title="Title!" class="class1">aliquip ex ea commodo
@@ -17,8 +18,8 @@ storiesOf('Components/Blockquotes', module)
       <p class="ws2-sig-title">Signature</p>
       <p class="ws2-sig-byline">Sig byline</p>
     </blockquote>
-    <hr>
-    <blockquote class="ws2-element-maroon ws2-element-style">
+    <hr />
+    <blockquote class="ws2-element-maroon ws2-element-style ws2-element-spacing-entity">
       <p>Blockquote maroon.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
       quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues.
       It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
@@ -26,18 +27,18 @@ storiesOf('Components/Blockquotes', module)
       <p class="sig-title ws2-sig-title">Signature name</p>
       <p class="sig-byline ws2-sig-byline">Signature byline</p>
     </blockquote>
-    <hr>
-    <blockquote class="ws2-element-gold ws2-element-style">
+    <hr />
+    <blockquote class="ws2-element-gold ws2-element-style ws2-element-spacing-entity">
       <p>Blockquote Gold.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam
       ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It
       va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
       Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
     </blockquote>
-    <hr>
-    <blockquote class="ws2-bq ws2-bq-image ws2-element-gray ws2-element-style">
+    <hr />
+    <blockquote class="ws2-bq ws2-bq-image ws2-element-gray ws2-element-style ws2-element-spacing-entity">
       <p>
         <img alt="one eighty alt" title="one eighty title" class="media-element file-default panopoly-image-original"
-        data-delta="1" src="/sites/default/files/styles/panopoly_image_original/public/180x180.png?itok=VfkmZBz7">
+        data-delta="1" src="${blockquoteImage}">
         Blockquote Image.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
         quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan
         lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un
@@ -48,7 +49,7 @@ storiesOf('Components/Blockquotes', module)
   `)
 
   .add('Blockquote (Alt Gray)', () => `
-    <blockquote class="ws2-element-alt ws2-element-alt-gray ws2-element-alt-no-padding">
+    <blockquote class="ws2-element-alt ws2-element-alt-gray ws2-element-alt-no-padding ws2-element-spacing-entity-full">
       <p>Blockquote default<br>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
       euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
       tation ullamcorper suscipit lobortis nisl ut <a href="some-link" title="Title!" class="class1">aliquip ex ea commodo
@@ -58,8 +59,8 @@ storiesOf('Components/Blockquotes', module)
       <p class="ws2-sig-title">Signature</p>
       <p class="ws2-sig-byline">Sig byline</p>
     </blockquote>
-    <hr>
-      <blockquote class="ws2-element-maroon ws2-element-style ws2-element-alt ws2-element-alt-gray">
+    <hr />
+    <blockquote class="ws2-element-maroon ws2-element-style ws2-element-alt ws2-element-alt-gray ws2-element-spacing-entity-full">
       <p>Blockquote maroon.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
       quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues.
       It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
@@ -67,11 +68,11 @@ storiesOf('Components/Blockquotes', module)
       <p class="sig-title ws2-sig-title">Signature name</p>
       <p class="sig-byline ws2-sig-byline">Signature byline</p>
     </blockquote>
-    <hr>
-    <blockquote class="ws2-bq ws2-bq-image ws2-element-style ws2-element-gray ws2-element-alt ws2-element-alt-gray">
+    <hr />
+    <blockquote class="ws2-bq ws2-bq-image ws2-element-style ws2-element-gray ws2-element-alt ws2-element-alt-gray ws2-element-spacing-entity-full">
       <p>
         <img alt="one eighty alt" title="one eighty title" class="media-element file-default panopoly-image-original"
-        data-delta="1" src="/sites/default/files/styles/panopoly_image_original/public/180x180.png?itok=VfkmZBz7">
+        data-delta="1" src="${blockquoteImage}">
         Blockquote Image.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
         quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan
         lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un
@@ -82,7 +83,7 @@ storiesOf('Components/Blockquotes', module)
   `)
 
   .add('Blockquote (Alt Black)', () => `
-    <blockquote class="ws2-element-alt ws2-element-alt-black ws2-element-style ws2-element-white">
+    <blockquote class="ws2-element-alt ws2-element-alt-black ws2-element-style ws2-element-white ws2-element-spacing-inline">
       <p>Blockquote default<br>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
       euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
       tation ullamcorper suscipit lobortis nisl ut <a href="some-link" title="Title!" class="class1">aliquip ex ea commodo
@@ -92,18 +93,16 @@ storiesOf('Components/Blockquotes', module)
       <p class="ws2-sig-title">Signature</p>
       <p class="ws2-sig-byline">Sig byline</p>
     </blockquote>
-    <hr>
-    <blockquote class="ws2-element-gold ws2-element-style ws2-element-alt ws2-element-alt-black">
+    <blockquote class="ws2-element-gold ws2-element-style ws2-element-alt ws2-element-alt-black ws2-element-spacing-inline">
       <p>Blockquote Gold.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam
       ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It
       va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
       Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
     </blockquote>
-    <hr>
-    <blockquote class="ws2-bq ws2-bq-image ws2-element-style ws2-element-gray ws2-element-alt ws2-element-alt-black">
+    <blockquote class="ws2-bq ws2-bq-image ws2-element-style ws2-element-gray ws2-element-alt ws2-element-alt-black ws2-element-spacing-inline">
       <p>
         <img alt="one eighty alt" title="one eighty title" class="media-element file-default panopoly-image-original"
-        data-delta="1" src="/sites/default/files/styles/panopoly_image_original/public/180x180.png?itok=VfkmZBz7">
+        data-delta="1" src="${blockquoteImage}">
         Blockquote Image.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
         quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan
         lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un
