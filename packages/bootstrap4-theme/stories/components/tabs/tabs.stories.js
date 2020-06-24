d0c4fab18136aa56452430838f7ac9fc6f6bc2c6
tkilbour <tyk@asu.edu>
2020-06-24 13:35:31 -0700

    updated tabs

--- packages/bootstrap4-theme/stories/components/tabs/tabs.stories.js
+++ packages/bootstrap4-theme/stories/components/tabs/tabs.stories.js
@@ -6,7 +6,7 @@ storiesOf('Components/Tabs', module)
     happo: false,
   })
 
-  .add('Tabs', () => `
+  .add('Desktop', () => `
 <div class="container-fluid">
 <div class="row">
 
@@ -28,7 +28,32 @@ storiesOf('Components/Tabs', module)
 
 </div>
 </div>
-  `);
+  `)
+
+  .add('Mobile', () => `
+<div class="container-fluid">
+<div class="row">
+
+<div class="col-4">
+
+<nav class="mobile-tabs">
+          <div class="nav nav-tabs" id="nav-tab" role="tablist">
+            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+          </div>
+        </nav>
+        <div class="tab-content" id="nav-tabContent">
+                  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Home tab content...</div>
+                  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Profile tab content...</div>
+                  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">Contact tab content...</div>
+                </div>
+</div>
+
+</div>
+</div>
+  `)
+  ;
 /*
   .add('Nav element', () => `<nav class="nav">
   <a class="nav-link active" href="#">Active</a>
