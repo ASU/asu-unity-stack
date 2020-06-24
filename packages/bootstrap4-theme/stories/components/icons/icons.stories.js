8f1f8833df2108b0d4b60c2aefaf8279e155c436
Steve Ryan <steve.ryan@asu.edu>
2020-06-24 14:52:05 -0700

    Fixes output of icon stories in Storybook

--- packages/bootstrap4-theme/stories/components/icons/icons.stories.js
+++ packages/bootstrap4-theme/stories/components/icons/icons.stories.js
@@ -11,99 +11,99 @@ storiesOf('Components/Icons', module)
   <div class="row">
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>fa-home</em></h4>
-      <span class="fas icon-small fa-home"><span class="sr-only">Home Icon</span></span>
-      <span class="fas icon-base fa-home"><span class="sr-only">Home Icon</span></span>
-      <span class="fas icon-large fa-home"><span class="sr-only">Home Icon</span></span>
-      <span class="fas icon-xl fa-home"><span class="sr-only">Home Icon</span></span>
-      <span class="fas icon-xxl fa-home"><span class="sr-only">Home Icon</span></span>
+      <span class="fas icon-small fa-home" title="Home Icon"></span>
+      <span class="fas icon-base fa-home" title="Home Icon"></span>
+      <span class="fas icon-large fa-home" title="Home Icon"></span>
+      <span class="fas icon-xl fa-home" title="Home Icon"></span>
+      <span class="fas icon-xxl fa-home" title="Home Icon"></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>fa-bars</em></h4>
-      <span class="fas icon-small fa-bars"><span class="sr-only">Menu Icon</span></span>
-      <span class="fas icon-base fa-bars"><span class="sr-only">Menu Icon</span></span>
-      <span class="fas icon-large fa-bars"><span class="sr-only">Menu Icon</span></span>
-      <span class="fas icon-xl fa-bars"><span class="sr-only">Menu Icon</span></span>
-      <span class="fas icon-xxl fa-bars"><span class="sr-only">Menu Icon</span></span>
+      <span class="fas icon-small fa-bars" title="Menu Icon"></span>
+      <span class="fas icon-base fa-bars" title="Menu Icon"></span>
+      <span class="fas icon-large fa-bars" title="Menu Icon"></span>
+      <span class="fas icon-xl fa-bars" title="Menu Icon"></span>
+      <span class="fas icon-xxl fa-bars" title="Menu Icon"></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>fa-times</em></h4>
-      <span class="fas icon-small fa-times"><span class="sr-only">Exit Icon</span></span>
-      <span class="fas icon-base fa-times"><span class="sr-only">Exit Icon</span></span>
-      <span class="fas icon-large fa-times"><span class="sr-only">Exit Icon</span></span>
-      <span class="fas icon-xl fa-times"><span class="sr-only">Exit Icon</span></span>
-      <span class="fas icon-xxl fa-times"><span class="sr-only">Exit Icon</span></span>
+      <span class="fas icon-small fa-times" title="Exit Icon"></span>
+      <span class="fas icon-base fa-times" title="Exit Icon"></span>
+      <span class="fas icon-large fa-times" title="Exit Icon"></span>
+      <span class="fas icon-xl fa-times" title="Exit Icon"></span>
+      <span class="fas icon-xxl fa-times" title="Exit Icon"></span>
     </div>
   </div>
   <div class="row">
   <div class="col col-sm-12 col-md-4 p-4">
     <h4><em>fa-search</em></h4>
-    <span class="fas icon-small fa-search"><span class="sr-only">Search Icon</span></span>
-    <span class="fas icon-base fa-search"><span class="sr-only">Search Icon</span></span>
-    <span class="fas icon-large fa-search"><span class="sr-only">Search Icon</span></span>
-    <span class="fas icon-xl fa-search"><span class="sr-only">Search Icon</span></span>
-    <span class="fas icon-xxl fa-search"><span class="sr-only">Search Icon</span></span>
+    <span class="fas icon-small fa-search" title="Search Icon"></span>
+    <span class="fas icon-base fa-search" title="Search Icon"></span>
+    <span class="fas icon-large fa-search" title="Search Icon"></span>
+    <span class="fas icon-xl fa-search" title="Search Icon"></span>
+    <span class="fas icon-xxl fa-search" title="Search Icon"></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
     <h4><em>fa-arrow-right</em></h4>
-    <span class="fas icon-small fa-arrow-right"><span class="sr-only">Arrow Right Icon</span></span>
-    <span class="fas icon-base fa-arrow-right"><span class="sr-only">Arrow Right Icon</span></span>
-    <span class="fas icon-large fa-arrow-right"><span class="sr-only">Arrow Right Icon</span></span>
-    <span class="fas icon-xl fa-arrow-right"><span class="sr-only">Arrow Right Icon</span></span>
-    <span class="fas icon-xxl fa-arrow-right"><span class="sr-only">Arrow Right Icon</span></span>
+    <span class="fas icon-small fa-arrow-right" title="Arrow Right Icon"></span>
+    <span class="fas icon-base fa-arrow-right" title="Arrow Right Icon"></span>
+    <span class="fas icon-large fa-arrow-right" title="Arrow Right Icon"></span>
+    <span class="fas icon-xl fa-arrow-right" title="Arrow Right Icon"></span>
+    <span class="fas icon-xxl fa-arrow-right" title="Arrow Right Icon"></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
     <h4><em>fa-external-link-alt</em></h4>
-    <span class="fas icon-small fa-external-link-alt"><span class="sr-only">External Link Icon</span></span>
-    <span class="fas icon-base fa-external-link-alt"><span class="sr-only">External Link Icon</span></span>
-    <span class="fas icon-large fa-external-link-alt"><span class="sr-only">External Link Icon</span></span>
-    <span class="fas icon-xl fa-external-link-alt"><span class="sr-only">External Link Icon</span></span>
-    <span class="fas icon-xxl fa-external-link-alt"><span class="sr-only">External Link Icon</span></span>
+    <span class="fas icon-small fa-external-link-alt" title="External Link Icon"></span>
+    <span class="fas icon-base fa-external-link-alt" title="External Link Icon"></span>
+    <span class="fas icon-large fa-external-link-alt" title="External Link Icon"></span>
+    <span class="fas icon-xl fa-external-link-alt" title="External Link Icon"></span>
+    <span class="fas icon-xxl fa-external-link-alt" title="External Link Icon"></span>
     </div>
   </div>
   <div class="row">
   <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>fa-chevron-**</em></h4>
-      <span class="fas icon-small fa-chevron-up"><span class="sr-only">Chevron Up Icon</span></span>
-      <span class="fas icon-small fa-chevron-down"><span class="sr-only">Chevron Down Icon</span></span>
-      <span class="fas icon-small fa-chevron-left"><span class="sr-only">Chevron Left Icon</span></span>
-      <span class="fas icon-small fa-chevron-right"><span class="sr-only">Chevron Right Icon</span></span>
+      <span class="fas icon-small fa-chevron-up" title="Chevron Up Icon"></span>
+      <span class="fas icon-small fa-chevron-down" title="Chevron Down Icon"></span>
+      <span class="fas icon-small fa-chevron-left" title="Chevron Left Icon"></span>
+      <span class="fas icon-small fa-chevron-right" title="Chevron Right Icon"></span>
       <br/ >
-      <span class="fas icon-base fa-chevron-up"><span class="sr-only">Chevron Up Icon</span></span>
-      <span class="fas icon-base fa-chevron-down"><span class="sr-only">Chevron Down Icon</span></span>
-      <span class="fas icon-base fa-chevron-left"><span class="sr-only">Chevron Left Icon</span></span>
-      <span class="fas icon-base fa-chevron-right"><span class="sr-only">Chevron Right Icon</span></span>
+      <span class="fas icon-base fa-chevron-up" title="Chevron Up Icon"></span>
+      <span class="fas icon-base fa-chevron-down" title="Chevron Down Icon"></span>
+      <span class="fas icon-base fa-chevron-left" title="Chevron Left Icon"></span>
+      <span class="fas icon-base fa-chevron-right" title="Chevron Right Icon"></span>
       <br/ >
-      <span class="fas icon-large fa-chevron-up"><span class="sr-only">Chevron Up Icon</span></span>
-      <span class="fas icon-large fa-chevron-down"><span class="sr-only">Chevron Down Icon</span></span>
-      <span class="fas icon-large fa-chevron-left"><span class="sr-only">Chevron Left Icon</span></span>
-      <span class="fas icon-large fa-chevron-right"><span class="sr-only">Chevron Right Icon</span></span>
+      <span class="fas icon-large fa-chevron-up" title="Chevron Up Icon"></span>
+      <span class="fas icon-large fa-chevron-down" title="Chevron Down Icon"></span>
+      <span class="fas icon-large fa-chevron-left" title="Chevron Left Icon"></span>
+      <span class="fas icon-large fa-chevron-right" title="Chevron Right Icon"></span>
       <br/ >
-      <span class="fas icon-xl fa-chevron-up"><span class="sr-only">Chevron Up Icon</span></span>
-      <span class="fas icon-xl fa-chevron-down"><span class="sr-only">Chevron Down Icon</span></span>
-      <span class="fas icon-xl fa-chevron-left"><span class="sr-only">Chevron Left Icon</span></span>
-      <span class="fas icon-xl fa-chevron-right"><span class="sr-only">Chevron Right Icon</span></span>
+      <span class="fas icon-xl fa-chevron-up" title="Chevron Up Icon"></span>
+      <span class="fas icon-xl fa-chevron-down" title="Chevron Down Icon"></span>
+      <span class="fas icon-xl fa-chevron-left" title="Chevron Left Icon"></span>
+      <span class="fas icon-xl fa-chevron-right" title="Chevron Right Icon"></span>
       <br/ >
-      <span class="fas icon-xxl fa-chevron-up"><span class="sr-only">Chevron Up Icon</span></span>
-      <span class="fas icon-xxl fa-chevron-down"><span class="sr-only">Chevron Down Icon</span></span>
-      <span class="fas icon-xxl fa-chevron-left"><span class="sr-only">Chevron Left Icon</span></span>
-      <span class="fas icon-xxl fa-chevron-right"><span class="sr-only">Chevron Right Icon</span></span>
+      <span class="fas icon-xxl fa-chevron-up" title="Chevron Up Icon"></span>
+      <span class="fas icon-xxl fa-chevron-down" title="Chevron Down Icon"></span>
+      <span class="fas icon-xxl fa-chevron-left" title="Chevron Left Icon"></span>
+      <span class="fas icon-xxl fa-chevron-right" title="Chevron Right Icon"></span>
     </div>
     <div class="col col-sm-12 col-md-4 p-4">
       <h4><em>fa-play   fa-pause</em></h4>
-      <span class="fas icon-small fa-play"><span class="sr-only">Play Icon</span></span>
-      <span class="fas icon-small fa-pause"><span class="sr-only">Pause Icon</span></span>
+      <span class="fas icon-small fa-play" title="Play Icon"></span>
+      <span class="fas icon-small fa-pause" title="Pause Icon"></span>
       <br />
-      <span class="fas icon-base fa-play"><span class="sr-only">Play Icon</span></span>
-      <span class="fas icon-base fa-pause"><span class="sr-only">Pause Icon</span></span>
+      <span class="fas icon-base fa-play" title="Play Icon"></span>
+      <span class="fas icon-base fa-pause" title="Pause Icon"></span>
       <br />
-      <span class="fas icon-large fa-play"><span class="sr-only">Play Icon</span></span>
-      <span class="fas icon-large fa-pause"><span class="sr-only">Pause Icon</span></span>
+      <span class="fas icon-large fa-play" title="Play Icon"></span>
+      <span class="fas icon-large fa-pause" title="Pause Icon"></span>
       <br />
-      <span class="fas icon-xl fa-play"><span class="sr-only">Play Icon</span></span>
-      <span class="fas icon-xl fa-pause"><span class="sr-only">Pause Icon</span></span>
+      <span class="fas icon-xl fa-play" title="Play Icon"></span>
+      <span class="fas icon-xl fa-pause" title="Pause Icon"></span>
       <br />
-      <span class="fas icon-xxl fa-play"><span class="sr-only">Play Icon</span></span>
-      <span class="fas icon-xxl fa-pause"><span class="sr-only">Pause Icon</span></span>
+      <span class="fas icon-xxl fa-play" title="Play Icon"></span>
+      <span class="fas icon-xxl fa-pause" title="Pause Icon"></span>
     </div>
   </div>
 </div>`)
@@ -113,27 +113,27 @@ storiesOf('Components/Icons', module)
   <div class="row">
   <div class="col col-sm-12 col-md-4 p-4">
     <h4><em>Email Icon</em></h4>
-    <span class="fas icon-small fa-envelope"><span class="sr-only">Email Icon</span></span>
-    <span class="fas icon-base fa-envelope"><span class="sr-only">Email Icon</span></span>
-    <span class="fas icon-large fa-envelope"><span class="sr-only">Email Icon</span></span>
-    <span class="fas icon-xl fa-envelope"><span class="sr-only">Email Icon</span></span>
-    <span class="fas icon-xxl fa-envelope"><span class="sr-only">Email Icon</span></span>
+    <span class="fas icon-small fa-envelope" title="Email Icon"></span>
+    <span class="fas icon-base fa-envelope" title="Email Icon"></span>
+    <span class="fas icon-large fa-envelope" title="Email Icon"></span>
+    <span class="fas icon-xl fa-envelope" title="Email Icon"></span>
+    <span class="fas icon-xxl fa-envelope" title="Email Icon"></span>
   </div>
   <div class="col col-sm-12 col-md-4 p-4">
     <h4><em>Phone Icon</em></h4>
-    <span class="fas icon-small fa-phone"><span class="sr-only">Phone Icon</span></span>
-    <span class="fas icon-base fa-phone"><span class="sr-only">Phone Icon</span></span>
-    <span class="fas icon-large fa-phone"><span class="sr-only">Phone Icon</span></span>
-    <span class="fas icon-xl fa-phone"><span class="sr-only">Phone Icon</span></span>
-    <span class="fas icon-xxl fa-phone"><span class="sr-only">Phone Icon</span></span>
+    <span class="fas icon-small fa-phone" title="Phone Icon"></span>
+    <span class="fas icon-base fa-phone" title="Phone Icon"></span>
+    <span class="fas icon-large fa-phone" title="Phone Icon"></span>
+    <span class="fas icon-xl fa-phone" title="Phone Icon"></span>
+    <span class="fas icon-xxl fa-phone" title="Phone Icon"></span>
   </div>
   <div class="col col-sm-12 col-md-4 p-4">
     <h4><em>Address Icon</em></h4>
-    <span class="fas icon-small fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
-    <span class="fas icon-base fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
-    <span class="fas icon-large fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
-    <span class="fas icon-xl fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
-    <span class="fas icon-xxl fa-map-marker-alt"><span class="sr-only">Address Icon</span></span>
+    <span class="fas icon-small fa-map-marker-alt" title="Address Icon"></span>
+    <span class="fas icon-base fa-map-marker-alt" title="Address Icon"></span>
+    <span class="fas icon-large fa-map-marker-alt" title="Address Icon"></span>
+    <span class="fas icon-xl fa-map-marker-alt" title="Address Icon"></span>
+    <span class="fas icon-xxl fa-map-marker-alt" title="Address Icon"></span>
   </div>
 </div>`)
 
@@ -142,26 +142,26 @@ storiesOf('Components/Icons', module)
   <div class="row">
   <div class="col col-sm-12 col-md-4 p-4">
     <h4><em>Info Icon</em></h4>
-    <span class="fas icon-small fa-info-circle"><span class="sr-only">Info Icon</span></span>
-    <span class="fas icon-base fa-info-circle"><span class="sr-only">Info Icon</span></span>
-    <span class="fas icon-large fa-info-circle"><span class="sr-only">Info Icon</span></span>
-    <span class="fas icon-xl fa-info-circle"><span class="sr-only">Info Icon</span></span>
-    <span class="fas icon-xxl fa-info-circle"><span class="sr-only">Info Icon</span></span>
+    <span class="fas icon-small fa-info-circle" title="Info Icon"></span>
+    <span class="fas icon-base fa-info-circle" title="Info Icon"></span>
+    <span class="fas icon-large fa-info-circle" title="Info Icon"></span>
+    <span class="fas icon-xl fa-info-circle" title="Info Icon"></span>
+    <span class="fas icon-xxl fa-info-circle" title="Info Icon"></span>
   </div>
   <div class="col col-sm-12 col-md-4 p-4">
     <h4><em>Security Icon</em></h4>
-    <span class="fas icon-small fa-lock"><span class="sr-only">Security Icon</span></span>
-    <span class="fas icon-base fa-lock"><span class="sr-only">Security Icon</span></span>
-    <span class="fas icon-large fa-lock"><span class="sr-only">Security Icon</span></span>
-    <span class="fas icon-xl fa-lock"><span class="sr-only">Security Icon</span></span>
-    <span class="fas icon-xxl fa-lock"><span class="sr-only">Security Icon</span></span>
+    <span class="fas icon-small fa-lock" title="Security Icon"></span>
+    <span class="fas icon-base fa-lock" title="Security Icon"></span>
+    <span class="fas icon-large fa-lock" title="Security Icon"></span>
+    <span class="fas icon-xl fa-lock" title="Security Icon"></span>
+    <span class="fas icon-xxl fa-lock" title="Security Icon"></span>
   </div>
   <div class="col col-sm-12 col-md-4 p-4">
     <h4><em>Download Icon</em></h4>
-    <span class="fas icon-small fa-file-download"><span class="sr-only">Download Icon</span></span>
-    <span class="fas icon-base fa-file-download"><span class="sr-only">Download Icon</span></span>
-    <span class="fas icon-large fa-file-download"><span class="sr-only">Download Icon</span></span>
-    <span class="fas icon-xl fa-file-download"><span class="sr-only">Download Icon</span></span>
-    <span class="fas icon-xxl fa-file-download"><span class="sr-only">Download Icon</span></span>
+    <span class="fas icon-small fa-file-download" title="Download Icon"></span>
+    <span class="fas icon-base fa-file-download" title="Download Icon"></span>
+    <span class="fas icon-large fa-file-download" title="Download Icon"></span>
+    <span class="fas icon-xl fa-file-download" title="Download Icon"></span>
+    <span class="fas icon-xxl fa-file-download" title="Download Icon"></span>
   </div>
 </div>`)
