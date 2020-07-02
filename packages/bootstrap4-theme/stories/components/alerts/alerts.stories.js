dd24a7a00446607a7277a0b0ecb0711109112365
Michael Samuelson <mlsamuel@asu.edu>
2020-07-02 10:58:57 -0700

    UDS-238 Adding banners and implementing alerts with flexbox.

--- packages/bootstrap4-theme/stories/components/alerts/alerts.stories.js
+++ packages/bootstrap4-theme/stories/components/alerts/alerts.stories.js
@@ -6,33 +6,61 @@ storiesOf('Components/Alerts', module)
   })
 
 .add('Alerts', () => `
+
   <div class="alert alert-warning" role="alert">
-    <span title="Alert" class="fa fa-icon fa-bell"></span>
-    <button aria-label="Close" onclick="event.target.parentNode.style.display='none';" class="close">x</button>
-    Alert (orange): This is a warning alert to alert, confirm or notify. It is
-    built using <a href="https://getbootstrap.com/docs/4.0/components/alerts">the
-    Bootstrap 4 .alert-warning class</a>.
+    <div class="alert-icon">
+      <span title="Alert" class="fa fa-icon fa-bell"></span>
+    </div>
+    <div class="alert-content">
+      Alert (orange): This is a warning alert to alert, confirm or notify. It is
+      built using <a href="https://getbootstrap.com/docs/4.0/components/alerts">the
+      Bootstrap 4 .alert-warning class</a>.
+    </div>
+    <div class="alert-close">
+      <button type="button" class="btn btn-circle btn-circle-alt-white close" aria-label="Close" onclick="event.target.parentNode.style.display='none';">x</button>
+    </div>
   </div>
+
   <div class="alert alert-success" role="alert">
-    <span title="Success" class="fa fa-icon fa-check-circle"></span>
-    <button aria-label="Close" onclick="event.target.parentNode.style.display='none';" class="close">x</button>
-    Success (green): This is a success alert to confirm or notify. It is built
-    using <a href="https://getbootstrap.com/docs/4.0/components/alerts">the
-    Bootstrap 4 .alert-success class</a>.
+    <div class="alert-icon">
+      <span title="Success" class="fa fa-icon fa-check-circle"></span>
+    </div>
+    <div class="alert-content">
+      Success (green): This is a success alert to confirm or notify. It is built
+      using <a href="https://getbootstrap.com/docs/4.0/components/alerts">the
+      Bootstrap 4 .alert-success class</a>.
+    </div>
+    <div class="alert-close">
+      <button type="button" class="btn btn-circle btn-circle-alt-white close" aria-label="Close" onclick="event.target.parentNode.style.display='none';">x</button>
+    </div>
   </div>
+
   <div class="alert alert-info" role="alert">
-    <span title="Information" class="fa fa-icon fa-info-circle"></span>
-    <button aria-label="Close" onclick="event.target.parentNode.style.display='none';" class="close">x</button>
-    Information (blue): This is a info alert to confirm or notify. It is built
-    using <a href="https://getbootstrap.com/docs/4.0/components/alerts">the
-    Bootstrap 4 .alert-info class</a>.
+    <div class="alert-icon">
+      <span title="Information" class="fa fa-icon fa-info-circle"></span>
+    </div>
+    <div class="alert-content">
+      Information (blue): This is a info alert to confirm or notify. It is built
+      using <a href="https://getbootstrap.com/docs/4.0/components/alerts">the
+      Bootstrap 4 .alert-info class</a>.
+    </div>
+    <div class="alert-close">
+      <button type="button" class="btn btn-circle btn-circle-alt-white close" aria-label="Close" onclick="event.target.parentNode.style.display='none';">x</button>
+    </div>
   </div>
+
   <div class="alert alert-danger" role="alert">
-    <span title="Error" class="fa fa-icon fa-exclamation-triangle"></span>
-    <button aria-label="Close" onclick="event.target.parentNode.style.display='none';" class="close">x</button>
-    Error (red): This is a danger alert used specifically for errors. It is
-    built using <a href="https://getbootstrap.com/docs/4.0/components/alerts">the
-    Bootstrap 4 .alert-danger class</a>.
+    <div class="alert-icon">
+      <span title="Error" class="fa fa-icon fa-exclamation-triangle"></span>
+    </div>
+    <div class="alert-content">
+      Error (red): This is a danger alert used specifically for errors. It is
+      built using <a href="https://getbootstrap.com/docs/4.0/components/alerts">the
+      Bootstrap 4 .alert-danger class</a>.
+    </div>
+    <div class="alert-close">
+      <button type="button" class="btn btn-circle btn-circle-alt-white close" aria-label="Close" onclick="event.target.parentNode.style.display='none';">x</button>
+    </div>
   </div>
 `)
 
---
+++ packages/bootstrap4-theme/stories/components/banners/banners.stories.js
@@ -0,0 +1,123 @@
+import { document, console } from 'global';
+import { storiesOf } from '@storybook/html';
+storiesOf('Components/Banners', module)
+  .addParameters({
+    happo: false,
+  })
+
+  // TODO FLEXBOX THIS SUCKER.
+  // TODO Re-namespace the top level class and copy over rules.
+
+.add('Banners', () => `
+  <!--<div class="alert alert-warning" role="alert">-->
+
+  <div class="banner banner-green" role="banner">
+    <div class="banner-icon">
+      <span title="Banner" class="fa fa-icon fa-bell"></span>
+    </div>
+    <div class="banner-content">
+      <h3>Stay up-to-date on what's new at ASU</h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud <a href="https://asunow.asu.edu/">read the latest updates
+      here</a>.
+    </div>
+    <div class="banner-buttons">
+      <a href="https://provost.asu.edu/sync/students" type="button" class="btn btn-sm btn-dark">Info. on teaching and learning remotely</a>
+      <a href="https://students.asu.edu/faq" type="button" class="btn btn-sm btn-dark">FAQ Page</a>
+    </div>
+    <div class="banner-close">
+      <button type="button" class="btn btn-circle btn-circle-alt-white close" aria-label="Close" onclick="event.target.parentNode.parentNode.style.display='none';">x</button>
+    </div>
+  </div>
+
+  <br>
+
+  <div class="banner banner-orange" role="banner">
+    <div class="banner-icon">
+      <span title="Banner" class="fa fa-icon fa-bell"></span>
+    </div>
+    <div class="banner-content">
+      <h3>Stay up-to-date on what's new at ASU</h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud <a href="https://asunow.asu.edu/">read the latest updates
+      here</a>.
+    </div>
+    <div class="banner-buttons">
+      <a href="https://provost.asu.edu/sync/students" type="button" class="btn btn-sm btn-dark">Info. on teaching and learning remotely</a>
+      <a href="https://students.asu.edu/faq" type="button" class="btn btn-sm btn-dark">FAQ Page</a>
+    </div>
+    <div class="banner-close">
+      <button type="button" class="btn btn-circle btn-circle-alt-white close" aria-label="Close" onclick="event.target.parentNode.parentNode.style.display='none';">x</button>
+    </div>
+  </div>
+
+  <br>
+
+  <div class="banner banner-blue" role="banner">
+    <div class="banner-icon">
+      <span title="Banner" class="fa fa-icon fa-bell"></span>
+    </div>
+    <div class="banner-content">
+      <h3>Stay up-to-date on what's new at ASU</h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud <a href="https://asunow.asu.edu/">read the latest updates
+      here</a>.
+    </div>
+    <div class="banner-buttons">
+      <a href="https://provost.asu.edu/sync/students" type="button" class="btn btn-sm btn-dark">Info. on teaching and learning remotely</a>
+      <a href="https://students.asu.edu/faq" type="button" class="btn btn-sm btn-dark">FAQ Page</a>
+    </div>
+    <div class="banner-close">
+      <button type="button" class="btn btn-circle btn-circle-alt-white close" aria-label="Close" onclick="event.target.parentNode.parentNode.style.display='none';">x</button>
+    </div>
+  </div>
+
+  <br>
+
+  <div class="banner banner-gray" role="banner">
+    <div class="banner-icon">
+      <span title="Banner" class="fa fa-icon fa-bell"></span>
+    </div>
+    <div class="banner-content">
+      <h3>Stay up-to-date on what's new at ASU</h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud <a href="https://asunow.asu.edu/">read the latest updates
+      here</a>.
+    </div>
+    <div class="banner-buttons">
+      <a href="https://provost.asu.edu/sync/students" type="button" class="btn btn-sm btn-dark">Info. on teaching and learning remotely</a>
+      <a href="https://students.asu.edu/faq" type="button" class="btn btn-sm btn-dark">FAQ Page</a>
+    </div>
+    <div class="banner-close">
+      <button type="button" class="btn btn-circle btn-circle-alt-white close" aria-label="Close" onclick="event.target.parentNode.parentNode.style.display='none';">x</button>
+    </div>
+  </div>
+
+  <br>
+
+  <div class="banner banner-black" role="banner">
+    <div class="banner-icon">
+      <span title="Banner" class="fa fa-icon fa-bell"></span>
+    </div>
+    <div class="banner-content">
+      <h3>Stay up-to-date on what's new at ASU</h3>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud <a href="https://asunow.asu.edu/">read the latest updates
+      here</a>.
+    </div>
+    <div class="banner-buttons">
+      <a href="https://provost.asu.edu/sync/students" type="button" class="btn btn-sm btn-gold">Info. on teaching and learning remotely</a>
+      <a href="https://students.asu.edu/faq" type="button" class="btn btn-sm btn-gold">FAQ Page</a>
+    </div>
+    <div class="banner-close">
+      <button type="button" class="btn btn-circle btn-circle-alt-white close" aria-label="Close" onclick="event.target.parentNode.parentNode.style.display='none';">x</button>
+    </div>
+  </div>
+`)
+
+; // close
