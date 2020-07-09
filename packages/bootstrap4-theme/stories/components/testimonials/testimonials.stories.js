5e23f125f3f7d2aa0232a4bd7e93f2dd325e45a9
Nathan Rollins <nathan.rollins@asu.edu>
2020-07-08 17:11:23 -0700

    Update class names in Stories renamed from "ws2-" to "uds-"

--- packages/bootstrap4-theme/stories/components/testimonials/testimonials.stories.js
+++ packages/bootstrap4-theme/stories/components/testimonials/testimonials.stories.js
@@ -18,13 +18,13 @@ To come.
 .add('Testimonials, Option 2', () => `
 <div class="uds-testimonial uds-testimonial-2" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
   <div class="container uds-testimonial-container">
-    <blockquote class="ws2-element-gold ws2-element-spacing-entity">
+    <blockquote class="uds-element-gold uds-element-spacing-entity">
       <p>Having spent a lot of time learning on-the-job I thought I knew what
       earning my degree would be like, but ASU Online has surpassed my
       expectations in both the quality of coursework and in services offered.</p>
       <hr class="copy-divider">
-      <cite class="ws2-sig-title">Dustin Hall</cite>
-      <cite class="ws2-sig-byline">ASU Online student, 2019</cite>
+      <cite class="uds-sig-title">Dustin Hall</cite>
+      <cite class="uds-sig-byline">ASU Online student, 2019</cite>
     </blockquote>
   </div>
 </div>
