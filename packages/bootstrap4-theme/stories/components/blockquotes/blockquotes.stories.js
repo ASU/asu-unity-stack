506162374468acd546bc8f5a409381e08becbc94
Bryan R <aubjr@asu.edu>
2020-07-17 14:45:05 -0700

    UDS-123 (Blockquotes) - Design review fix of links in black alt background; Updated blockquote sample text with better descriptions

--- packages/bootstrap4-theme/stories/components/blockquotes/blockquotes.stories.js
+++ packages/bootstrap4-theme/stories/components/blockquotes/blockquotes.stories.js
@@ -1,4 +1,4 @@
-import { document, console } from 'global';
+/*import { document, console } from 'global'; */
 import { storiesOf } from '@storybook/html';
 import blockquoteImage from './180x180.png';
 
@@ -9,39 +9,46 @@ storiesOf('Components/Blockquotes', module)
     happo: false,
   })
 
-  .add('Blockquote (Default)', () => `
+  .add('Default', () => `
     <blockquote class="ws2-element-spacing-entity">
-      <p>Blockquote default<br>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+      <p>Blockquote default<br>
+      <strong>This has default 48px margins on top and bottom...</strong> -- Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
       euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
-      tation ullamcorper suscipit lobortis nisl ut <a href="some-link" title="Title!" class="class1">aliquip ex ea commodo
+      tation ullamcorper suscipit lobortis nisl ut <a href="#" title="Title!" class="class1">aliquip ex ea commodo
       consequat</a>. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
       dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
       delenit augue duis dolore te feugait nulla facilisi.</p>
       <cite class="ws2-sig-title">Signature</cite>
       <cite class="ws2-sig-byline">Sig byline</cite>
     </blockquote>
-    <hr />
+   `)
+  .add('Default (Maroon)', () => `
     <blockquote class="ws2-element-maroon ws2-element-style ws2-element-spacing-entity">
-      <p>Blockquote maroon.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+      <p>Blockquote maroon.<br>
+      <strong>This has default 48px margins on top and bottom...</strong> -- Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
       quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues.
       It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
       Angles, quam un skeptic Cambridge amico dit me que Occidental es. END TESTIMONIAL</p>
       <cite class="sig-title ws2-sig-title">Signature name</cite>
       <cite class="sig-byline ws2-sig-byline">Signature byline</cite>
     </blockquote>
-    <hr />
+   `)
+  .add('Default (Gold)', () => `
     <blockquote class="ws2-element-gold ws2-element-style ws2-element-spacing-entity">
-      <p>Blockquote Gold.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam
+      <p>Blockquote Gold.<br>
+      <strong>This has default 48px margins on top and bottom...</strong> -- Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam
       ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It
       va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
       Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
     </blockquote>
-    <hr />
-    <blockquote class="ws2-bq ws2-bq-image ws2-element-gray ws2-element-style ws2-element-spacing-entity">
+  `)
+  .add('Default (Image)', () => `
+  <blockquote class="ws2-bq ws2-bq-image ws2-element-gray ws2-element-style ws2-element-spacing-entity">
       <p>
         <img alt="one eighty alt" title="one eighty title" class="media-element file-default panopoly-image-original"
         data-delta="1" src="${blockquoteImage}">
-        Blockquote Image.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+        Blockquote Image.<br>
+        <strong>This has default 48px margins on top and bottom...</strong> -- Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
         quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan
         lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un
         simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
@@ -50,32 +57,37 @@ storiesOf('Components/Blockquotes', module)
     </blockquote>
   `)
 
-  .add('Blockquote (Alt Gray)', () => `
+  .add('Alt Gray (Default)', () => `
     <blockquote class="ws2-element-alt ws2-element-alt-gray ws2-element-alt-no-padding ws2-element-spacing-entity-full">
-      <p>Blockquote default<br>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+      <p>Blockquote default<br>
+      <strong>This has the FULL 96px margins on top and bottom...</strong> -- Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
       euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
-      tation ullamcorper suscipit lobortis nisl ut <a href="some-link" title="Title!" class="class1">aliquip ex ea commodo
+      tation ullamcorper suscipit lobortis nisl ut <a href="#" title="Title!" class="class1">aliquip ex ea commodo
       consequat</a>. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
       dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
       delenit augue duis dolore te feugait nulla facilisi.</p>
       <cite class="ws2-sig-title">Signature</cite>
       <cite class="ws2-sig-byline">Sig byline</cite>
     </blockquote>
-    <hr />
+  `)
+  .add('Alt Gray (Maroon)', () => `
     <blockquote class="ws2-element-maroon ws2-element-style ws2-element-alt ws2-element-alt-gray ws2-element-spacing-entity-full">
-      <p>Blockquote maroon.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+      <p>Blockquote maroon.<br>
+      <strong>This has the FULL 96px margins on top and bottom...</strong> -- Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
       quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues.
       It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
       Angles, quam un skeptic Cambridge amico dit me que Occidental es. END TESTIMONIAL</p>
       <cite class="sig-title ws2-sig-title">Signature name</cite>
       <cite class="sig-byline ws2-sig-byline">Signature byline</cite>
     </blockquote>
-    <hr />
+  `)
+  .add('Alt Gray (Image)', () => `
     <blockquote class="ws2-bq ws2-bq-image ws2-element-style ws2-element-gray ws2-element-alt ws2-element-alt-gray ws2-element-spacing-entity-full">
       <p>
         <img alt="one eighty alt" title="one eighty title" class="media-element file-default panopoly-image-original"
         data-delta="1" src="${blockquoteImage}">
-        Blockquote Image.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+        Blockquote Image.<br>
+        <strong>This has the FULL 96px margins on top and bottom...</strong> -- Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
         quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan
         lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un
         simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
@@ -84,28 +96,35 @@ storiesOf('Components/Blockquotes', module)
     </blockquote>
   `)
 
-  .add('Blockquote (Alt Black)', () => `
+  .add('Alt Black (Default)', () => `
     <blockquote class="ws2-element-alt ws2-element-alt-black ws2-element-style ws2-element-white ws2-element-spacing-inline">
-      <p>Blockquote default<br>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
+      <p>Blockquote default<br>
+      <strong>This has 0px/96px margins on the top/bottom...</strong> -- Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh
       euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci
-      tation ullamcorper suscipit lobortis nisl ut <a href="some-link" title="Title!" class="class1">aliquip ex ea commodo
+      tation ullamcorper suscipit lobortis nisl ut <a href="#" title="Title!" class="class1">aliquip ex ea commodo
       consequat</a>. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum
       dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril
       delenit augue duis dolore te feugait nulla facilisi.</p>
       <cite class="ws2-sig-title">Signature</cite>
       <cite class="ws2-sig-byline">Sig byline</cite>
     </blockquote>
+   `)
+  .add('Alt Black (Gold)', () => `
     <blockquote class="ws2-element-gold ws2-element-style ws2-element-alt ws2-element-alt-black ws2-element-spacing-inline">
-      <p>Blockquote Gold.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam
+      <p>Blockquote Gold.<br>
+      <strong>This has 0px/96px margins on the top/bottom...</strong> -- Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam
       ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It
       va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un simplificat
       Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
     </blockquote>
+   `)
+  .add('Alt Black (Image)', () => `
     <blockquote class="ws2-bq ws2-bq-image ws2-element-style ws2-element-gray ws2-element-alt ws2-element-alt-black ws2-element-spacing-inline">
       <p>
         <img alt="one eighty alt" title="one eighty title" class="media-element file-default panopoly-image-original"
         data-delta="1" src="${blockquoteImage}">
-        Blockquote Image.<br>Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
+        Blockquote Image.<br>
+        <strong>This has 0px/96px margins on the top/bottom...</strong> -- Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari
         quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan
         lingues. It va esser tam simplic quam Occidental: in fact, it va esser Occidental. A un Angleso it va semblar un
         simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es.</p>
