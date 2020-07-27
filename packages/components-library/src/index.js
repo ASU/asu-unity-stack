50efdbba4021838a90f129d03009c792b0835285
Colton Testamarck <colt@asu.edu>
2020-07-27 14:07:35 -0700

    renamed Icon to Icons

--- packages/components-library/src/index.js
+++ packages/components-library/src/index.js
@@ -3,7 +3,7 @@
 import { h, Fragment } from "preact";
 import Header from "./components/Header";
 import Button from "./components/Button";
-import {Icon} from "./components/Icon/styles";
+import {Icon} from "./components/Icons/styles";
 import {H4} from "./components/Heading/styles";
 import {Panel} from "./components/Panel/styles";
 import {HydratePreact, RenderPreact} from "./helpers";
