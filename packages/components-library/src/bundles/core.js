559a4cfefae060e6f8776b263c322c49e04ffffd
Colton Testamarck <colt@asu.edu>
2020-07-28 15:05:18 -0700

    removed experimental code

--- packages/components-library/src/bundles/core.js
+++ packages/components-library/src/bundles/core.js
@@ -1,7 +1,5 @@
 import Header from "../components/Header";
 import Button from "../components/Button";
 import {HydratePreact, RenderPreact} from "../helpers";
-import * as Preact from "preact";
-import * as Emotion from "emotion";
 
-export {Header, Button, HydratePreact, RenderPreact, Preact, Emotion};
+export {Header, Button, HydratePreact, RenderPreact};
