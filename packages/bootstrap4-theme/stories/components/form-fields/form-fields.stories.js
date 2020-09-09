1145fa73da812bf524819ea1cfda7bbcf6de45a2
Michael Samuelson <mlsamuel@asu.edu>
2020-09-09 14:35:26 -0700

    UDS-176 UDS-177 UDS-178 UDS-179 Lots of form stuff. Largely radios and checkboxes.

--- packages/bootstrap4-theme/stories/components/form-fields/form-fields.stories.js
+++ packages/bootstrap4-theme/stories/components/form-fields/form-fields.stories.js
@@ -381,7 +381,260 @@ Should only run and be packaged for storybook preview.
 
   `)
 
-  .add('Kitchen Sink Form', () => `
+.add('Checkboxes', () => `
+<form>
+  <div class="form-check">
+    <input class="form-check-input" type="checkbox" id="loneCheckbox1" value="option1">
+    <label class="form-check-label" for="loneCheckbox1">I like checkboxes</label>
+  </div>
+
+  <fieldset>
+    <legend>A Group of Checkboxes</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="checkbox1" value="option1">
+      <label class="form-check-label" for="checkbox1">1</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="checkbox2" value="option2" checked>
+      <label class="form-check-label" for="checkbox2">2</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="checkbox3" value="option3" disabled>
+      <label class="form-check-label" for="checkbox3">3 (disabled)</label>
+    </div>
+  </fieldset>
+</form>
+`)
+
+.add('Radios', () => `
+<form>
+  <fieldset>
+    <legend>A Group of Radios</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+      <label class="form-check-label" for="exampleRadios1">
+        Default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+      <label class="form-check-label" for="exampleRadios2">
+        Second default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option3">
+      <label class="form-check-label" for="exampleRadios3">
+        Third default radio
+      </label>
+    </div>
+    <div class="form-check disabled">
+      <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option4" disabled>
+      <label class="form-check-label" for="exampleRadios4">
+        Disabled radio
+      </label>
+    </div>
+  </fieldset>
+</form>
+`)
+
+.add('Checkboxes and Radios, multiple backgrounds', () => `
+
+<div style="padding:2rem;background-color:white;"><!-- This div for Storybook display only. -->
+<form class="uds-form-white">
+
+  <fieldset>
+    <legend>A Group of Checkboxes</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="whiteCheckbox1" value="option1">
+      <label class="form-check-label" for="whiteCheckbox1">1</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="whiteCheckbox2" value="option2" checked>
+      <label class="form-check-label" for="whiteCheckbox2">2</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="whiteCheckbox3" value="option3" disabled>
+      <label class="form-check-label" for="whiteCheckbox3">3 (disabled)</label>
+    </div>
+  </fieldset>
+  <fieldset>
+    <legend>A Group of Radios</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="whiteExampleRadios" id="whiteExampleRadios1" value="option1" checked>
+      <label class="form-check-label" for="WhiteExampleRadios1">
+        Default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="whiteExampleRadios" id="whiteExampleRadios2" value="option2">
+      <label class="form-check-label" for="whiteExampleRadios2">
+        Second default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="whiteExampleRadios" id="whiteExampleRadios3" value="option3">
+      <label class="form-check-label" for="whiteExampleRadios3">
+        Third default radio
+      </label>
+    </div>
+    <div class="form-check disabled">
+      <input class="form-check-input" type="radio" name="whiteExampleRadios" id="whiteExampleRadios4" value="option4" disabled>
+      <label class="form-check-label" for="whiteExampleRadios4">
+        Disabled radio
+      </label>
+    </div>
+  </fieldset>
+</form>
+</div>
+
+<div style="padding:2rem;background-color:#fafafa;"><!-- This div for Storybook display only. -->
+<form class="uds-form-gray1">
+
+  <fieldset>
+    <legend>A Group of Checkboxes</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gray1Checkbox1" value="option1">
+      <label class="form-check-label" for="gray1Checkbox1">1</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gray1Checkbox2" value="option2" checked>
+      <label class="form-check-label" for="gray1Checkbox2">2</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gray1Checkbox3" value="option3" disabled>
+      <label class="form-check-label" for="gray1Checkbox3">3 (disabled)</label>
+    </div>
+  </fieldset>
+  <fieldset>
+    <legend>A Group of Radios</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="gray1ExampleRadios" id="gray1ExampleRadios1" value="option1" checked>
+      <label class="form-check-label" for="gray1ExampleRadios1">
+        Default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="gray1ExampleRadios" id="gray1ExampleRadios2" value="option2">
+      <label class="form-check-label" for="gray1ExampleRadios2">
+        Second default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="gray1ExampleRadios" id="gray1ExampleRadios3" value="option3">
+      <label class="form-check-label" for="gray1ExampleRadios3">
+        Third default radio
+      </label>
+    </div>
+    <div class="form-check disabled">
+      <input class="form-check-input" type="radio" name="gray1ExampleRadios" id="greay1ExampleRadios4" value="option4" disabled>
+      <label class="form-check-label" for="gray1ExampleRadios4">
+        Disabled radio
+      </label>
+    </div>
+  </fieldset>
+</form>
+</div>
+
+<div style="padding:2rem;background-color:#e8e8e8;"><!-- This div for Storybook display only. -->
+<form class="uds-form-gray2">
+
+  <fieldset>
+    <legend>A Group of Checkboxes</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gray2Checkbox1" value="option1">
+      <label class="form-check-label" for="gray2Checkbox1">1</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gray2Checkbox2" value="option2" checked>
+      <label class="form-check-label" for="gray2Checkbox2">2</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gray2Checkbox3" value="option3" disabled>
+      <label class="form-check-label" for="gray2Checkbox3">3 (disabled)</label>
+    </div>
+  </fieldset>
+  <fieldset>
+    <legend>A Group of Radios</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="gray2ExampleRadios" id="gray2ExampleRadios1" value="option1" checked>
+      <label class="form-check-label" for="gray2ExampleRadios1">
+        Default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="gray2ExampleRadios" id="gray2ExampleRadios2" value="option2">
+      <label class="form-check-label" for="gray2ExampleRadios2">
+        Second default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="gray2ExampleRadios" id="gray2ExampleRadios3" value="option3">
+      <label class="form-check-label" for="gray2ExampleRadios3">
+        Third default radio
+      </label>
+    </div>
+    <div class="form-check disabled">
+      <input class="form-check-input" type="radio" name="gray2ExampleRadios" id="gray2ExampleRadios4" value="option4" disabled>
+      <label class="form-check-label" for="gray2ExampleRadios4">
+        Disabled radio
+      </label>
+    </div>
+  </fieldset>
+</form>
+</div>
+
+<div style="padding:2rem;background-color:#191919;"><!-- This div for Storybook display only. -->
+<form class="uds-form-gray7">
+
+  <fieldset>
+    <legend>A Group of Checkboxes</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gray7Checkbox1" value="option1">
+      <label class="form-check-label" for="gray7Checkbox1">1</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gray7Checkbox2" value="option2" checked>
+      <label class="form-check-label" for="gray7Checkbox2">2</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gray7Checkbox3" value="option3" disabled>
+      <label class="form-check-label" for="gray7Checkbox3">3 (disabled)</label>
+    </div>
+  </fieldset>
+  <fieldset>
+    <legend>A Group of Radios</legend>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="gray7ExampleRadios" id="gray7ExampleRadios1" value="option1" checked>
+      <label class="form-check-label" for="gray7ExampleRadios1">
+        Default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="gray7ExampleRadios" id="gray7ExampleRadios2" value="option2">
+      <label class="form-check-label" for="gray7ExampleRadios2">
+        Second default radio
+      </label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="gray7ExampleRadios" id="gray7ExampleRadios3" value="option3">
+      <label class="form-check-label" for="gray7ExampleRadios3">
+        Third default radio
+      </label>
+    </div>
+    <div class="form-check disabled">
+      <input class="form-check-input" type="radio" name="gray7ExampleRadios" id="gray7ExampleRadios4" value="option4" disabled>
+      <label class="form-check-label" for="gray7ExampleRadios4">
+        Disabled radio
+      </label>
+    </div>
+  </fieldset>
+</form>
+
+</div>
+`)
+
+.add('Kitchen Sink Form', () => `
 <p><a href="https://getbootstrap.com/docs/4.0/components/forms/">Bootstrap 4 form docs</a></p>
 
 <form>
@@ -446,20 +699,20 @@ Should only run and be packaged for storybook preview.
   </div>
 
   <div class="form-check">
-    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-    <label class="form-check-label" for="inlineCheckbox1">1</label>
+    <input class="form-check-input" type="checkbox" id="checkbox1" value="option1">
+    <label class="form-check-label" for="checkbox1">1</label>
   </div>
   <div class="form-check">
-    <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" checked>
-    <label class="form-check-label" for="inlineCheckbox2">2</label>
+    <input class="form-check-input" type="checkbox" id="checkbox2" value="option2" checked>
+    <label class="form-check-label" for="checkbox2">2</label>
   </div>
   <div class="form-check">
-    <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" disabled>
-    <label class="form-check-label" for="inlineCheckbox3">3 (disabled)</label>
+    <input class="form-check-input" type="checkbox" id="checkbox3" value="option3" disabled>
+    <label class="form-check-label" for="checkbox3">3 (disabled)</label>
   </div>
 
 </form>
   `)
 
-; // clos)
+; // close
 
