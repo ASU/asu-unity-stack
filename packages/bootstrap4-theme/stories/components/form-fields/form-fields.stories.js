f9de48ceb0d2c89f5d32d2e78779357e31557a62
Michael Samuelson <mlsamuel@asu.edu>
2020-10-07 11:25:38 -0700

    UDS-360 Adding Selects story for easier testing.

--- packages/bootstrap4-theme/stories/components/form-fields/form-fields.stories.js
+++ packages/bootstrap4-theme/stories/components/form-fields/form-fields.stories.js
@@ -1190,6 +1190,55 @@ Should only run and be packaged for storybook preview.
 </div>
 `)
 
+.add('Selects', () => `
+<form>
+<div class="form-group">
+<label for="exampleFormControlSelect1">Example select</label>
+<select class="form-control" id="exampleFormControlSelect1">
+  <option>1</option>
+  <option>2</option>
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+</select>
+</div>
+<div class="form-group">
+<label for="exampleFormControlSelect2">Example multiple select</label>
+<select multiple class="form-control" id="exampleFormControlSelect2">
+  <option>1</option>
+  <option>2</option>
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+</select>
+</div>
+
+<div class="form-group">
+<label for="exampleFormControlSelect3">Example select with server-side validation</label>
+<select class="form-control is-valid" id="exampleFormControlSelect3" aria-describedby="myValidSelectMsg">
+  <option>1</option>
+  <option>2</option>
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+</select>
+<small id="myValidSelectMsg" class="valid-feedback is-valid"><span title="Alert" class="fa fa-icon fa-check-circle"></span>Success message</small>
+</div>
+<div class="form-group">
+<label for="exampleFormControlSelect4"><span title="Required" class="fa fa-icon fa-circle uds-field-required"></span>Example multiple select - required with server-side invalidation</label>
+<select multiple class="form-control is-invalid" id="exampleFormControlSelect4" aria-describedby="myInvalidSelectMsg" aria-required="true" required>
+  <option>1</option>
+  <option>2</option>
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+</select>
+<small id="myInvalidSelectMsg" class="invalid-feedback is-invalid"><span title="Alert" class="fa fa-icon fa-exclamation-triangle"></span>Form error message</small>
+</div>
+
+</form>
+`)
+
 .add('Kitchen Sink Form', () => `
 <p><a href="https://getbootstrap.com/docs/4.0/components/forms/">Bootstrap 4 form docs</a></p>
 
