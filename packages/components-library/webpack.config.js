727f6c574c394bdf252d8c7f34326116d20490be
Colton Testamarck <colt@asu.edu>
2020-10-28 14:27:41 -0700

    fix(asu-unity-stack): upgraded component modules to use common dependencies and upgrade to webpack 5

--- packages/components-library/webpack.config.js
+++ packages/components-library/webpack.config.js
@@ -4,10 +4,10 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = [];
 
-
 const shared = {
+  context: __dirname,
   entry: {
-    core: "./index.js"
+    "components-library": "./src/index.js",
   },
   module: {
     rules: [
@@ -18,12 +18,97 @@ const shared = {
           {
             loader: "babel-loader",
             options: {
-              presets: ['preact'],
-            }
+              presets: ["preact"],
+            },
           },
         ],
       },
     ],
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      // Must be below test-utils
+    },
+  },
+  externals: {
+    preact : {
+      commonjs: 'preact',
+      commonjs2: 'preact',
+      amd: 'preact',
+      root: 'preact'
+    },
+    emotion: {
+      commonjs: 'emotion',
+      commonjs2: 'emotion',
+      amd: 'emotion',
+      root: 'emotion'
+    },
+    "prop-types": {
+      commonjs: 'prop-types',
+      commonjs2: 'prop-types',
+      amd: 'prop-types',
+      root: 'propTypes'
+    }
+  }
+};
+
+
+
+// development bundle config
+module.exports.push({
+  ...shared,
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].dev.js",
+    libraryTarget: "umd",
+    library: "componentsLibrary",
+    umdNamedDefine: true,
+  },
+});
+
+// production bundle
+module.exports.push({
+  ...shared,
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    library: "componentsLibrary",
+    umdNamedDefine: true,
+  },
+  plugins: [
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+        },
+      }),
+    ],
+  },
+});
+
+/** Vendor bundle config */
+module.exports.push({
+  context: shared.context,
+  entry: {
+    vendor: ["./src/vendor.js"]
+  },
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    umdNamedDefine: true
   },
   resolve: {
     extensions: [".js", ".jsx"],
@@ -34,94 +119,46 @@ const shared = {
       // Must be below test-utils
     },
   }
-}
-
-// development bundle config
-module.exports.push({
-  ...shared,
-  context: path.join(__dirname, "src"),
-  mode: "development",
-
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].development.js",
-    libraryTarget: "umd",
-    library: "AsuWeb[name]",
-    umdNamedDefine: true,
-  },
-});
-
-// production bundle
-module.exports.push({
-  ...shared,
-  context: path.join(__dirname, "src"),
-  mode: "production",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].production.js",
-    libraryTarget: "umd",
-    library: "AsuWeb[name]",
-    umdNamedDefine: true,
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all'
-        }
-      },
-    },
-    minimizer: [new TerserPlugin({
-      parallel: true,
-      terserOptions: {
-        ecma: 6,
-      },
-    })],
-  },
 });
 
 // SSR bundle config - should be used server-side only
-module.exports.push(
-  {
-    mode: "production",
-    context: path.join(__dirname, "src"),
-    target: 'node',
-    entry: {
-      code: "./ssr.js",
-    },
-    output: {
-      path: path.join(__dirname, "dist"),
-      filename: "[name].ssr.js",
-      libraryTarget: "umd",
-      globalObject: "this",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(jsx|js)?$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "babel-loader",
-              // options: {
-              //   rootMode: "upward",
-              // },
-            },
-          ],
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".js", ".jsx"],
-      alias: {
-        react: "preact/compat",
-        //"react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-        // Must be below test-utils
+module.exports.push({
+  mode: "production",
+  context: path.join(__dirname, "src"),
+  target: "node",
+  entry: {
+    code: "./ssr.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].ssr.js",
+    libraryTarget: "umd",
+    globalObject: "this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            // options: {
+            //   rootMode: "upward",
+            // },
+          },
+        ],
       },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      react: "preact/compat",
+      //"react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      // Must be below test-utils
     },
-    externals: [nodeExternals()],
-  }
-);
+  },
+  externals: [nodeExternals()],
+});
