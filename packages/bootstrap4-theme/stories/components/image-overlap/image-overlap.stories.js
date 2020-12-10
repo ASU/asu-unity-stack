68ac9a6a5c080a9b1288ab9e36ab5faa28ff8cfb
Steve Ryan <steve.ryan@asu.edu>
2020-12-10 09:47:36 -0700

    Work in progress - image content overlap.

---
+++ packages/bootstrap4-theme/stories/components/image-overlap/image-overlap.stories.js
@@ -0,0 +1,35 @@
+export default { title: 'Components/Content Image Overlap' };
+
+export const overlapRight = () => `
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+
+        <div class="image-overlap">
+          <img src="https://picsum.photos/800/600/?blur" alt="Generic image from Lorem Pixum" />
+          <div class="content-wrapper">
+            <h3>This is the content that goes in the box.</h3>
+            <p>Instagram tour operator travel sailing flying package. Territory New York City group discount active lifestyle creditcard insurance wellness kayak guide overnight rural lonely planet.</p>
+            <p>Train luxury Cuba Paris recommendations nature France sight seeing. Flexibility Amsterdam maps. Pacific lonely planet private jet national insurance taxi tourist attractions. Budget Pacific guide caravan Barcelona place to stay maps gateway diary tour operator money</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="row mt-6">
+      <div class="col-md-12">
+
+        <div class="image-overlap content-bottom">
+          <img src="https://picsum.photos/800/600/?blur" alt="Generic image from Lorem Pixum" />
+          <div class="content-wrapper">
+            <h3>This is the content that goes in the box.</h3>
+            <p>Instagram tour operator travel sailing flying package. Territory New York City group discount active lifestyle creditcard insurance wellness kayak guide overnight rural lonely planet.</p>
+            <p>Train luxury Cuba Paris recommendations nature France sight seeing. Flexibility Amsterdam maps. Pacific lonely planet private jet national insurance taxi tourist attractions. Budget Pacific guide caravan Barcelona place to stay maps gateway diary tour operator money</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+`
