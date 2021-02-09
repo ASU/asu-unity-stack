0c0a01353985b9a753c73c7e92652a88dd2dc826
Steve Ryan <steve.ryan@asu.edu>
2021-02-09 11:59:09 -0700

    Added mobile layout for the component + a separate documentation story

--- packages/bootstrap4-theme/stories/components/image-overlap/image-overlap.stories.js
+++ packages/bootstrap4-theme/stories/components/image-overlap/image-overlap.stories.js
@@ -1,27 +1,30 @@
-export default { title: 'Components/Content Image Overlap' };
+export default { title: 'Components/Content Sections' };
 
-export const overlapRight = () => `
+export const contentImageOverlapRight = () => `
   <div class="container">
     <div class="row">
       <div class="col-md-12">
 
-        <div class="image-overlap">
-          <img src="https://picsum.photos/800/600/?blur" alt="Generic image from Lorem Pixum" />
+        <div class="uds-image-overlap">
+          <img class="img-fluid" src="https://placeimg.com/800/600/nature" alt="Generic image from PlaceIMG" />
           <div class="content-wrapper">
             <h3>This is the content that goes in the box.</h3>
             <p>Instagram tour operator travel sailing flying package. Territory New York City group discount active lifestyle creditcard insurance wellness kayak guide overnight rural lonely planet.</p>
-            <p>Train luxury Cuba Paris recommendations nature France sight seeing. Flexibility Amsterdam maps. Pacific lonely planet private jet national insurance taxi tourist attractions. Budget Pacific guide caravan Barcelona place to stay maps gateway diary tour operator money</p>
+            <p>Train luxury Paris recommendations nature France sight seeing. Flexibility Amsterdam maps. Pacific lonely planet private jet national insurance taxi tourist attractions. Budget Pacific guide caravan Barcelona place to stay maps gateway diary tour operator money</p>
           </div>
         </div>
 
       </div>
     </div>
-
-    <div class="row mt-6">
+  </div>
+`
+export const contentImageOverlapLeft = () => `
+  <div class="container">
+    <div class="row">
       <div class="col-md-12">
 
-        <div class="image-overlap content-bottom">
-          <img src="https://picsum.photos/800/600/?blur" alt="Generic image from Lorem Pixum" />
+        <div class="uds-image-overlap content-left">
+          <img class="img-fluid" src="https://placeimg.com/800/600/nature" alt="Generic image from PlaceIMG" />
           <div class="content-wrapper">
             <h3>This is the content that goes in the box.</h3>
             <p>Instagram tour operator travel sailing flying package. Territory New York City group discount active lifestyle creditcard insurance wellness kayak guide overnight rural lonely planet.</p>
