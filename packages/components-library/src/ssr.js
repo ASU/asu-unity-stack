beaa95b666f7446dcdc4f5543d00d7b2cd59dcbe
Colton Testamarck <colt@asu.edu>
2021-02-18 14:15:29 -0700

    feat(components-library): added footer component and template build

--- packages/components-library/src/ssr.js
+++ packages/components-library/src/ssr.js
@@ -1,29 +1,19 @@
 import render from "preact-render-to-string";
 
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 import { renderStylesToString } from "@emotion/server";
 import { NavTreeMega } from "./components/Nav/NavTreeExample";
 import { dom } from "@fortawesome/fontawesome-svg-core";
 
-/*
-import createEmotionServer from 'create-emotion-server'
-import createCache from '@emotion/cache'
-import { CacheProvider } from '@emotion/core'
-const cache = createCache()
-const { extractCritical } = createEmotionServer(cache)
-
-let element = (
-  <CacheProvider value={cache}>
-    <App />
-  </CacheProvider>
-)
-
-let { html, css, ids } = extractCritical(renderToString(element))*/
-
 let HeaderTemplate;
 let FullExample;
-let MobileTemplate;
+let FooterTemplate;
 
+// Generates a header template with a 'stub' div which can later be replaced with
+// an include directive. This allows the template to be injected with static markup
+// which acts a placeholder until Preact can initialize menu functionality.
+// See the script located under scripts/prerender-templates.js for how that's done
 try {
   HeaderTemplate = `<style type="text/css">${dom.css()}</style>${renderStylesToString(
     render(<Header dangerouslyGenerateStub={true} />)
@@ -34,6 +24,8 @@ try {
 } finally {
 }
 
+// Full pre-rendered example can be useful for seeing the general markup which
+// can be injected into the 'stub' templates
 try {
   FullExample = `<style type="text/css">${dom.css()}</style>${renderStylesToString(
     render(
@@ -53,8 +45,8 @@ try {
 }
 
 try {
-  MobileTemplate = `<style type="text/css">${dom.css()}</style>${renderStylesToString(
-    render(<Header />)
+  FooterTemplate = `${renderStylesToString(
+    render(<Footer />)
   )}`;
 } catch (error) {
   // handle error
@@ -62,4 +54,4 @@ try {
 } finally {
 }
 
-export { HeaderTemplate, FullExample, MobileTemplate };
+export { HeaderTemplate, FullExample, FooterTemplate };
