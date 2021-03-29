d99f699270db705ff55b96e653013e82b98996ea
tkilbour <tyk@asu.edu>
2021-03-29 10:29:24 -0700

    added dark and gray stories for tabs

--- packages/bootstrap4-theme/stories/components/tabs/tabs.stories.js
+++ packages/bootstrap4-theme/stories/components/tabs/tabs.stories.js
@@ -65,6 +65,74 @@ storiesOf('Components/Tabs', module)
 </div>
 </div>
   `)
+  .add('Dark background', () => `
+<div class="container background-darkmode">
+<div class="row">
+
+<div class="col-12 col-md-8 col-lg-10 col-xl-12">
+
+<nav class="mobile-tabs darkmode">
+<div class="nav nav-tabs" id="nav-tab" role="tablist" data-scroll-position="0">
+  <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home long tab</a>
+  <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile long tab</a>
+  <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact us is a long tab</a>
+
+
+
+</div>
+<a class="scroll-control-prev" href="#carouselExampleControls" role="button" data-scroll="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="scroll-control-next" href="#carouselExampleControls" role="button" data-scroll="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</nav>
+        <div class="tab-content" id="nav-tabContent">
+                  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Home tab content...</div>
+                  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Profile tab content...</div>
+                  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">Contact tab content...</div>
+                </div>
+</div>
+
+</div>
+</div>
+  `)
+  .add('Gray background', () => `
+<div class="container background-smokemode">
+<div class="row">
+
+<div class="col-12 col-md-8 col-lg-10 col-xl-12">
+
+<nav class="mobile-tabs smokemode">
+<div class="nav nav-tabs" id="nav-tab" role="tablist" data-scroll-position="0">
+  <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home long tab</a>
+  <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile long tab</a>
+  <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact us is a long tab</a>
+
+
+
+</div>
+<a class="scroll-control-prev" href="#carouselExampleControls" role="button" data-scroll="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="scroll-control-next" href="#carouselExampleControls" role="button" data-scroll="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</nav>
+        <div class="tab-content" id="nav-tabContent">
+                  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Home tab content...</div>
+                  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Profile tab content...</div>
+                  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">Contact tab content...</div>
+                </div>
+</div>
+
+</div>
+</div>
+  `)
   ;
 /*
   .add('Nav element', () => `<nav class="nav">
