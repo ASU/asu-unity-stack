1cf26b26494b23de3906f30df678b0b643f6a1c8
tkilbour <tyk@asu.edu>
2021-03-30 08:24:51 -0700

    removed desktop tab story

--- packages/bootstrap4-theme/stories/components/tabs/tabs.stories.js
+++ packages/bootstrap4-theme/stories/components/tabs/tabs.stories.js
@@ -7,31 +7,7 @@ storiesOf('Components/Tabs', module)
     happo: false,
   })
 
-  .add('Desktop', () => `
-<div class="container-fluid">
-<div class="row">
-
-<div class="col-12">
-
-<nav class="nav-tabs-scrolling">
-          <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home long tab</a>
-            <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile long tab</a>
-            <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact us is a long tab</a>
-          </div>
-        </nav>
-        <div class="tab-content" id="nav-tabContent">
-                  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">Home tab content...</div>
-                  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">Profile tab content...</div>
-                  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">Contact tab content...</div>
-                </div>
-</div>
-
-</div>
-</div>
-  `)
-
-  .add('Mobile', () => `
+  .add('Regular tabs', () => `
 <div class="container">
 <div class="row">
 
