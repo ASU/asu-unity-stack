d4fda33cff190acf1bbde9364784d851c1bc3d95
Steve Ryan <steve.ryan@asu.edu>
2021-03-30 23:15:33 -0700

    SASS cleanup, editing documentation.

--- packages/bootstrap4-theme/stories/components/blockquote/blockquote.stories.js
+++ packages/bootstrap4-theme/stories/components/blockquote/blockquote.stories.js
@@ -24,7 +24,7 @@ export const blockquoteNoImage = () => `
     <div class="row pt-6">
       <div class="col-md-8">
 
-        <div class="wrapper p-6 bg-gray-7">
+        <div class="wrapper p-4 p-sm-6 bg-gray-7">
 
           <div class="uds-blockquote accent-gold text-white">
             <svg title="Open quote" role="presentation" viewBox="0 0 302.87 245.82">
@@ -52,7 +52,7 @@ export const blockquoteWithImage = () => `
     <div class="row">
       <div class="col-md-8">
 
-        <div class="uds-blockquote accent-gold with-image">
+        <div class="uds-blockquote with-image">
           <img src="https://placeimg.com/400/400/any" alt="Pretend this is Michael M. Crow, President of ASU"/>
           <blockquote>
             <p>ASU is a comprehensive public research university, measured not by whom we exclude, but rather by whom we include and how they succeed; advancing research and discovery of public value; and assuming fundamental responsibility for the economic, social, cultural and overall health of the communities it serves.</p>
@@ -69,7 +69,7 @@ export const blockquoteWithImage = () => `
     <div class="row mt-6">
       <div class="col-md-10">
 
-        <div class="wrapper p-6 bg-gray-2">
+        <div class="wrapper p-4 p-sm-6 bg-gray-2">
 
           <div class="uds-blockquote with-image reversed">
             <img src="https://placeimg.com/300/300/any" alt="Pretend this is Michael M. Crow, President of ASU"/>
@@ -95,7 +95,7 @@ export const blockquoteNoCitation = () => `
     <div class="row">
       <div class="col-md-8">
 
-        <div class="uds-blockquote no-cite with-image">
+        <div class="uds-blockquote no-citation with-image">
           <img src="https://placeimg.com/300/300/nature" alt="Image of Walt Disney"/>
           <blockquote>
             <h3><span class="highlight-black">Walt Disney</span></h3>
@@ -109,9 +109,9 @@ export const blockquoteNoCitation = () => `
     <div class="row pt-6">
       <div class="col-md-8">
 
-        <div class="wrapper p-6 bg-gray-7">
+        <div class="wrapper p-4 p-sm-6 bg-gray-7">
 
-        <div class="uds-blockquote no-cite with-image reversed">
+        <div class="uds-blockquote no-citation with-image reversed">
           <img src="https://placeimg.com/300/300/tech" alt="Image of Walt Disney"/>
           <blockquote>
             <h3><span class="highlight-gold">Walt Disney</span></h3>
@@ -140,11 +140,9 @@ export const blockquoteAltCitation = () => `
             <p>Some people want it to happen, some people wish it would happen and some people make it happen.</p>
             <div class="citation">
               <cite class="name">Michael Jordan</cite>
+              <cite class="description">NBA Superstar</cite>
             </div>
           </blockquote>
-          <svg title="Close quote" role="presentation" viewBox="0 0 191 150">
-            <path d="M135.851,23.1h68.8V72.855q0,30.049-5.264,47.291T179.8,151.3q-14.323,13.916-36.115,21.8l-13.466-28.818q20.567-6.9,29.5-19.212T168.9,92.313H135.851Zm-115.567,0h68.8V72.855q0,30.049-5.142,47.291T64.478,151.3q-14.323,13.916-36.36,21.8L14.652,144.284q20.812-6.9,29.626-19.212t9.3-32.759h-33.3Z" transform="translate(-13.652 -23.102)"/>
-          </svg>
         </div>
 
       </div><!-- end .col -->
@@ -153,7 +151,7 @@ export const blockquoteAltCitation = () => `
     <div class="row pt-6">
       <div class="col-md-9">
 
-        <div class="wrapper p-6 bg-gray-2">
+        <div class="wrapper p-4 p-sm-6 bg-gray-1">
 
           <div class="uds-blockquote alt-citation accent-maroon">
             <svg title="Open quote" role="presentation" viewBox="0 0 302.87 245.82">
@@ -163,11 +161,9 @@ export const blockquoteAltCitation = () => `
               <p>Talent wins games, but teamwork and intelligence wins championships.</p>
               <div class="citation">
                 <cite class="name">Michael Jordan</cite>
+                <cite class="description">NBA Superstar</cite>
               </div>
             </blockquote>
-            <svg title="Close quote" role="presentation" viewBox="0 0 191 150">
-              <path d="M135.851,23.1h68.8V72.855q0,30.049-5.264,47.291T179.8,151.3q-14.323,13.916-36.115,21.8l-13.466-28.818q20.567-6.9,29.5-19.212T168.9,92.313H135.851Zm-115.567,0h68.8V72.855q0,30.049-5.142,47.291T64.478,151.3q-14.323,13.916-36.36,21.8L14.652,144.284q20.812-6.9,29.626-19.212t9.3-32.759h-33.3Z" transform="translate(-13.652 -23.102)"/>
-            </svg>
           </div>
 
         </div>
@@ -184,7 +180,7 @@ export const testimonialsNoImage = () => `
     <div class="row">
       <div class="col-md-6">
 
-        <div class="uds-blockquote uds-testimonial alt-citation accent-gold">
+        <div class="uds-blockquote uds-testimonial accent-gold">
           <svg title="Open quote" role="presentation" viewBox="0 0 302.87 245.82">
             <path d="M113.61,245.82H0V164.56q0-49.34,8.69-77.83T40.84,35.58Q64.29,12.95,100.67,0l22.24,46.9q-34,11.33-48.72,31.54T58.63,132.21h55Zm180,0H180V164.56q0-49.74,8.7-78T221,35.58Q244.65,12.95,280.63,0l22.24,46.9q-34,11.33-48.72,31.54t-15.57,53.77h55Z"/>
           </svg>
@@ -203,9 +199,9 @@ export const testimonialsNoImage = () => `
     <div class="row mt-6">
       <div class="col-md-8">
 
-        <div class="wrapper p-6 bg-gray-1">
+        <div class="wrapper p-4 p-sm-6 bg-gray-2">
 
-          <div class="uds-blockquote uds-testimonial alt-citation accent-maroon">
+          <div class="uds-blockquote uds-testimonial accent-maroon">
             <svg title="Open quote" role="presentation" viewBox="0 0 302.87 245.82">
               <path d="M113.61,245.82H0V164.56q0-49.34,8.69-77.83T40.84,35.58Q64.29,12.95,100.67,0l22.24,46.9q-34,11.33-48.72,31.54T58.63,132.21h55Zm180,0H180V164.56q0-49.74,8.7-78T221,35.58Q244.65,12.95,280.63,0l22.24,46.9q-34,11.33-48.72,31.54t-15.57,53.77h55Z"/>
             </svg>
@@ -250,7 +246,7 @@ export const testimonialsWithImage = () => `
     <div class="row mt-6">
       <div class="col-md-8">
 
-        <div class="wrapper p-6 bg-gray-7">
+        <div class="wrapper p-6 p-sm-4 bg-gray-7">
 
           <div class="uds-blockquote uds-testimonial with-image alt-citation accent-gold text-white">
             <img src="https://placeimg.com/400/400/tech" alt="Pretend this is Han Solo"/>
