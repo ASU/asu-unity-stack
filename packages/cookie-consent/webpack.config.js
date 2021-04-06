6a8fe8d77cfc0bfcb2332bfed3e162ad4b4260ef
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-06 12:08:50 -0700

    build (cookie-consent): remove vendor.js build output

--- packages/cookie-consent/webpack.config.js
+++ packages/cookie-consent/webpack.config.js
@@ -92,27 +92,3 @@ module.exports.push({
     },
   },
 });
-
-/** Vendor bundle config */
-module.exports.push({
-  context: shared.context,
-  entry: {
-    vendor: ["./src/vendor.js"],
-  },
-  mode: "production",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    libraryTarget: "umd",
-    umdNamedDefine: true,
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-    alias: {
-      "react": "preact/compat",
-      "react-dom/test-utils": "preact/test-utils",
-      "react-dom": "preact/compat",
-      // Must be below test-utils
-    },
-  },
-});
