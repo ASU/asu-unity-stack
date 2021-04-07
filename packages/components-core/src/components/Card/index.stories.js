189bbe38f468b29b7ab16e0580212a1c4158b1c3
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-07 15:30:31 -0700

    docs (components-core): expand stories variants

--- packages/components-core/src/components/Card/index.stories.js
+++ packages/components-core/src/components/Card/index.stories.js
@@ -44,3 +44,65 @@ Default.args = {
   buttons: ["Google label", "Microsoft label"],
   tags: ["tag one", "tag two", "tag three", "tag four"],
 };
+
+export const Degree = Template.bind({});
+Degree.args = {
+  type: "degree",
+  width: "25%",
+  horizontal: false,
+  hover: false,
+  image: "https://picsum.photos/300/200",
+  imageAltText: "An example image",
+  icon: "",
+  title: "Default title",
+  body:
+    "Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.",
+};
+
+export const Event = Template.bind({});
+Event.args = {
+  type: "default",
+  width: "25%",
+  horizontal: false,
+  hover: false,
+  image: "https://picsum.photos/300/200",
+  imageAltText: "An example image",
+  icon: "",
+  title: "Event title",
+  body:
+    "Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.",
+  eventDisplay: "Inline",
+  startTime: "Wednesday, November 21st, 2021 11:30 a.m",
+  stopTime: "12:30 p.m.",
+  location: "Downtown Phoenix campus",
+};
+
+export const Story = Template.bind({});
+Story.args = {
+  type: "story",
+  width: "25%",
+  horizontal: false,
+  hover: false,
+  image: "https://picsum.photos/300/200",
+  imageAltText: "An example image",
+  icon: "",
+  title: "Story title",
+  body:
+    "Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.",
+  tags: ["tag one", "tag two", "tag three"],
+};
+
+export const StoryButtonCard = Template.bind({});
+StoryButtonCard.args = {
+  type: "story",
+  width: "25%",
+  horizontal: false,
+  hover: true,
+  image: "https://picsum.photos/300/200",
+  imageAltText: "An example image",
+  icon: "",
+  title: "Story title",
+  body:
+    "Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.",
+  tags: ["tag one", "tag two", "tag three"],
+};
