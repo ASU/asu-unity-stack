eea493fae9517ce44c012d725c34606cc5daad96
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-07 07:27:18 -0700

    build (components-core): install storybook and additional dependencies

---
+++ packages/components-core/.storybook/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  resolve: {
+    extensions: [".js", "jsx"],
+    alias: {
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      // Must be below test-utils
+    },
+  },
+};
