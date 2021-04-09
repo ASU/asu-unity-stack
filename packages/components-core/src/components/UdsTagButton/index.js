fb0e2e159cbd303d3dc1cbf55bc688b820d2c57c
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-08 19:23:20 -0700

    feat (components-core): add UdsTagButton

---
+++ packages/components-core/src/components/UdsTagButton/index.js
@@ -0,0 +1,29 @@
+import { h } from "preact";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { Button } from "reactstrap";
+
+import UdsStyles from "../../../assets/css/bootstrap-asu.min.module.css";
+
+export const UdsTagButton = ({ color, children, ...props }) => {
+  const btnClasses = classNames(UdsStyles["btn"], UdsStyles[`btn-tag`], {
+    [UdsStyles[`btn-tag-alt-white`]]: color === "white",
+    [UdsStyles[`btn-tag-alt-gray`]]: color === "gray",
+    [UdsStyles[`btn-tag-alt-dark`]]: color === "dark",
+  });
+
+  return (
+    <Button className={btnClasses} {...props}>
+      {children}
+    </Button>
+  );
+};
+
+UdsTagButton.propTypes = {
+  ...Button.propTypes,
+  color: PropTypes.oneOf(["gold", "maroon", "gray", "dark"]),
+};
+
+UdsTagButton.defaultProps = {
+  color: "gray",
+};
