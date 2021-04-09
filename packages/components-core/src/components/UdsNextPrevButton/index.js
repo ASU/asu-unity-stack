3f4102d43910fff1d3e900e6821aa373a4e3a4f3
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-08 19:22:59 -0700

    feat (components-core): add UdsNextPrevButton

---
+++ packages/components-core/src/components/UdsNextPrevButton/index.js
@@ -0,0 +1,46 @@
+import { h } from "preact";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { Button } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+import UdsStyles from "../../../assets/css/bootstrap-asu.min.module.css";
+
+export const UdsNextPrevButton = ({ color, direction, ...rest }) => {
+  const btnClasses = classNames(
+    UdsStyles["btn"],
+    UdsStyles["btn-circle"],
+    UdsStyles["btn-circle-large"],
+    UdsStyles[`btn-circle-alt-${color}`]
+  );
+
+  if (direction === "next") {
+    return (
+      <Button className={btnClasses} {...rest}>
+        <FontAwesomeIcon icon={faChevronRight} />
+        <span class="sr-only">Next</span>
+      </Button>
+    );
+  }
+
+  return (
+    <Button className={btnClasses} {...rest}>
+      <FontAwesomeIcon icon={faChevronLeft} />
+      <span class="sr-only">Previous</span>
+    </Button>
+  );
+};
+
+UdsNextPrevButton.propTypes = {
+  color: PropTypes.oneOf(["white", "gray", "black"]),
+  direction: PropTypes.oneOf(["next", "prev"]),
+  ...Button.propTypes,
+};
+
+UdsNextPrevButton.defaultProps = {
+  color: "white",
+};
---
+++ packages/components-core/src/components/UdsNextPrevButton/index.stories.js
@@ -0,0 +1,38 @@
+import { h } from "preact";
+
+import { UdsNextPrevButton } from ".";
+
+export default {
+  title: "UDS/UdsNextPrevButton",
+  component: UdsNextPrevButton,
+  argTypes: {
+    color: {
+      type: "select",
+      options: ["white", "gray", "black"],
+    },
+    direction: {
+      type: "select",
+      options: ["next", "prev"],
+    },
+  },
+};
+
+const Template = args => (
+  <div class="container-fluid">
+    <div class="col col-sm-12 p-3">
+      <UdsNextPrevButton {...args} />
+    </div>
+  </div>
+);
+
+export const Next = Template.bind({});
+Next.args = {
+  color: "white",
+  direction: "next",
+};
+
+export const Previous = Template.bind({});
+Previous.args = {
+  color: "white",
+  direction: "prev",
+};
