4a64554c225e5fc2ed1339d0adb2eeae565c8803
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-11 16:07:21 -0700

    fix (components-core): fix project exports

--- packages/components-core/src/index.js
+++ packages/components-core/src/index.js
@@ -1,6 +1,1 @@
-import { h } from "preact";
-import { render } from "preact/compat";
-
-import Example from "./components/Example";
-
-render(<Example />, document.getElementById("root"));
+export * from './components';
