c65b3c784ef359e485855e61487d12f6b9c2482c
Mark Lambe <Mark@MarkLambe.com>
2021-04-14 17:18:41 +0100

    feat(bootstrap4-theme): 🎸 Adding Background Combinations
    ✅ Closes: UDS-457

---
+++ packages/bootstrap4-theme/stories/design/backgrounds/backgrounds.stories.js
@@ -0,0 +1,66 @@
+export default { title: 'Design/Backgrounds' };
+
+
+export const approvedBackgroundColors = () => `
+  <div class="container-fluid mt-2">
+    <div class="row">
+      <div class="col-12 pb-3">
+        <h3><span class="highlight-black">Approved Background Colors</span></h3>
+      </div>
+
+      <div class="col-sm-6">
+        <div class="background-panel gray-7-bg m-3"></div>
+      </div>
+      <div class="col-sm-6">
+        <div class="background-panel gray-2-bg m-3"></div>
+      </div>
+
+      <div class="col-sm-6">
+        <div class="background-panel gray-1-bg m-3"></div>
+      </div>
+      <div class="col-sm-6">
+        <div class="background-panel white-bg m-3"></div>
+      </div>
+    </div>
+  </div>
+`;
+
+export const approvedBackgroundPatterns = () => `
+  <div class="container-fluid mt-2">
+    <div class="row">
+      <div class="col-12 pb-3">
+        <h3><span class="highlight-black">Approved Background Patterns</span></h3>
+      </div>
+
+      <div class="col-12 pb-3">
+        <h3>Learn to thrive morse code</h3>
+      </div>
+      <div class="col-sm-6">
+        <div class="background-panel morse-code-bg-white m-3"></div>
+      </div>
+      <div class="col-sm-6">
+        <div class="background-panel morse-code-bg-black m-3"></div>
+      </div>
+
+      <div class="col-12 pb-3">
+        <h3>Network</h3>
+      </div>
+      <div class="col-sm-6">
+        <div class="background-panel network-bg-white m-3"></div>
+      </div>
+      <div class="col-sm-6">
+        <div class="background-panel network-bg-black m-3"></div>
+      </div>
+
+      <div class="col-12 pb-3">
+        <h3>A Mountain topographical map</h3>
+      </div>
+      <div class="col-sm-6">
+        <div class="background-panel topo-pattern-bg-white m-3"></div>
+      </div>
+      <div class="col-sm-6">
+        <div class="background-panel topo-pattern-bg-black m-3"></div>
+      </div>
+    </div>
+  </div>
+`;
