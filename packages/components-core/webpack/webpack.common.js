762ee313da182115caf62e0999a41fde39a59137
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-13 21:14:42 -0700

    build (components-core): refine webpack setup

--- packages/components-core/webpack/webpack.common.js
+++ packages/components-core/webpack/webpack.common.js
@@ -1,15 +1,18 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const WebpackBar = require("webpackbar");
-const path = require("path");
-// const glob = require("glob");
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const PurgecssPlugin = require("purgecss-webpack-plugin");
+
+const path = require("path");
+const glob = require("glob");
 
 const PROJECT_DIR = path.resolve(__dirname, "../");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    lib: "./src/index.js",
+  },
   output: {
     path: path.resolve(PROJECT_DIR, "dist"),
     filename: "[name].js",
@@ -26,8 +29,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          "style-loader",
-          // MiniCssExtractPlugin.loader,
+          MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
             options: {
@@ -55,7 +57,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new WebpackBar(),
     /**
      * All files inside webpack's output.path directory will be removed once, but the
      * directory itself will not be. If using webpack 4+'s default configuration,
@@ -68,28 +69,44 @@ module.exports = {
      * See `Options and Defaults` for information
      */
     new CleanWebpackPlugin(),
-    // new MiniCssExtractPlugin({
-    //   filename: "[name].css",
-    // }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
     // new PurgecssPlugin({
     //   paths: glob.sync(`${PROJECT_DIR}src/**/*`, { nodir: true }),
     // }),
-    new BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin(),
   ],
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM",
-  },
   // optimization: {
-  //   splitChunks: {
-  //     cacheGroups: {
-  //       styles: {
-  //         name: "styles",
-  //         test: /\.css$/,
-  //         chunks: "all",
-  //         enforce: true,
+  //   minimize: true,
+  //   minimizer: [
+  //     new CssMinimizerPlugin({
+  //       minimizerOptions: {
+  //         preset: [
+  //           "default",
+  //           {
+  //             discardComments: { removeAll: true },
+  //           },
+  //         ],
   //       },
-  //     },
-  //   },
+  //     }),
+  //   ],
   // },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+      // Must be below test-utils
+    },
+  },
+  externals: {
+    preact: {
+      commonjs: "preact",
+      commonjs2: "preact",
+      amd: "preact",
+      root: "preact",
+    },
+  },
 };
