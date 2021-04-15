58b34d96acce2936d301029dfe2feea37aa44998
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-15 12:41:39 -0700

    build(components-core): configure webpack to build fresh css module
    webpack now copies the current bootstrap4 css into /vendor/ folder and renames it to .module.css

--- packages/components-core/.storybook/main.js
+++ packages/components-core/.storybook/main.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const PROJECT_DIR = path.resolve(__dirname, "../");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -5,4 +8,16 @@ module.exports = {
     "@storybook/addon-essentials",
     "storybook-css-modules-preset",
   ],
+  webpackFinal: config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          Components: path.resolve(PROJECT_DIR, "src/components/"),
+          Vendor: path.resolve(PROJECT_DIR, "vendor/"),
+        },
+      },
+    };
+  },
 };
--- packages/components-core/webpack/webpack.common.js
+++ packages/components-core/webpack/webpack.common.js
@@ -1,5 +1,7 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const CopyPlugin = require("copy-webpack-plugin");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const PurgecssPlugin = require("purgecss-webpack-plugin");
@@ -75,8 +77,29 @@ module.exports = {
     // new PurgecssPlugin({
     //   paths: glob.sync(`${PROJECT_DIR}src/**/*`, { nodir: true }),
     // }),
-    new BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(
+            PROJECT_DIR,
+            "node_modules/@asu-design-system/bootstrap4-theme/dist/css/bootstrap-asu.min.css"
+          ),
+          to: path.resolve(
+            PROJECT_DIR,
+            "vendor/css/bootstrap-asu.min.module.css"
+          ),
+          force: true,
+        },
+      ],
+    }),
   ],
+  resolve: {
+    alias: {
+      Components: path.resolve(PROJECT_DIR, "src/components/"),
+      Vendor: path.resolve(PROJECT_DIR, "vendor/"),
+    },
+  },
   externals: {
     "react": "React",
     "react-dom": "ReactDOM",
