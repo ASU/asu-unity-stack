e0f484be0967c79ce95f980a1e4028fc6f668ff5
Mark Lambe <Mark@MarkLambe.com>
2021-04-15 17:26:40 +0100

    feat(bootstrap4-theme): 🎸 add backgrounds stories
    adding stories for the backgrounds and scaling xd file
    
    ✅ Closes: UDS-457

--- packages/bootstrap4-theme/stories/design/backgrounds/backgrounds.stories.js
+++ packages/bootstrap4-theme/stories/design/backgrounds/backgrounds.stories.js
@@ -64,3 +64,33 @@ export const approvedBackgroundPatterns = () => `
     </div>
   </div>
 `;
+
+export const imageBackgrounds = () => `
+  <div class="container-fluid mt-2">
+    <div class="row">
+      <div class="col-12 pb-3">
+        <h3><span class="highlight-black">Image Backgrounds</span></h3>
+      </div>
+
+      <div class="col-sm-6">
+        <div class="background-panel image-bg m-3"></div>
+      </div>
+    </div>
+  </div>
+`;
+
+export const backgroundsScaling = () => `
+  <div class="jumbotron-fluid max-size-container scaling-container">
+    <span class="content-description">1920px max width</span>
+    <div class="section-line"></div>
+    <div class="content-description-container scaling-container">
+      <div class="container scaling-container">
+        <span class="content-description">1200px margin width</span>
+        <div class="section-line"></div>
+        <div class="container content-container">
+          Section Content
+        </div>
+      </div>
+    </div>
+  </div>
+`;
