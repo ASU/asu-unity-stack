8e3139f03bb637ee24531af4c5cb8be1b2d5482c
Mark Lambe <Mark@MarkLambe.com>
2021-04-22 13:21:16 +0100

    feat: 🎸 add transition instead of animation

--- packages/bootstrap4-theme/stories/asucms/quote-with-animation/quote-with-animation.stories.js
+++ packages/bootstrap4-theme/stories/asucms/quote-with-animation/quote-with-animation.stories.js
@@ -1,35 +1,38 @@
 export default { title: 'ASUCMS/Quote With Animation' };
 
 export const defaultQuote = () => `
-  <div class="container space-for-scrolling">
-    <div>
-      <h4>The ASU difference:</h4>
-    </div>
-    <div>
-      <h1>We are measured not by whom we <span class="highlight-on-scroll-text">exclude,</span> but by whom we <span class="highlight-on-scroll-text">include</span> and how they <span class="highlight-on-scroll-text">succeed</span></h1>
-    </div>
-    <div>
-      <span>
-        —
-      </span>
-      <span>
-        Excerpt from ASU charter
-      </span>
-    </div>
-    <script>
-      document.querySelectorAll('.highlight-on-scroll-text').forEach((elem) => {
-        const observer = new IntersectionObserver(entries => {
-          entries.forEach(entry => {
-            console.log(entry);
-            if (entry.isIntersecting) {
-              console.log('intersecting!');
-              entry.target.classList.add('animate-bg-in-scroll');
-            }
+  <div class="container">
+    <h1>Scroll down to see the animated quote.</h1>
+    <div class="space-for-scrolling">
+      <div>
+        <h4>The ASU difference:</h4>
+      </div>
+      <div>
+        <h1>We are measured not by whom we <span class="highlight-on-scroll-text">exclude,</span> but by whom we <span class="highlight-on-scroll-text">include</span> and how they <span class="highlight-on-scroll-text delayed">succeed</span></h1>
+      </div>
+      <div>
+        <span>
+          —
+        </span>
+        <span>
+          Excerpt from ASU charter
+        </span>
+      </div>
+      <script>
+        document.querySelectorAll('.highlight-on-scroll-text').forEach((elem) => {
+          const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+              console.log(entry);
+              if (entry.isIntersecting) {
+                console.log('intersecting!');
+                entry.target.classList.add('animate-bg-in-scroll');
+              }
+            });
           });
+          observer.observe(elem);
         });
-        observer.observe(elem);
-      });
 
-    </script>
+      </script>
+    </div>
   </div>
 `;
