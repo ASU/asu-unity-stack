96d17f52b4180b0f149264747c16baaecf75d1e3
connercms <connercms@gmail.com>
2021-04-22 09:19:35 -0500

    fix(bootstrap4-theme): change header levels in story to be semantically correct

--- packages/bootstrap4-theme/stories/components/image-text-block/image-text-block.stories.js
+++ packages/bootstrap4-theme/stories/components/image-text-block/image-text-block.stories.js
@@ -8,7 +8,7 @@ export const ImageOnLeft = () => `
 
   <div class="uds-image-text-block-text-container">
     <h2>This is a secondary heading, Lorem ipsum dolor sit amet</h2>
-    <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.</h4>
+    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.</h3>
     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</p>
     <div class="d-flex mb-2">
       <a href="#" class="btn btn-dark mr-2">Button link here</a>
@@ -22,7 +22,7 @@ export const ImageOnRight = () => `
 <div class="uds-image-text-block-container">
   <div class="uds-image-text-block-text-container">
     <h2>This is a secondary heading, Lorem ipsum dolor sit amet</h2>
-    <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.</h4>
+    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.</h3>
     <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.</p>
     <div class="d-flex mb-2">
       <a href="#" class="btn btn-dark mr-2">Button link here</a>
