683fe2b3ed4bd7a77ba05c826caa138d786f2669
connercms <connercms@gmail.com>
2021-04-22 11:06:20 -0500

    feat(bootstrap4-theme): quote on image background

---
+++ packages/bootstrap4-theme/stories/components/quote-image-background/quote-image-background.stories.js
@@ -0,0 +1,9 @@
+export default { title: "Components/Content Sections/Quote On Image Background" }
+
+export const Default = () => `
+<div class="uds-quote-image-background-container">
+  <div class="uds-quote-image-background-image-container">
+  </div>
+</div>
+
+`;
