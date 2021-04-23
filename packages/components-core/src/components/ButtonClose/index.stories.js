0abb9ea30836abe515a8421b8b08f4d3f33eeb42
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-22 23:57:59 -0700

    feat(components-core): add story markup for Button color backgrounds

--- packages/components-core/src/components/ButtonClose/index.stories.js
+++ packages/components-core/src/components/ButtonClose/index.stories.js
@@ -8,23 +8,38 @@ export default {
   component: ButtonClose,
 };
 
-const Template = args => (
-  <div className="container-fluid">
-    <div className="col col-sm-12 p-3">
-      <ButtonClose {...args} />
+const Template = args => {
+  const { bgColor } = args;
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className={`col col-12 col-lg-4 bg-${bgColor} p-3`}>
+          <div className="container-fluid">
+            <div className="col col-sm-12 p-3">
+              <ButtonClose {...args} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export const DefaultCloseButton = Template.bind({});
-DefaultCloseButton.args = {};
+DefaultCloseButton.args = {
+  bgColor: "white",
+  color: "white",
+};
 
 export const GrayCloseButton = Template.bind({});
 GrayCloseButton.args = {
+  bgColor: "light",
   color: "gray",
 };
 
 export const DarkCloseButton = Template.bind({});
 DarkCloseButton.args = {
+  bgColor: "dark",
   color: "dark",
 };
