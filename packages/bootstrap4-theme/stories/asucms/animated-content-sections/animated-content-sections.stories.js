9e2477996ed0b0df3d310c94c831d49a1b183e15
Mark Lambe <Mark@MarkLambe.com>
2021-04-23 17:24:52 +0100

    feat: 🎸 added animated content cards

---
+++ packages/bootstrap4-theme/stories/asucms/animated-content-sections/animated-content-sections.stories.js
@@ -0,0 +1,52 @@
+export default { title: 'ASUCMS/Animated Content Sections' };
+
+export const AnimatiedcontentSectionWithCTAs = () => `
+  <div class="container my-5">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-4 text-white content-section">
+          <div class="image-holder"></div>
+          <div class="content-holder mx-4">
+            <h2>Experience world-class academics</h2>
+            <div class="my-3">
+              As a comprehensive public research university, ASU is committed to providing excellence in education through the Academic Enterprise, and enables the success of each unique student and increases access to higher education for all.
+            </div>
+            <div class="button-area">
+              <!-- START INSERT: Button Component -->
+              <input class="btn btn-gold mb-2" type="submit" value="Learn More">
+              <!-- END INSERT: Button Component -->
+            </div>
+          </div>
+        </div>
+        <div class="col-4 text-white content-section">
+          <div class="image-holder"></div>
+          <div class="content-holder mx-4">
+            <h2>Experience world-class academics</h2>
+            <div class="my-3">
+              As a comprehensive public research university, ASU is committed to providing excellence in education through the Academic Enterprise, and enables the success of each unique student and increases access to higher education for all.
+            </div>
+            <div class="button-area">
+              <!-- START INSERT: Button Component -->
+              <input class="btn btn-gold mb-2" type="submit" value="Learn More">
+              <!-- END INSERT: Button Component -->
+            </div>
+          </div>
+        </div>
+        <div class="col-4 text-white content-section">
+          <div class="image-holder"></div>
+          <div class="content-holder mx-4">
+            <h2>Experience world-class academics</h2>
+            <div class="my-3">
+              As a comprehensive public research university, ASU is committed to providing excellence in education through the Academic Enterprise, and enables the success of each unique student and increases access to higher education for all.
+            </div>
+            <div class="button-area">
+              <!-- START INSERT: Button Component -->
+              <input class="btn btn-gold mb-2" type="submit" value="Learn More">
+              <!-- END INSERT: Button Component -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
