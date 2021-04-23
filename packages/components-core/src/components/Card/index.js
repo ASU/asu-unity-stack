58806c21ed37f5c2261c6cf26bb56744d9919533
Nathan Rollins <nathan.rollins@asu.edu>
2021-04-22 22:08:51 -0700

    refactor(components-core): refactor Article and Card components
    Remove unneeded EventTitle and EventLocation components. Add Card click to href support and re-enable Buttons and Tags in Card.

--- packages/components-core/src/components/Card/index.js
+++ packages/components-core/src/components/Card/index.js
@@ -7,9 +7,8 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import UdsStyles from "../../../vendor/css/bootstrap-asu.min.module.css";
-import { EventLocation } from "../EventLocation";
-// import { UdsButton } from "../UdsButton";
-// import { UdsTagButton } from "../UdsTagButton";
+import { Button } from "../Button";
+import { ButtonTag } from "../ButtonTag";
 
 const sanitizeDangerousMarkup = content => {
   const sanitizer = dompurify.sanitize;
@@ -22,16 +21,132 @@ export const Card = ({
   width,
   horizontal,
   clickable,
+  clickHref,
   image,
   imageAltText,
   title,
-  link,
+  icon,
   body,
   eventFormat,
   eventLocation,
   eventTime,
-  // buttons,
-  // tags,
+  buttons,
+  tags,
+}) => {
+  if (clickable && clickHref) {
+    return (
+      <a href={clickHref}>
+        <BaseCard
+          type={type}
+          width={width}
+          horizontal={horizontal}
+          clickable={clickable}
+          image={image}
+          imageAltText={imageAltText}
+          title={title}
+          icon={icon}
+          body={body}
+          eventFormat={eventFormat}
+          eventLocation={eventLocation}
+          eventTime={eventTime}
+          buttons={buttons}
+          tags={tags}
+        />
+      </a>
+    );
+  }
+  return (
+    <BaseCard
+      type={type}
+      width={width}
+      horizontal={horizontal}
+      clickable={clickable}
+      image={image}
+      imageAltText={imageAltText}
+      title={title}
+      icon={icon}
+      body={body}
+      eventFormat={eventFormat}
+      eventLocation={eventLocation}
+      eventTime={eventTime}
+      buttons={buttons}
+      tags={tags}
+    />
+  );
+};
+
+Card.propTypes = {
+  type: PropTypes.oneOf(["default", "degree", "event", "news", "story"]),
+  width: PropTypes.oneOf(["25%", "50%", "75%", "100%"]),
+  horizontal: PropTypes.bool,
+  clickable: PropTypes.bool,
+  clickHref: PropTypes.string, // card target if clickable
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.elementType, // React Component
+  body: PropTypes.string,
+  eventFormat: PropTypes.oneOf(["stack", "inline"]),
+  eventLocation: PropTypes.string,
+  eventTime: PropTypes.string,
+  image: PropTypes.string,
+  imageAltText: PropTypes.string,
+  buttons: PropTypes.arrayOf(
+    PropTypes.shape({
+      ariaLabel: PropTypes.string,
+      color: PropTypes.oneOf(["gold", "maroon", "gray", "dark"]),
+      icon: PropTypes.elementType,
+      href: PropTypes.string,
+      label: PropTypes.string,
+      onClick: PropTypes.func,
+      size: PropTypes.oneOf(["default", "small", "xsmall"]),
+    })
+  ),
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      ariaLabel: PropTypes.string,
+      color: PropTypes.oneOf(["white", "gray", "dark"]),
+      href: PropTypes.string,
+      label: PropTypes.string,
+      onClick: PropTypes.func,
+    })
+  ),
+};
+
+Card.defaultProps = {
+  type: "default",
+  width: "100%",
+  horizontal: false,
+  clickable: false,
+  clickHref: undefined,
+  body: undefined,
+  eventFormat: "stack",
+  eventTime: undefined,
+  eventLocation: undefined,
+  icon: undefined,
+  image: undefined,
+  imageAltText: undefined,
+  buttons: undefined,
+  tags: undefined,
+};
+
+/*
+ * Sub-components defined after this
+ */
+
+const BaseCard = ({
+  type,
+  width,
+  horizontal,
+  clickable,
+  image,
+  imageAltText,
+  title,
+  icon,
+  body,
+  eventFormat,
+  eventLocation,
+  eventTime,
+  buttons,
+  tags,
 }) => {
   const cardClass = classNames(UdsStyles["card"], {
     [UdsStyles[`card-degree`]]: type === "degree",
@@ -52,8 +167,7 @@ export const Card = ({
             alt={imageAltText}
           />
         )}
-        {/* TODO: implement FA icon
-          {!image && icon} */}
+        {!image && icon && <FontAwesomeIcon icon={icon} />}
         {horizontal ? (
           <div className={UdsStyles["card-content-wrapper"]}>
             <CardContent
@@ -63,9 +177,8 @@ export const Card = ({
               eventLocation={eventLocation}
               eventTime={eventTime}
               title={title}
-              link={link}
-              // buttons={buttons}
-              // tags={tags}
+              buttons={buttons}
+              tags={tags}
             />
           </div>
         ) : (
@@ -76,9 +189,8 @@ export const Card = ({
             eventLocation={eventLocation}
             eventTime={eventTime}
             title={title}
-            link={link}
-            // buttons={buttons}
-            // tags={tags}
+            buttons={buttons}
+            tags={tags}
           />
         )}
       </div>
@@ -86,40 +198,56 @@ export const Card = ({
   );
 };
 
-Card.propTypes = {
+BaseCard.propTypes = {
   type: PropTypes.oneOf(["default", "degree", "event", "news", "story"]),
   width: PropTypes.oneOf(["25%", "50%", "75%", "100%"]),
   horizontal: PropTypes.bool,
   clickable: PropTypes.bool,
   title: PropTypes.string.isRequired,
-  link: PropTypes.string, // TODO: should this be required?
+  icon: PropTypes.elementType, // React Component
   body: PropTypes.string,
   eventFormat: PropTypes.oneOf(["stack", "inline"]),
   eventLocation: PropTypes.string,
   eventTime: PropTypes.string,
-  // icon: PropTypes.elementType, // React Component
   image: PropTypes.string,
   imageAltText: PropTypes.string,
+  buttons: PropTypes.arrayOf(
+    PropTypes.shape({
+      ariaLabel: PropTypes.string,
+      color: PropTypes.oneOf(["gold", "maroon", "gray", "dark"]),
+      icon: PropTypes.elementType,
+      href: PropTypes.string,
+      label: PropTypes.string,
+      onClick: PropTypes.func,
+      size: PropTypes.oneOf(["default", "small", "xsmall"]),
+    })
+  ),
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      ariaLabel: PropTypes.string,
+      color: PropTypes.oneOf(["white", "gray", "dark"]),
+      href: PropTypes.string,
+      label: PropTypes.string,
+      onClick: PropTypes.func,
+    })
+  ),
 };
 
-Card.defaultProps = {
+BaseCard.defaultProps = {
   type: "default",
   width: "100%",
   horizontal: false,
   clickable: false,
-  link: "",
   body: "",
   eventFormat: "stack",
   eventTime: "",
   eventLocation: "",
-  // icon: "",
+  icon: undefined,
   image: "",
   imageAltText: "",
+  buttons: undefined,
+  tags: undefined,
 };
-
-/*
- * Sub-components defined after this
- */
 
 const CardContent = ({
   type,
@@ -128,8 +256,8 @@ const CardContent = ({
   eventLocation,
   eventTime,
   title,
-  // buttons,
-  // tags,
+  buttons,
+  tags,
 }) => (
   <>
     <div className={UdsStyles["card-header"]}>
@@ -145,21 +273,41 @@ const CardContent = ({
         eventLocation={eventLocation}
       />
     )}
-    {/* {buttons &&
-      buttons.map((button, index) => (
-        <div key={index} className={UdsStyles["card-button"]}>
-          <Button color={button.color}>{button.label}</Button>
+    {buttons &&
+      buttons.map(button => (
+        <div
+          key={`${button.label}-${button.href}`}
+          className={UdsStyles["card-button"]}
+        >
+          <Button
+            ariaLabel={button.ariaLabel}
+            color={button.color}
+            icon={button.icon}
+            href={button.href}
+            label={button.label}
+            onClick={button.onClick}
+            size={button.size}
+          >
+            {button.label}
+          </Button>
         </div>
       ))}
     {tags && (
       <div className={UdsStyles["card-tags"]}>
-        {tags.map((tag, index) => (
-          <ButtonTag key={index} color={tag.color}>
+        {tags.map(tag => (
+          <ButtonTag
+            key={`${tag.label}-${tag.href}`}
+            ariaLabel={tag.ariaLabel}
+            color={tag.color}
+            href={tag.href}
+            label={tag.label}
+            onClick={tag.onClick}
+          >
             {tag.label}
           </ButtonTag>
         ))}
       </div>
-    )} */}
+    )}
   </>
 );
 
@@ -170,6 +318,26 @@ CardContent.propTypes = {
   eventLocation: PropTypes.string,
   eventTime: PropTypes.string,
   title: PropTypes.string.isRequired,
+  buttons: PropTypes.arrayOf(
+    PropTypes.shape({
+      ariaLabel: PropTypes.string,
+      color: PropTypes.oneOf(["gold", "maroon", "gray", "dark"]),
+      icon: PropTypes.elementType,
+      href: PropTypes.string,
+      label: PropTypes.string,
+      onClick: PropTypes.func,
+      size: PropTypes.oneOf(["default", "small", "xsmall"]),
+    })
+  ),
+  tags: PropTypes.arrayOf(
+    PropTypes.shape({
+      ariaLabel: PropTypes.string,
+      color: PropTypes.oneOf(["white", "gray", "dark"]),
+      href: PropTypes.string,
+      label: PropTypes.string,
+      onClick: PropTypes.func,
+    })
+  ),
 };
 
 CardContent.defaultProps = {
@@ -178,6 +346,8 @@ CardContent.defaultProps = {
   eventFormat: "stack",
   eventLocation: "",
   eventTime: "",
+  buttons: undefined,
+  tags: undefined,
 };
 
 const EventInfo = ({ eventFormat, eventTime, eventLocation }) => {
@@ -194,14 +364,15 @@ const EventInfo = ({ eventFormat, eventTime, eventLocation }) => {
           <div>
             <FontAwesomeIcon icon={faMapMarkerAlt} />
           </div>
-          <span>
-            <EventLocation title={eventLocation} />
-          </span>
+          <div
+            dangerouslySetInnerHTML={sanitizeDangerousMarkup(eventLocation)}
+          />
         </div>
       </div>
     );
   }
 
+  // else "stacked"
   return (
     <>
       <div className={UdsStyles["card-event-details"]}>
@@ -218,7 +389,9 @@ const EventInfo = ({ eventFormat, eventTime, eventLocation }) => {
             <FontAwesomeIcon icon={faMapMarkerAlt} />
           </div>
           <span>
-            <EventLocation title={eventLocation} />
+            <div
+              dangerouslySetInnerHTML={sanitizeDangerousMarkup(eventLocation)}
+            />
           </span>
         </div>
       </div>
