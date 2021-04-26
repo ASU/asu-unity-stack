d200bc6ea64de6244a6ecc0884b89c54081876e9
Mark Lambe <Mark@MarkLambe.com>
2021-04-26 14:43:42 +0100

    feat(bootstrap4-theme): 🎸 added animated content sections
    ✅ Closes: ACMSD9-19

--- packages/bootstrap4-theme/stories/asucms/animated-content-sections/animated-content-sections.stories.js
+++ packages/bootstrap4-theme/stories/asucms/animated-content-sections/animated-content-sections.stories.js
@@ -1,49 +1,51 @@
 export default { title: 'ASUCMS/Animated Content Sections' };
 
 export const AnimatiedcontentSectionWithCTAs = () => `
-  <div class="container my-5">
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-4 text-white content-section">
-          <div class="image-holder"></div>
-          <div class="content-holder mx-4">
-            <h2>Experience world-class academics</h2>
-            <div class="my-3">
-              As a comprehensive public research university, ASU is committed to providing excellence in education through the Academic Enterprise, and enables the success of each unique student and increases access to higher education for all.
-            </div>
-            <div class="button-area">
-              <!-- START INSERT: Button Component -->
-              <input class="btn btn-gold mb-2" type="submit" value="Learn More">
-              <!-- END INSERT: Button Component -->
-            </div>
+  <div class="animated-content-section my-5 text-white">
+    <div class="content-section my-2 section-1">
+      <div class="image-holder"></div>
+      <div class="content-holder px-4">
+        <h2>Experience world-class academics</h2>
+        <div class="hidden-details">
+          <div class="long-text mt-1 mb-3">
+            As a comprehensive public research university, ASU is committed to providing excellence in education through the Academic Enterprise, and enables the success of each unique student and increases access to higher education for all.
+          </div>
+          <div class="button-area">
+            <!-- START INSERT: Button Component -->
+            <input class="btn btn-gold mb-2" type="submit" value="Learn More">
+            <!-- END INSERT: Button Component -->
           </div>
         </div>
-        <div class="col-4 text-white content-section">
-          <div class="image-holder"></div>
-          <div class="content-holder mx-4">
-            <h2>Experience world-class academics</h2>
-            <div class="my-3">
-              As a comprehensive public research university, ASU is committed to providing excellence in education through the Academic Enterprise, and enables the success of each unique student and increases access to higher education for all.
-            </div>
-            <div class="button-area">
-              <!-- START INSERT: Button Component -->
-              <input class="btn btn-gold mb-2" type="submit" value="Learn More">
-              <!-- END INSERT: Button Component -->
-            </div>
+      </div>
+    </div>
+    <div class="content-section my-2 section-2">
+      <div class="image-holder"></div>
+      <div class="content-holder px-4">
+        <h2>Discovery and innovation that serves the public</h2>
+        <div class="hidden-details">
+          <div class="long-text mt-1 mb-3">
+          As ASU focuses on research and discovery of public value, the Knowledge Enterprise advances research, innovation, strategic partnerships, entrepreneurship, technology transfer, and international development.
+          </div>
+          <div class="button-area">
+            <!-- START INSERT: Button Component -->
+            <input class="btn btn-gold mb-2" type="submit" value="Learn More">
+            <!-- END INSERT: Button Component -->
           </div>
         </div>
-        <div class="col-4 text-white content-section">
-          <div class="image-holder"></div>
-          <div class="content-holder mx-4">
-            <h2>Experience world-class academics</h2>
-            <div class="my-3">
-              As a comprehensive public research university, ASU is committed to providing excellence in education through the Academic Enterprise, and enables the success of each unique student and increases access to higher education for all.
-            </div>
-            <div class="button-area">
-              <!-- START INSERT: Button Component -->
-              <input class="btn btn-gold mb-2" type="submit" value="Learn More">
-              <!-- END INSERT: Button Component -->
-            </div>
+      </div>
+    </div>
+    <div class="content-section my-2 section-3">
+      <div class="image-holder"></div>
+      <div class="content-holder px-4">
+        <h2>Serving all learners at every stage of life</h2>
+        <div class="hidden-details">
+          <div class="long-text mt-1 mb-3">
+          Assuming fundamental responsibility for the communities it serves, ASU's Learning Enterprise aims to serve all learners at every stage of life by providing high quality, accessible and affordable learning opportunities to everyone.
+          </div>
+          <div class="button-area">
+            <!-- START INSERT: Button Component -->
+            <input class="btn btn-gold mb-2" type="submit" value="Learn More">
+            <!-- END INSERT: Button Component -->
           </div>
         </div>
       </div>
