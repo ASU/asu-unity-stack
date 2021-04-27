92c6f86bb946913d09fa8934bb2c4c80cb9f0bae
connercms <connercms@gmail.com>
2021-04-27 13:30:02 -0500

    feat(bs4-theme): add class for img right alignment variation

--- packages/bootstrap4-theme/stories/components/card-and-image/card-and-image.stories.js
+++ packages/bootstrap4-theme/stories/components/card-and-image/card-and-image.stories.js
@@ -19,6 +19,25 @@ export const Default = () => `
 </div>
 `;
 
+export const DefaultWithRightAlignedImage = () => `
+<div class="uds-card-and-image uds-card-and-image-reverse" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+  <div class="uds-card-and-image-container">
+    <div class="card card-centered">
+      <i class="fas fa-rocket fa-2x card-icon-top"></i>
+      <div class="card-header">
+        <h3 class="card-title">Headline goes here</h3>
+      </div>
+      <div class="card-body">
+        Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.
+      </div>
+      <div class="card-button">
+        <a href="#" class="btn btn-dark">Button link here</a>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
 export const Small = () => `
 <div class="uds-card-and-image uds-card-and-image-sm" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
   <div class="uds-card-and-image-container">
