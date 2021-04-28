c89a9487a64960d9ee541e09c32bab63a20db200
connercms <connercms@gmail.com>
2021-04-28 07:44:22 -0500

    feat(bs4-theme): quote image background

--- packages/bootstrap4-theme/stories/components/quote-image-background/quote-image-background.stories.js
+++ packages/bootstrap4-theme/stories/components/quote-image-background/quote-image-background.stories.js
@@ -1,27 +1,78 @@
 export default { title: "Components/Content Sections/Quote On Image Background" }
 
+
 export const Default = () => `
-<div class="container">
-  <div class="uds-quote-image-background-container">
+<div class="uds-quote-image-background" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
 
-    <div class="uds-quote-image-background-image-container">
-      <img src="https://source.unsplash.com/random/1200x750" class="img-fluid" alt="Random image. REPLACE with appropriate alt text for accessibility."></img>
+<div class="uds-quote-image-background-container">
+    <div class="uds-blockquote uds-testimonial accent-gold">
+      <svg title="Open quote" role="presentation" viewBox="0 0 302.87 245.82">
+        <path d="M113.61,245.82H0V164.56q0-49.34,8.69-77.83T40.84,35.58Q64.29,12.95,100.67,0l22.24,46.9q-34,11.33-48.72,31.54T58.63,132.21h55Zm180,0H180V164.56q0-49.74,8.7-78T221,35.58Q244.65,12.95,280.63,0l22.24,46.9q-34,11.33-48.72,31.54t-15.57,53.77h55Z"/>
+      </svg>
+      <blockquote>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+        <div class="citation">
+          <cite class="name">Somebody, probably</cite>
+        </div>
+      </blockquote>
+    </div>
+</div>
+</div>
+`;
+
+export const Small = () => `
+<div class="uds-quote-image-background uds-quote-image-background-sm" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+
+    <div class="uds-blockquote uds-testimonial accent-gold">
+      <svg title="Open quote" role="presentation" viewBox="0 0 302.87 245.82">
+        <path d="M113.61,245.82H0V164.56q0-49.34,8.69-77.83T40.84,35.58Q64.29,12.95,100.67,0l22.24,46.9q-34,11.33-48.72,31.54T58.63,132.21h55Zm180,0H180V164.56q0-49.74,8.7-78T221,35.58Q244.65,12.95,280.63,0l22.24,46.9q-34,11.33-48.72,31.54t-15.57,53.77h55Z"/>
+      </svg>
+      <blockquote>
+        <p>Computers make excellent and efficient servants, but I have no wish to serve under them.</p>
+        <div class="citation">
+          <cite class="name">Spock</cite>
+          <cite class="description">First officer, USS Enterprise</cite>
+        </div>
+      </blockquote>
     </div>
 
-    <div class="uds-quote-image-background-quote-container">
-      <div class="uds-blockquote uds-testimonial accent-gold">
-        <svg title="Open quote" role="presentation" viewBox="0 0 302.87 245.82">
-          <path d="M113.61,245.82H0V164.56q0-49.34,8.69-77.83T40.84,35.58Q64.29,12.95,100.67,0l22.24,46.9q-34,11.33-48.72,31.54T58.63,132.21h55Zm180,0H180V164.56q0-49.74,8.7-78T221,35.58Q244.65,12.95,280.63,0l22.24,46.9q-34,11.33-48.72,31.54t-15.57,53.77h55Z"/>
-        </svg>
-        <blockquote>
-          <p>Computers make excellent and efficient servants, but I have no wish to serve under them.</p>
-          <div class="citation">
-            <cite class="name">Spock</cite>
-            <cite class="description">First officer, USS Enterprise</cite>
-          </div>
-        </blockquote>
-      </div>
+</div>
+`;
+
+export const Medium = () => `
+<div class="uds-quote-image-background uds-quote-image-background-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+
+    <div class="uds-blockquote uds-testimonial accent-gold">
+      <svg title="Open quote" role="presentation" viewBox="0 0 302.87 245.82">
+        <path d="M113.61,245.82H0V164.56q0-49.34,8.69-77.83T40.84,35.58Q64.29,12.95,100.67,0l22.24,46.9q-34,11.33-48.72,31.54T58.63,132.21h55Zm180,0H180V164.56q0-49.74,8.7-78T221,35.58Q244.65,12.95,280.63,0l22.24,46.9q-34,11.33-48.72,31.54t-15.57,53.77h55Z"/>
+      </svg>
+      <blockquote>
+        <p>Computers make excellent and efficient servants, but I have no wish to serve under them.</p>
+        <div class="citation">
+          <cite class="name">Spock</cite>
+          <cite class="description">First officer, USS Enterprise</cite>
+        </div>
+      </blockquote>
     </div>
-  </div>
+
+</div>
+`;
+
+export const Large = () => `
+<div class="uds-quote-image-background uds-quote-image-background-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+
+    <div class="uds-blockquote uds-testimonial accent-gold">
+      <svg title="Open quote" role="presentation" viewBox="0 0 302.87 245.82">
+        <path d="M113.61,245.82H0V164.56q0-49.34,8.69-77.83T40.84,35.58Q64.29,12.95,100.67,0l22.24,46.9q-34,11.33-48.72,31.54T58.63,132.21h55Zm180,0H180V164.56q0-49.74,8.7-78T221,35.58Q244.65,12.95,280.63,0l22.24,46.9q-34,11.33-48.72,31.54t-15.57,53.77h55Z"/>
+      </svg>
+      <blockquote>
+        <p>Computers make excellent and efficient servants, but I have no wish to serve under them.</p>
+        <div class="citation">
+          <cite class="name">Spock</cite>
+          <cite class="description">First officer, USS Enterprise</cite>
+        </div>
+      </blockquote>
+    </div>
+
 </div>
 `;
