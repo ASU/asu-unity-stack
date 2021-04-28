f15d44aab4dacceb37793165d9615b90bc8fbc68
connercms <connercms@gmail.com>
2021-04-28 08:39:18 -0500

    feat(bs4-theme): remove stories, fix sizing

--- packages/bootstrap4-theme/stories/components/card-and-image/card-and-image.stories.js
+++ packages/bootstrap4-theme/stories/components/card-and-image/card-and-image.stories.js
@@ -9,9 +9,7 @@ export const Default = () => `
         <h3 class="card-title">Headline goes here</h3>
       </div>
       <div class="card-body">
-        Body copy goes here. Default component will grow to fit content. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Body copy goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </div>
       <div class="card-button">
         <a href="#" class="btn btn-dark">Button link here</a>
@@ -21,81 +19,4 @@ export const Default = () => `
 </div>
 `;
 
-export const DefaultWithRightAlignedContent = () => `
-<div class="uds-card-and-image uds-card-and-image-reverse" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="uds-card-and-image-container">
-    <div class="card card-centered">
-      <i class="fas fa-rocket fa-2x card-icon-top"></i>
-      <div class="card-header">
-        <h3 class="card-title">Headline goes here</h3>
-      </div>
-      <div class="card-body">
-        Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.
-      </div>
-      <div class="card-button">
-        <a href="#" class="btn btn-dark">Button link here</a>
-      </div>
-    </div>
-  </div>
-</div>
-`;
-
-export const Small = () => `
-<div class="uds-card-and-image uds-card-and-image-sm" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="uds-card-and-image-container">
-    <div class="card card-centered">
-      <i class="fas fa-rocket fa-2x card-icon-top"></i>
-      <div class="card-header">
-        <h3 class="card-title">Headline goes here</h3>
-      </div>
-      <div class="card-body">
-        Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.
-      </div>
-      <div class="card-button">
-        <a href="#" class="btn btn-sm btn-dark">Button link here</a>
-      </div>
-    </div>
-  </div>
-</div>
-`;
-
-
-export const Medium = () => `
-<div class="uds-card-and-image uds-card-and-image-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="uds-card-and-image-container">
-    <div class="card card-centered">
-      <i class="fas fa-rocket fa-2x card-icon-top"></i>
-      <div class="card-header">
-        <h3 class="card-title">Headline goes here</h3>
-      </div>
-      <div class="card-body">
-        Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.
-      </div>
-      <div class="card-button">
-        <a href="#" class="btn btn-dark">Button link here</a>
-      </div>
-    </div>
-  </div>
-</div>
-`;
-
-
-export const Large = () => `
-<div class="uds-card-and-image uds-card-and-image-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="uds-card-and-image-container">
-    <div class="card card-centered">
-      <i class="fas fa-rocket fa-2x card-icon-top"></i>
-      <div class="card-header">
-        <h3 class="card-title">Headline goes here</h3>
-      </div>
-      <div class="card-body">
-        Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.
-      </div>
-      <div class="card-button">
-        <a href="#" class="btn btn-dark">Button link here</a>
-      </div>
-    </div>
-  </div>
-</div>
-`;
 
