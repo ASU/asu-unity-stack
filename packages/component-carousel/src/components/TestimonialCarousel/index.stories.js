92783bdddc631c402a9152cc4e8b8d8fb4e7b4dd
michele dibenedetto <michele.dibe78@gmail.com>
2021-04-28 11:54:19 +0100

    feat(component-carousel): uds-171 -Add new Testimonial Carousel

---
+++ packages/component-carousel/src/components/TestimonialCarousel/index.stories.js
@@ -0,0 +1,131 @@
+// @ts-check
+/** @jsx h */
+import {
+  withKnobs,
+  text,
+  boolean,
+  object,
+  select,
+} from "@storybook/addon-knobs";
+import { h } from "preact";
+
+import { TestimonialCarousel } from ".";
+
+import {
+  testimonialNoImage,
+  testimonialWithImage,
+  testimonialNoImage2,
+  testimonialWithImage2,
+  testimonialNoCitation,
+} from "./examples";
+
+export default {
+  component: TestimonialCarousel,
+  title: "Testimonial Carousel",
+  decorators: [withKnobs],
+};
+
+const maxWidth = "500px";
+const itemColorCombinations = {
+  Gold: ["accent-gold", "with-image"],
+  // Gold_Black_And_White: ["accent-gold", "text-white", "with-image"],
+  Maroon: ["accent-maroon", "with-image"],
+  // eslint-disable-next-line quote-props
+  None: null,
+};
+
+const itemTitleColorCombinations = {
+  "Highlight gold": ["highlight-gold"],
+  "Highlight black": ["highlight-black"],
+  // eslint-disable-next-line quote-props
+  "None": null,
+};
+
+const itemQuoteColorCombinations = {
+  White: ["text-white"],
+  Maroon: ["text-maroon"],
+  None: null,
+};
+
+const GROUP_STYLE = "Styles";
+
+export const TestimonialCarouselDefault = () => (
+  <TestimonialCarousel
+    itemStyle={{
+      itemCssClass: select(
+        "Item Color Combination",
+        itemColorCombinations,
+        itemColorCombinations.Gold,
+        GROUP_STYLE
+      ),
+      itemQuoteContentCssClass: select(
+        "Quote Content Color Combination",
+        itemQuoteColorCombinations,
+        itemQuoteColorCombinations.None,
+        GROUP_STYLE
+      ),
+    }}
+    maxWidth={text("Max Width", maxWidth, GROUP_STYLE)}
+    hasNavButtons={boolean("Has Nav Buttons", true, GROUP_STYLE)}
+    hasPositionIndicators={boolean("Has Indicators", true, GROUP_STYLE)}
+    testimonialItems={object("Testimonials", testimonialWithImage, GROUP_STYLE)}
+  />
+);
+
+export const TestimonialCarouselWithNoImage = () => (
+  <TestimonialCarousel
+    itemStyle={{
+      itemCssClass: select(
+        "Item Color Combination",
+        itemColorCombinations,
+        itemColorCombinations.Gold
+      ),
+    }}
+    maxWidth={text("Max Width", maxWidth)}
+    hasNavButtons={boolean("Has Nav Buttons", true)}
+    hasPositionIndicators={boolean("Has Indicators", true)}
+    testimonialItems={object("Testimonials", testimonialNoImage)}
+  />
+);
+
+export const TestimonialCarouselWithSlider = () => (
+  <TestimonialCarousel
+    maxWidth={text("Max Width", maxWidth)}
+    hasNavButtons={boolean("Has Nav Buttons", true)}
+    hasPositionIndicators={boolean("Has Indicators", false)}
+    testimonialItems={object("Testimonials", testimonialWithImage2)}
+    itemStyle={{
+      itemCssClass: ["with-image", "accent-gold"],
+    }}
+  />
+);
+
+export const TestimonialCarouselWithSliderAndPositionIndicatorDots = () => (
+  <TestimonialCarousel
+    maxWidth={text("Max Width", maxWidth)}
+    hasNavButtons={boolean("Has Nav Buttons", true)}
+    hasPositionIndicators={boolean("Has Indicators", true)}
+    testimonialItems={object("Testimonials", testimonialNoImage2)}
+  />
+);
+
+export const TestimonialCarouselWithNoCitation = () => (
+  <TestimonialCarousel
+    itemStyle={{
+      itemCssClass: select(
+        "Item Color Combination",
+        itemColorCombinations,
+        itemColorCombinations.Gold
+      ),
+      itemTitleCssClass: select(
+        "Title Color Combination",
+        itemTitleColorCombinations,
+        itemTitleColorCombinations["Highlight gold"]
+      ),
+    }}
+    maxWidth={text("Max Width", maxWidth)}
+    hasNavButtons={boolean("Has Nav Buttons", true)}
+    hasPositionIndicators={boolean("Has Indicators", true)}
+    testimonialItems={object("Testimonials", testimonialNoCitation)}
+  />
+);
