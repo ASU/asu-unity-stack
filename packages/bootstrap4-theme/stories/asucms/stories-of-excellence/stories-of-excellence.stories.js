396bc2555a221e622df8347ffaa646439510bf52
Mark Lambe <Mark@MarkLambe.com>
2021-04-28 10:19:23 +0100

    feat(bootstrap4-theme): 🎸 added stories of excellence

---
+++ packages/bootstrap4-theme/stories/asucms/stories-of-excellence/stories-of-excellence.stories.js
@@ -0,0 +1,64 @@
+export default { title: 'ASUCMS/Stories of Excellence' };
+import exampleImage from './example-image.jpg';
+
+export const StoriesOfExcellence = () => `
+  <div class="container stories-of-excellence p-0">
+    <h2><span class="highlight-gold">Stories of excellence</span></h2>
+    <div class="mt-5 row m-0">
+      <div class="excellence-card col-lg-4 col-md-6 col-sm-12 p-0">
+        <div class="image-holder">
+          <img src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/Luminosity-NASAaward.jpg">
+        </div>
+        <div class="content-holder mx-3 p-3">
+          <h4>Students' lunar system lauded by NASA</h4>
+          <p>Students in ASU's Luminosity Lab created a system to explore the moon's shadowy regions and became finalists for a NASA challenge.</p>
+        </div>
+      </div>
+      <div class="excellence-card col-lg-4 col-md-6 col-sm-12 p-0">
+        <div class="image-holder">
+          <img src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/AlexanderSojourney-Marshallscholar-.jpg">
+        </div>
+        <div class="content-holder mx-3 p-3">
+          <h4>ASU grad wins prestigious scholarship</h4>
+          <p>Alexander Sojourney wins the prestigious Marshall Scholarship to study conflict and social justice.</p>
+        </div>
+      </div>
+      <div class="excellence-card col-lg-4 col-md-6 col-sm-12 p-0">
+        <div class="image-holder">
+          <img src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/Biodesign-InnoAward-covid.jpg">
+        </div>
+        <div class="content-holder mx-3 p-3">
+          <h4>Biodesign recognized for innovation</h4>
+          <p>Arizona leaders presented ASU's Biodesign Institute with the Innovator of the Year Award for its rapid COVID-19 response.</p>
+        </div>
+      </div>
+      <div class="excellence-card col-lg-4 col-md-6 col-sm-12 p-0">
+        <div class="image-holder">
+          <img src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/WelcomeBack-spring2021.jpg">
+        </div>
+        <div class="content-holder mx-3 p-3">
+          <h4>Welcome students!</h4>
+          <p>ASU has a full array of services and resources available to help students start the spring semester. Classes start Jan. 11.</p>
+        </div>
+      </div>
+      <div class="excellence-card col-lg-4 col-md-6 col-sm-12 p-0">
+        <div class="image-holder">
+          <img src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/class_0%20(1).jpg">
+        </div>
+        <div class="content-holder mx-3 p-3">
+          <h4>Live well, be well</h4>
+          <p>Stay informed with the latest information about the 2020-21 school year.</p>
+        </div>
+      </div>
+      <div class="excellence-card col-lg-4 col-md-6 col-sm-12 p-0">
+        <div class="image-holder">
+          <img src="https://ux-annual-report.ws.asu.edu/sites/all/libraries/Prototype/images/Hired-graduation.jpg">
+        </div>
+        <div class="content-holder mx-3 p-3">
+          <h4>BofA, ASU partner on work programs</h4>
+          <p>Bank of America is investing $1M in ASU to expand programs that prepare students of color to enter and advance in the job market.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
