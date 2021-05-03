a95c3282591a9ee44cbcd1b4f19e4d773d65c501
connercms <connercms@gmail.com>
2021-05-03 07:45:14 -0500

    feat(bootstrap4-theme): create grid links component

---
+++ packages/bootstrap4-theme/stories/components/grid-links/grid-links.stories.js
@@ -0,0 +1,45 @@
+export default { title: 'Components/Grid Links' };
+
+export const Default = () => `
+<div class="container">
+  <div class="row">
+    <div class="col-6">
+
+      <div class="uds-grid-links">
+        <div>
+          <i class="fa fa-university uds-grid-links-icon"></i>
+          <a href="">First-year student</a>
+        </div>
+        <div>
+          <i class="fa fa-desktop uds-grid-links-icon"></i>
+          Online student
+        </div>
+        <div>
+          <i class="fa fa-lightbulb uds-grid-links-icon"></i>
+          Transfer student
+        </div>
+        <div>
+          <i class="fa fa-user-graduate uds-grid-links-icon"></i>
+          Veteran Student
+        </div>
+        <div>
+          <i class="fa fa-graduation-cap uds-grid-links-icon"></i>
+          Graduate student
+        </div>
+        <div>
+          <i class="fa fa-rocket uds-grid-links-icon"></i>
+          Universal Learner
+        </div>
+        <div>
+          <i class="fa fa-globe-americas uds-grid-links-icon"></i>
+          International student
+        </div>
+        <div>
+          <i class="fa fa-users uds-grid-links-icon"></i>
+          Non-degree student
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`;
