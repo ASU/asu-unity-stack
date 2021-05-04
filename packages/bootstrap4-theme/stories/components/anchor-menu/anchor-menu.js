912038c75863cf432dc2477fc47de89a92464a0e
connercms <connercms@gmail.com>
2021-05-04 08:41:28 -0500

    fix(bootstrap4-theme): anchor menu (quicklinks) changes and fixes

---
+++ packages/bootstrap4-theme/stories/components/anchor-menu/anchor-menu.js
@@ -0,0 +1,65 @@
+window.addEventListener('DOMContentLoaded', function () {
+  const navbar = document.getElementById('uds-anchor-menu');
+  const anchors = navbar.getElementsByClassName('nav-link');
+  const anchorTargets = new Map();
+  const navbarInitialPos = navbar.getBoundingClientRect().top;
+
+  // Cache the anchor target elements by mapping them
+  // as a key / pair so we don't have to
+  // parse the dom on every scroll event
+  anchors.forEach((anchor) => {
+    const targetId = anchor.getAttribute('href').replace('#', '');
+    const target = document.getElementById(targetId);
+    anchorTargets.set(anchor, target);
+  });
+
+  window.onscroll = function () {
+    const positionFromTop =
+      (document.body.scrollTop || document.documentElement.scrollTop) +
+      navbar.offsetHeight;
+
+    if (positionFromTop > navbarInitialPos) {
+      navbar.classList.add('uds-anchor-menu-shrink');
+    } else {
+      navbar.classList.remove('uds-anchor-menu-shrink');
+    }
+
+    for (let [key, value] of anchorTargets) {
+      const offsets = getOffset(value);
+
+      if (
+        positionFromTop > offsets.y &&
+        positionFromTop < offsets.y + value.clientHeight
+      ) {
+        key.classList.add('active');
+      } else {
+        key.classList.remove('active');
+      }
+    }
+  };
+
+  // This code sets active class on anchor on click
+  // anchors.forEach((anchor) => {
+  //   anchor.addEventListener('click', function (e) {
+  //     e.preventDefault();
+
+  //     scrollTo()
+
+  //     const active = document.querySelector('.nav-link.active');
+  //     if (active) active.classList.remove('active');
+
+  //     e.target.classList.add('active');
+  //   });
+  // });
+});
+
+function getOffset(el) {
+  var _x = 0;
+  var _y = 0;
+  while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+    _x += el.offsetLeft - el.scrollLeft;
+    _y += el.offsetTop - el.scrollTop;
+    el = el.offsetParent;
+  }
+  return { y: parseInt(_y), x: parseInt(_x) };
+}
