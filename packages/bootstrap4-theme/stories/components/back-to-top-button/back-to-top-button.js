b95871f8a68647ec4669dd0797b9b92d7a3ee14e
connercms <connercms@gmail.com>
2021-05-05 09:45:08 -0500

    fix(bootstrap4-theme): fix styles, js
    Made button appear only once the user has scrolled beyond 150% vh and begins to scroll back to top.
    Centered button vertically to match new XD requirements.

--- packages/bootstrap4-theme/stories/components/back-to-top-button/back-to-top-button.js
+++ packages/bootstrap4-theme/stories/components/back-to-top-button/back-to-top-button.js
@@ -1,34 +1,54 @@
-document.addEventListener('DOMContentLoaded', function () {
-  window.onscroll = function () {
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    let hasScrolledToNecessaryDepth = false;
     // Safari || Chrome, Firefox, IE, Opera
-    const positionFromTop =
+    let previousScrollPosition =
       document.body.scrollTop || document.documentElement.scrollTop;
 
-    // window.innerHeight * 1.5 = 150% viewport height
-    if (positionFromTop > window.innerHeight * 1.5)
-      document.getElementById('uds-back-to-top').className =
-        'uds-back-to-top-button uds-back-to-top-button-show';
-    else
-      document.getElementById('uds-back-to-top').className =
-        'uds-back-to-top-button';
+    window.onscroll = function () {
+      let currentScrollPosition =
+        document.body.scrollTop || document.documentElement.scrollTop;
+
+      // If back at top, hide the button
+      if (currentScrollPosition === 0) {
+        hasScrolledToNecessaryDepth = false;
+        document.getElementById('uds-back-to-top').className =
+          'uds-back-to-top-button';
+      }
+      // If scrolling past 150% vh, allow button to be displayed once scrolling back up
+      else if (
+        !hasScrolledToNecessaryDepth &&
+        previousScrollPosition > window.innerHeight * 1.5
+      ) {
+        hasScrolledToNecessaryDepth = true;
+      }
+      // If they have scrolled to necessary depth, and are now scrolling back up, show the button
+      else if (
+        hasScrolledToNecessaryDepth &&
+        currentScrollPosition < previousScrollPosition
+      ) {
+        document.getElementById('uds-back-to-top').className =
+          'uds-back-to-top-button uds-back-to-top-button-show';
+      }
+
+      previousScrollPosition = currentScrollPosition;
+    };
+
+    document
+      .getElementById('uds-back-to-top')
+      .addEventListener('click', scrollToTop);
+  });
+
+  const scrollToTop = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+    // We'll also animate that scroll with requestAnimationFrame:
+    // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      // ScrollTo takes an x and a y coordinate.
+      // Increase the '10' value to get a smoother/slower scroll
+      window.scrollTo(0, c - c / 10);
+    }
   };
-
-  document
-    .getElementById('uds-back-to-top')
-    .addEventListener('click', function () {
-      scrollToTop();
-    });
-});
-
-const scrollToTop = () => {
-  const c = document.documentElement.scrollTop || document.body.scrollTop;
-  // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
-  // We'll also animate that scroll with requestAnimationFrame:
-  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
-  if (c > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    // ScrollTo takes an x and a y coordinate.
-    // Increase the '10' value to get a smoother/slower scroll
-    window.scrollTo(0, c - c / 10);
-  }
-};
+})();
