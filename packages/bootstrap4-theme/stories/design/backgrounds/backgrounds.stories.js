06328574299be9e1e3fcc4ec392089ba471ca262
connercms <connercms@gmail.com>
2021-05-07 12:31:11 -0500

    fix(bootstrap4-theme): move background patterns to background story under design

--- packages/bootstrap4-theme/stories/design/backgrounds/backgrounds.stories.js
+++ packages/bootstrap4-theme/stories/design/backgrounds/backgrounds.stories.js
@@ -94,3 +94,18 @@ export const backgroundsScaling = () => `
     </div>
   </div>
 `;
+
+export const PatternDividersAndBackgroundPatterns = () => `
+<div class="container p-5">
+  <h2>
+    <span class="highlight-gold">Pattern dividers and background patterns</span>
+  </h2>
+</div>
+<div style="height: 250px" class="container-fluid bg morse-code-white"></div>
+<div style="height: 250px" class="container-fluid bg morse-code-black"></div>
+<div style="height: 250px" class="container-fluid bg topo-white"></div>
+<div style="height: 250px" class="container-fluid bg topo-black"></div>
+<div style="height: 250px" class="container-fluid bg network-white"></div>
+<div style="height: 250px" class="container-fluid bg network-black"></div>
+`;
+
