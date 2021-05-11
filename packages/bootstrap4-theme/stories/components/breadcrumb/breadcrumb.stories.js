24e81f7aa2a6a9bc3e6758408b46ce82a504220e
Mark Lambe <Mark@MarkLambe.com>
2021-05-11 18:13:14 +0200

    fix(bootstrap4-theme): 🐛 changing breakcrumbs to not need specific classes

--- packages/bootstrap4-theme/stories/components/breadcrumb/breadcrumb.stories.js
+++ packages/bootstrap4-theme/stories/components/breadcrumb/breadcrumb.stories.js
@@ -1,49 +1,41 @@
 export default { title: 'Components/Breadcrumbs' };
 
 export const breadcrumbsAgainstWhiteBackground = () => `
-  <div class="bg-white">
-    <nav aria-label="breadcrumbs">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item"><a href="#">Second Nav Item</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Current Page</li>
-      </ol>
-    </nav>
-  </div>
+  <nav aria-label="breadcrumbs">
+    <ol class="breadcrumb bg-white">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Second Nav Item</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Current Page</li>
+    </ol>
+  </nav>
 `;
 
 export const breadcrumbsAgainstGray1Background = () => `
-  <div class="bg-gray-1">
-    <nav aria-label="breadcrumbs">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item"><a href="#">Second Nav Item</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Current Page</li>
-      </ol>
-    </nav>
-  </div>
+  <nav aria-label="breadcrumbs">
+    <ol class="breadcrumb bg-gray-1">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Second Nav Item</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Current Page</li>
+    </ol>
+  </nav>
 `;
 
 export const breadcrumbsAgainstGray2Background = () => `
-  <div class="bg-gray-2">
-    <nav aria-label="breadcrumbs">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item"><a href="#">Second Nav Item</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Current Page</li>
-      </ol>
-    </nav>
-  </div>
+  <nav aria-label="breadcrumbs">
+    <ol class="breadcrumb bg-gray-2">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Second Nav Item</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Current Page</li>
+    </ol>
+  </nav>
 `;
 
 export const breadcrumbsAgainstGray7Background = () => `
-  <div class="bg-gray-7">
-    <nav aria-label="breadcrumbs">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="#">Home</a></li>
-        <li class="breadcrumb-item"><a href="#">Second Nav Item</a></li>
-        <li class="breadcrumb-item active text-gray-1" aria-current="page">Current Page</li>
-      </ol>
-    </nav>
-  </div>
+  <nav aria-label="breadcrumbs">
+    <ol class="breadcrumb bg-gray-7">
+      <li class="breadcrumb-item"><a href="#">Home</a></li>
+      <li class="breadcrumb-item"><a href="#">Second Nav Item</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Current Page</li>
+    </ol>
+  </nav>
 `;
