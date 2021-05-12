0d56ce2d846e58fe78f107195061333abe762a48
connercms <connercms@gmail.com>
2021-05-12 14:29:30 -0500

    feat(bootstrap4-theme): table component

--- packages/bootstrap4-theme/stories/components/tables/tables.js
+++ packages/bootstrap4-theme/stories/components/tables/tables.js
@@ -1,32 +1,90 @@
 window.addEventListener('DOMContentLoaded', function () {
-  // let currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
-
   const tables = document.getElementsByClassName('uds-fixed-table');
 
   tables.forEach((table) => {
-    // let horizontalScrollPosition = table.scrollLeft;
+    const container = table.getElementsByClassName(
+      'uds-fixed-table-container'
+    )[0];
     const previous = table.getElementsByClassName('scroll-control previous')[0];
-    // const next = table.getElementsByClassName('scroll-control next')[0];
-
-    toggleNextButton(table); // Determine if next button should be displayed on load
-
-    table.onscroll = function () {
-      if (table.scrollLeft === 0) {
-        previous.classList.remove('show');
-      } else {
-        previous.classList.add('show');
-      }
-
-      toggleNextButton(table);
-    };
-  });
-
-  function toggleNextButton(table) {
     const next = table.getElementsByClassName('scroll-control next')[0];
-    if (table.scrollLeft < getComputedStyle(table).width) {
+
+    // If the user leaves the scrollable area, hide the scroll
+    table.addEventListener('mouseleave', function () {
+      previous.classList.remove('show');
       next.classList.remove('show');
-    } else {
-      next.classList.add('show');
+    });
+
+    // If the user mouses over the scrollable area, show the scroll
+    const showScrollWhenHoverTheseElements = container.querySelectorAll(
+      'tr > *:nth-child(n + 2)'
+    );
+
+    // If the user leaves the scrollable area, hide the scroll
+    const hideScrollWhenHoverTheseElements = container.querySelectorAll(
+      'tr > *:first-child'
+    );
+    for (let i = 0; i < showScrollWhenHoverTheseElements.length; i++) {
+      showScrollWhenHoverTheseElements[i].addEventListener(
+        'mouseenter',
+        function () {
+          previous.classList.add('show');
+          next.classList.add('show');
+        }
+      );
     }
-  }
+
+    // If the user leaves the scrollable area, hide the scroll
+    for (let i = 0; i < hideScrollWhenHoverTheseElements.length; i++) {
+      hideScrollWhenHoverTheseElements[i].addEventListener(
+        'mouseenter',
+        function () {
+          previous.classList.remove('show');
+          next.classList.remove('show');
+        }
+      );
+    }
+
+    previous.addEventListener('click', function () {
+      // Scroll can't go beyond it's bounds, so don't need to do checks here (once it hits zero, it won't go lower)
+      container.scrollLeft -= 100;
+    });
+
+    next.addEventListener('click', function () {
+      container.scrollLeft += 100;
+    });
+
+    // Originally thought scroll should show all the time, but only when not scrolled all the way to one direction
+    // ie. if not scrolled all the way to left, show previous button
+    // Will leave this code in case it is relevant later
+
+    // container.onscroll = function () {
+    //   if (container.scrollLeft === 0) {
+    //     previous.classList.remove('show');
+    //   } else {
+    //     previous.classList.add('show');
+    //     /**
+    //      * The first column is sticky and therefore offsets the actual scroll postion.
+    //      * To get the actual scroll position, we need to take what the browser thinks is the scrollLeft,
+    //      * add the fixed sticky column width (315px) to compensate, and then add the width of the scrollable
+    //      * part of the container
+    //      * container.offsetWidth - 315 is like saying
+    //      * "take the whole container and subtract the width of the sticky column to give me the width of the scrollable portion"
+    //      */
+    //     const actualScrollLeft =
+    //       container.scrollLeft + 315 + (container.offsetWidth - 315);
+
+    //     if (actualScrollLeft === container.scrollWidth) {
+    //       next.classList.remove('show');
+    //     } else {
+    //       next.classList.add('show');
+    //     }
+    //   }
+    // };
+
+    // // Also check on DOM loaded to determine if the next button should be displayed initially
+    // const actualScrollLeft =
+    //   container.scrollLeft + 315 + (container.offsetWidth - 315);
+
+    // if (actualScrollLeft !== container.scrollWidth) next.classList.add('show');
+  });
 });
