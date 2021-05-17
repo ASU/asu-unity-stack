2da2574da73fab4a500291a24d9f5aa071abb3e7
Mark Lambe <Mark@MarkLambe.com>
2021-05-17 12:28:21 +0200

    feat: 🎸 change donut graph to only require one percentage var

--- packages/bootstrap4-theme/stories/asucms/charts-and-graphs/donut.stories.js
+++ packages/bootstrap4-theme/stories/asucms/charts-and-graphs/donut.stories.js
@@ -7,7 +7,7 @@ export const Donut = () => `
 
       <div class="uds-charts-and-graphs-container">
         <div class="uds-charts-and-graphs-overlay">
-          <h4>50%</h4>
+          <h4 id="percentage-display"></h4>
           <span>Fact goes here</span>
         </div>
         <canvas id="uds-donut"></canvas>
@@ -18,6 +18,8 @@ export const Donut = () => `
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.2.0/chart.min.js" integrity="sha512-VMsZqo0ar06BMtg0tPsdgRADvl0kDHpTbugCBBrL55KmucH6hP9zWdLIWY//OTfMnzz6xWQRxQqsUFefwHuHyg==" crossorigin="anonymous"></script>
   <script>
     (function () {
+      const GRAPH_PERCENTAGE_COMPLETE = 50;
+      document.getElementById("percentage-display").innerHTML = GRAPH_PERCENTAGE_COMPLETE + '%';
       window.addEventListener('DOMContentLoaded', function () {
         var ctx = document.getElementById('uds-donut');
 
@@ -26,7 +28,7 @@ export const Donut = () => `
           data: {
             datasets: [
               {
-                data: [50, 50],
+                data: [GRAPH_PERCENTAGE_COMPLETE, 100 - GRAPH_PERCENTAGE_COMPLETE],
                 backgroundColor: ['#ffc627', '#fafafa'],
               },
             ],
