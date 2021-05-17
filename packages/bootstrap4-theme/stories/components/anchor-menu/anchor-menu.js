41badb4d233ea829594c42a35f378e81bfb77475
connercms <connercms@gmail.com>
2021-05-17 09:50:42 -0500

    fix(bootstrap4-theme): anchor menu fixes

--- packages/bootstrap4-theme/stories/components/anchor-menu/anchor-menu.js
+++ packages/bootstrap4-theme/stories/components/anchor-menu/anchor-menu.js
@@ -1,15 +1,14 @@
 window.addEventListener('DOMContentLoaded', function () {
+  initializeAnchorMenu();
+});
+
+function initializeAnchorMenu() {
   const navbar = document.getElementById('uds-anchor-menu');
   const anchors = navbar.getElementsByClassName('nav-link');
   const anchorTargets = new Map();
-  const navbarInitialPos = navbar.getBoundingClientRect().top;
-  const globalHeaderHeight = 91;
+  const globalHeader = document.getElementById('asu-header');
 
-  // !!!!!!! getBoundingClientRect gives coordinates of element as it relates to the window AT THE WINDOWS CURRENT POSITION. Need to make any calls to get position of elements from outside
-  // scroll method before it is effected by scroll.
-
-  // Cache the anchor target elements by mapping them
-  // as a key / pair so we don't have to
+  // Cache the anchor target elements by mapping them as a key/pair so don't have to
   // parse the dom on every scroll event
   for (anchor of anchors) {
     const targetId = anchor.getAttribute('href').replace('#', '');
@@ -18,56 +17,46 @@ window.addEventListener('DOMContentLoaded', function () {
   }
 
   window.onscroll = function () {
-    const scrollTop =
-      document.body.scrollTop || document.documentElement.scrollTop;
-    const positionFromTop = scrollTop;
+    // Assume global header will be present
+    const top = globalHeader.offsetHeight;
 
-    if (positionFromTop > navbarInitialPos) {
+    // If the top of the navbar is at or below the window top, apply style to shrink navbar
+    // Actual top height must be compensated, assuming global header is present
+    if (navbar.getBoundingClientRect().top <= top) {
       navbar.classList.add('uds-anchor-menu-shrink');
     } else {
       navbar.classList.remove('uds-anchor-menu-shrink');
     }
 
-    for (let [key, value] of anchorTargets) {
-      const offsets = getOffset(value);
+    for (let [anchor, target] of anchorTargets) {
+      const offsets = navbar.offsetHeight + globalHeader.offsetHeight;
 
+      // If top of screen (compensated for global header height + navbar height) is positioned between the start and end
+      // of content, apply the active style to the anchor targeting that content.
       if (
-        positionFromTop > value.getBoundingClientRect().top &&
-        positionFromTop < value.getBoundingClientRect().top + offsets.y
+        target.getBoundingClientRect().top < offsets &&
+        target.getBoundingClientRect().top + target.offsetHeight > offsets
       ) {
-        key.classList.add('active');
+        anchor.classList.add('active');
       } else {
-        key.classList.remove('active');
+        anchor.classList.remove('active');
       }
     }
   };
 
   // Set click event of anchors
-  for (let [key, value] of anchorTargets) {
-    key.addEventListener('click', function (e) {
+  for (let [anchor, anchorTarget] of anchorTargets) {
+    anchor.addEventListener('click', function (e) {
       e.preventDefault();
-      // Offset scroll by height of navbar and height of global header
-      window.scrollTo(
-        0,
-        value.getBoundingClientRect().top -
-          navbar.offsetHeight -
-          globalHeaderHeight
-      );
 
+      window.scrollTo(0, anchorTarget.getBoundingClientRect().top);
+
+      // Remove active class from other anchor in navbar, and add it to the clicked anchor
       const active = navbar.querySelector('.nav-link.active');
+
       if (active) active.classList.remove('active');
+
       e.target.classList.add('active');
     });
   }
-});
-
-function getOffset(el) {
-  var _x = 0;
-  var _y = 0;
-  while (el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
-    _x += el.offsetLeft - el.scrollLeft;
-    _y += el.offsetTop - el.scrollTop;
-    el = el.offsetParent;
-  }
-  return { y: parseInt(_y), x: parseInt(_x) };
 }
