19c1899896cc78f1b82e5661f6de2aa5eadbd05b
Michael Samuelson <mlsamuel@asu.edu>
2021-05-18 06:44:49 -0700

    chore(app-rfi): add SB and HTML notes and files

--- packages/app-rfi/.storybook/preview.js
+++ packages/app-rfi/.storybook/preview.js
@@ -3,4 +3,5 @@ export const parameters = {
 };
 
 // Import styles for Storybook.
+// TODO See the perhaps better option for this in preview-head.html
 import "/node_modules/@asu-design-system/bootstrap4-theme/dist/css/bootstrap-asu.min.css";
