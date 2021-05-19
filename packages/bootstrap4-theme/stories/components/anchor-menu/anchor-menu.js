e108b62e0ec0b306c8aae5b37c29a15d4be0eee8
connercms <connercms@gmail.com>
2021-05-19 13:36:20 -0500

    fix(bootstrap4-theme): anchor menu fixes

--- packages/bootstrap4-theme/stories/components/anchor-menu/anchor-menu.js
+++ packages/bootstrap4-theme/stories/components/anchor-menu/anchor-menu.js
@@ -7,6 +7,7 @@ function initializeAnchorMenu() {
   const navbar = document.getElementById('uds-anchor-menu');
   const anchors = navbar.getElementsByClassName('nav-link');
   const navbarInitialPosition = navbar.offsetTop;
+  const navbarInitialHeight = navbar.offsetHeight;
   const anchorTargets = new Map();
   let previousScrollPosition = window.scrollY;
 
@@ -19,20 +20,37 @@ function initializeAnchorMenu() {
   }
 
   window.onscroll = function () {
-    if (navbar.getBoundingClientRect().top <= globalHeader.offsetHeight) {
-      navbar.classList.add('uds-anchor-menu-sticky');
-      navbar.style.top = globalHeader.offsetHeight + 'px'; // Can't set this in stylesheet because global header height isn't static
+    const navbarY = navbar.getBoundingClientRect().top;
+    const headerHeight = globalHeader.offsetHeight;
+
+    // If scrolling DOWN
+    if (
+      window.scrollY > previousScrollPosition &&
+      !navbar.classList.contains('uds-anchor-menu-sticky')
+    ) {
+      if (navbarY > 0 && navbarY < headerHeight) {
+        globalHeader.style.top = -(headerHeight - navbarY) + 'px';
+      } else if (navbarY <= 0) {
+        globalHeader.style.top = -globalHeader.offsetHeight + 'px';
+        navbar.classList.add('uds-anchor-menu-sticky');
+      }
     }
-    if (window.scrollY <= navbarInitialPosition) {
+
+    // If scrolling UP
+    if (
+      window.scrollY < previousScrollPosition &&
+      window.scrollY < navbarInitialPosition
+    ) {
       navbar.classList.remove('uds-anchor-menu-sticky');
-      navbar.style.top = null;
+      if (globalHeader.getBoundingClientRect().top < 0) {
+        const offset = globalHeader.getBoundingClientRect().top + navbarY;
+        globalHeader.style.top = (offset < 0 ? offset : 0) + 'px';
+      }
     }
 
     for (let [anchor, target] of anchorTargets) {
-      const offsets = navbar.offsetHeight + globalHeader.offsetHeight;
+      const offsets = navbar.offsetHeight;
 
-      // If top of screen (compensated for global header height + navbar height) is positioned between the start and end
-      // of content, apply the active style to the anchor targeting that content.
       if (
         target.getBoundingClientRect().top < offsets &&
         target.getBoundingClientRect().top + target.offsetHeight > offsets
@@ -49,32 +67,26 @@ function initializeAnchorMenu() {
   // Set click event of anchors
   for (let [anchor, anchorTarget] of anchorTargets) {
     anchor.addEventListener('click', function (e) {
-      e.preventDefault(); // prevent default goto action, need to line up under header and navbar
+      e.preventDefault();
 
-      // OffsetTop + 1 is a hack. If you click the first anchor item, offsetTop is exactly the cutoff to make the navbar sticky, which pops it back into the dom and throws
-      // off positioning. Adding 1 to the offset top makes sure the navbar remains sticky
-      const targetPosition = anchorTarget.offsetTop + 1;
-      window.scrollTo(0, targetPosition);
+      // Compensate for height of navbar so content appears below it
+      let scrollBy =
+        anchorTarget.getBoundingClientRect().top - navbar.offsetHeight;
 
-      // If page has not been scrolled, global header will still be full size. Therefore, navigating to target at this point will not compensate for header size change.
-      // This will cause anchor menu to sticky to the bottom of where global header *was*, before the size change. The global header size change has an animation
-      // duration of .5s, so we need to wait for it to fully shrink before repositioning the anchor menu. Also, since the anchor menu will not have been stickied at this point,
-      // the content will now be covered by the anchor menu, so we must scroll back down some to avoid overlapping the content.
-      // To "fix" this, we need to
-      if (previousScrollPosition === 0) {
-        setTimeout(() => {
-          navbar.style.top = globalHeader.offsetHeight + 'px';
-          setTimeout(() => {
-            window.scrollTo(0, targetPosition - navbar.offsetHeight);
-          }, 200);
-        }, 300);
+      // If window hasn't been scrolled, compensate for header shrinking.
+      const approximateHeaderSize = 65;
+      if (window.scrollY === 0) scrollBy += approximateHeaderSize;
+
+      // If navbar hasn't been stickied yet, that means global header is still in view, so compensate for header height
+      if (!navbar.classList.contains('uds-anchor-menu-sticky')) {
+        if (window.scrollY > 0) scrollBy += 24;
+        scrollBy -= globalHeader.offsetHeight;
       }
 
-      if (previousScrollPosition < navbarInitialPosition) {
-        setTimeout(() => {
-          window.scrollTo(0, targetPosition - navbar.offsetHeight);
-        }, 100);
-      }
+      window.scrollBy({
+        top: scrollBy,
+        behavior: 'smooth',
+      });
 
       // Remove active class from other anchor in navbar, and add it to the clicked anchor
       const active = navbar.querySelector('.nav-link.active');
--- packages/bootstrap4-theme/stories/components/anchor-menu/anchor-menu.stories.js
+++ packages/bootstrap4-theme/stories/components/anchor-menu/anchor-menu.stories.js
@@ -8,6 +8,7 @@ import '../../../src/js/global-header.js';
 
 export const AnchorMenu = () => `
 <div class="container-fluid">
+
   <header id="asu-header" class="fixed-top">
     <div id="wrapper-header-top">
       <div class="container-xl">
@@ -45,12 +46,10 @@ export const AnchorMenu = () => `
         <div class="row">
           <div id="header-main" class="col-12">
             <nav class="navbar navbar-expand-xl" aria-label="Main" >
-
               <a class="navbar-brand" href="#">
                 <img class="vert" src="${vertlogo}" alt="Arizona State University" />
                 <img class="horiz" src="${horizlogo}" alt="Arizona State University" />
               </a>
-
               <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#menubar" aria-controls="menubar" aria-expanded="false" aria-label="Toggle navigation">
                 <span title="Open mobile menu" class="fa fa-bars"></span>
                 <span title="Close mobile menu" class="fa-stack">
@@ -58,22 +57,16 @@ export const AnchorMenu = () => `
                   <i class="fa fa-times fa-stack-1x"></i>
                 </span>
               </button>
-
               <div class="navbar-container">
-
                 <div class="title subdomain-name">University Technology Office</div>
-
                 <div class="collapse navbar-collapse w-100 justify-content-between" id="menubar">
                   <div class="navbar-nav">
-
                     <a class="nav-link nav-link-home active" href="/">
                       <span class="d-xl-none">Home</span>
                       <span title="Home" class="fas fa-fw fa-home"></span>
                     </a>
-
                     <a class="nav-link" href="#">Tour</a>
                     <a class="nav-link" href="#">Research</a>
-
                     <div class="nav-item dropdown">
                       <a class="nav-link" href="#" id="dropdown-one-col" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Dropdown
@@ -87,13 +80,10 @@ export const AnchorMenu = () => `
                         </div>
                       </div>
                     </div>
-
                   </div><!-- end .navbar-nav -->
-
                   <form class="navbar-site-buttons form-inline">
                     <a href="#" class="btn btn-md btn-maroon">Call to action</a>
                   </form>
-
                   <div class="navbar-mobile-footer">
                     <form class="form-inline navbar-mobile-search" action="https://search.asu.edu/search" method="get" name="gs">
                       <input class="form-control" type="search" name="q" aria-label="Search" placeholder="Search ASU">
@@ -114,10 +104,8 @@ export const AnchorMenu = () => `
                       </div>
                     </div>
                   </div>
-
                 </div>
               </div>
-
             </nav>
           </div>
         </div>
@@ -125,32 +113,43 @@ export const AnchorMenu = () => `
     </div>
   </header>
 
+  <div class="uds-hero uds-hero-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+    <div class="container uds-hero-container">
+      <h1 class="heading heading-one col-md-8"><span class="highlight highlight-gold highlight-heading-one">Heading 1</span></h1>
+      <div class="uds-hero-text col-sm-12 col-md-7">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <a href="#" class="btn btn-gold" />Call to action</a>
+      </div>
+    </div>
+  </div>
+
+  <div id="uds-anchor-menu" class="uds-anchor-menu uds-anchor-menu-expanded-lg">
+  <div class="container-xl">
+    <div class="uds-anchor-menu-wrapper">
+      <h4 data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        On This Page: <span class="fas fa-chevron-down"></span>
+      </h4>
+      <div id="collapseExample" class="card card-body collapse">
+        <nav class="nav" aria-label="Same Page">
+          <a class="nav-link" href="#first"><span class="fas fa-link"></span>Anchor item</a>
+          <a class="nav-link" href="#second"><span class="fas fa-link"></span>Anchor item</a>
+          <a class="nav-link" href="#third"><span class="fa fa-link"></span>Anchor item</a>
+          <a class="nav-link" href="#fourth"><span class="fa fa-link"></span>Anchor item</a>
+          <a class="nav-link" href="#fifth"><span class="fa fa-link"></span>Anchor item</a>
+        </nav>
+      </div>
+      </div>
+    </div>
+  </div>
+
   <div class="container">
     <div class="row">
       <div class="col-md-12 pb-5">
-        <div class="my-2">
+        <div class="py-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consectetur lectus nec laoreet elementum. Mauris ut sapien nibh. Aenean consequat pellentesque rutrum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam sollicitudin neque vel risus volutpat, nec hendrerit nisl tempus. Etiam suscipit purus imperdiet, ultrices enim et, maximus lectus. Aenean hendrerit, nibh eget vehicula sollicitudin, nulla tellus accumsan justo, quis gravida sapien quam ultricies magna. Nulla ullamcorper odio id risus pretium, sed ornare turpis luctus. Integer ac semper ex, ac convallis lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In eleifend lobortis lacus sed efficitur. Ut rhoncus consectetur accumsan.
           Nulla massa lorem, maximus eu venenatis in, ultrices sit amet purus. Duis et rutrum mi, id dictum risus. Fusce suscipit metus congue, fermentum sem non, molestie sem. Donec tortor quam, imperdiet vel dui at, finibus vestibulum neque. Nulla facilisi. Sed fermentum vulputate enim, at aliquam diam tincidunt vitae. Nunc eleifend volutpat pellentesque. Aliquam consectetur enim ac lectus dapibus bibendum. Cras eget erat magna. Donec turpis quam, efficitur in leo in, elementum interdum lorem. Duis tempor tempor nulla, at faucibus leo commodo vel. Nunc in tincidunt diam, ac ultrices mauris. Nunc quis vehicula tellus. Mauris vehicula gravida eros, eget facilisis elit viverra faucibus.
           Sed viverra rhoncus erat, eget dignissim orci tincidunt et. Nullam sodales elit eu metus lobortis, ut posuere lectus convallis. Maecenas id lectus id ligula bibendum congue ac ut risus. Integer eget neque volutpat, mollis nisi sed, lobortis mauris. In est metus, rhoncus in tristique nec, blandit sed mauris. Maecenas non risus mauris. Nunc pretium urna sit amet sapien venenatis mollis. Sed facilisis libero nisi, fermentum malesuada magna commodo at. Phasellus sed hendrerit ipsum. Donec ultrices sodales posuere. Proin a finibus mauris, non ornare arcu. Donec eget cursus augue.
           Nulla vel porta massa. Ut hendrerit mauris a suscipit ullamcorper. Donec sagittis tellus non ultrices feugiat. Mauris sodales, eros id vulputate porttitor, nunc augue aliquet tortor, eget mattis turpis mauris nec enim. Proin ac rhoncus lacus. Aliquam venenatis lorem ac arcu commodo tristique id eget dolor. Nam gravida nisi vel purus interdum, faucibus suscipit arcu placerat. Phasellus congue velit et quam elementum, a fermentum velit efficitur. Nunc lobortis, lacus at faucibus vestibulum, mauris nisl facilisis odio, at elementum nisi mauris nec tortor. Nullam commodo pellentesque ante ac porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat neque magna. Suspendisse potenti. Duis dictum ac elit at elementum. In sit amet hendrerit lacus.
-        </div>
-
-        <div id="uds-anchor-menu" class="uds-anchor-menu uds-anchor-menu-expanded-lg">
-          <div class="uds-anchor-menu-wrapper">
-            <h4 data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-              On This Page: <span class="fas fa-chevron-down"></span>
-            </h4>
-            <div class="separator"></div>
-            <div id="collapseExample" class="card card-body collapse">
-              <nav class="nav" aria-label="Same Page">
-                <a class="nav-link" href="#first"><span class="fas fa-link"></span>Anchor item</a>
-                <a class="nav-link" href="#second"><span class="fas fa-link"></span>Anchor item</a>
-                <a class="nav-link" href="#third"><span class="fa fa-link"></span>Anchor item</a>
-                <a class="nav-link" href="#fourth"><span class="fa fa-link"></span>Anchor item</a>
-                <a class="nav-link" href="#fifth"><span class="fa fa-link"></span>Anchor item</a>
-              </nav>
-            </div>
-          </div>
         </div>
 
         <div id="first" class="py-2">
