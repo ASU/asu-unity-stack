b8d1c2e1d51b272e8f1f810a21c4ac2b1f7501d3
connercms <connercms@gmail.com>
2021-05-21 09:04:17 -0500

    feat(component-footer): point webpack entry at the correct location

--- packages/component-footer/webpack/webpack.common.js
+++ packages/component-footer/webpack/webpack.common.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const common = {
   entry: {
-    Footer: "../index.js",
+    Footer: "../src/index.js",
   },
   module: {
     rules: [
