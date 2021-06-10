75b21dbaad6f9f09b76350a01b68ea08167441ad
connercms <connercms@gmail.com>
2021-06-10 15:38:40 -0500

    fix(bootstrap4-theme): fix tooltip accessibility issues

--- packages/bootstrap4-theme/stories/components/tooltips/tooltips.stories.js
+++ packages/bootstrap4-theme/stories/components/tooltips/tooltips.stories.js
@@ -1,73 +1,85 @@
 export default { title: 'Components/Tooltips' };
 
 export const WhiteBackground = () => `
-<div class="background white">
+<div class="bg-white">
 
-  <span class="uds-tooltip uds-tooltip-white">
-    <span tabindex="0" class="fa-stack" aria-describedby="tooltip-001">
-      <i class="fas fa-circle fa-stack-2x"></i>
-      <i class="fas fa-info fa-stack-1x"></i>
-    </span>
+  <div class="uds-tooltip-container">
+    <button tabindex="0" class="uds-tooltip uds-tooltip-white" aria-describedby="tooltip-desc">
+      <span class="fa-stack">
+        <i class="fas fa-circle fa-stack-2x"></i>
+        <i class="fas fa-info fa-stack-1x"></i>
+      </span>
+      <span class="uds-tooltip-visually-hidden">Notifications</span>
+    </button>
 
-    <span id="tooltip-001" class="uds-tooltip-text">
-      <span class="uds-tooltip-text-heading">Lorem ipsum dolor sit amet</span>
+    <div role="tooltip" class="uds-tooltip-description">
+      <span class="uds-tooltip-heading">Lorem ipsum dolor sit amet</span>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sediqua. Ut enim ad mitrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </span>
-  </span>
+    </div>
+  </div>
 
 </div>
 `;
 
-export const Gray1Background = () => `
-<div class="background base-gray">
+export const BaseGrayBackground = () => `
+<div class="bg-gray-1">
 
-  <span class="uds-tooltip uds-tooltip-base-gray">
-    <span tabindex="0" class="fa-stack" aria-describedby="tooltip-002">
-      <i class="fas fa-circle fa-stack-2x"></i>
-      <i class="fas fa-info fa-stack-1x"></i>
-    </span>
+  <div class="uds-tooltip-container">
+    <button tabindex="0" class="uds-tooltip uds-tooltip-gray-1" aria-describedby="tooltip-desc">
+      <span class="fa-stack">
+        <i class="fas fa-circle fa-stack-2x"></i>
+        <i class="fas fa-info fa-stack-1x"></i>
+      </span>
+      <span class="uds-tooltip-visually-hidden">Notifications</span>
+    </button>
 
-    <span id="tooltip-002" class="uds-tooltip-text">
-      <span class="uds-tooltip-text-heading">Lorem ipsum dolor sit amet</span>
+    <div role="tooltip" class="uds-tooltip-description">
+      <span class="uds-tooltip-heading">Lorem ipsum dolor sit amet</span>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sediqua. Ut enim ad mitrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </span>
-  </span>
+    </div>
+  </div>
 
 </div>
 `;
 
-export const Gray2Background = () => `
-<div class="background gray">
+export const GrayBackground = () => `
+<div class="bg-gray">
 
-  <span class="uds-tooltip uds-tooltip-gray">
-    <span tabindex="0" class="fa-stack" aria-describedby="tooltip-003">
-      <i class="fas fa-circle fa-stack-2x"></i>
-      <i class="fas fa-info fa-stack-1x"></i>
-    </span>
+   <div class="uds-tooltip-container">
+    <button tabindex="0" class="uds-tooltip uds-tooltip-gray" aria-describedby="tooltip-desc">
+      <span class="fa-stack">
+        <i class="fas fa-circle fa-stack-2x"></i>
+        <i class="fas fa-info fa-stack-1x"></i>
+      </span>
+      <span class="uds-tooltip-visually-hidden">Notifications</span>
+    </button>
 
-    <span id="tooltip-003" class="uds-tooltip-text">
-      <span class="uds-tooltip-text-heading">Lorem ipsum dolor sit amet</span>
+    <div role="tooltip" class="uds-tooltip-description">
+      <span class="uds-tooltip-heading">Lorem ipsum dolor sit amet</span>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sediqua. Ut enim ad mitrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </span>
-  </span>
+    </div>
+  </div>
 
 </div>
 `;
 
-export const Gray7Background = () => `
-<div class="background dark">
+export const DarkBackground = () => `
+<div class="bg-dark">
 
-  <span class="uds-tooltip uds-tooltip-dark">
-    <span tabindex="0" class="fa-stack" aria-describedby="tooltip-004">
-      <i class="fas fa-circle fa-stack-2x"></i>
-      <i class="fas fa-info fa-stack-1x"></i>
-    </span>
+  <div class="uds-tooltip-container">
+    <button tabindex="0" class="uds-tooltip uds-tooltip-dark" aria-describedby="tooltip-desc">
+      <span class="fa-stack">
+        <i class="fas fa-circle fa-stack-2x"></i>
+        <i class="fas fa-info fa-stack-1x"></i>
+      </span>
+      <span class="uds-tooltip-visually-hidden">Notifications</span>
+    </button>
 
-    <span id="tooltip-004" class="uds-tooltip-text">
-      <span class="uds-tooltip-text-heading">Lorem ipsum dolor sit amet</span>
+    <div role="tooltip" class="uds-tooltip-description">
+      <span class="uds-tooltip-heading">Lorem ipsum dolor sit amet</span>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sediqua. Ut enim ad mitrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-    </span>
-  </span>
+    </div>
+  </div>
 
 </div>
 `;
