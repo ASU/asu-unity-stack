be984ebe366a802dd94bf76808d503ea06dff30a
Mark Lambe <Mark@MarkLambe.com>
2021-06-14 10:20:48 +0100

    fix: 🐛 new markup needed to allow all forms of component

--- packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
+++ packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
@@ -1,36 +1,31 @@
 export default { title: 'Components/Heroes' };
 
 // Random photos sourced from Unsplash: https://source.unsplash.com
-
 export const heroSmall = () => `
-<div class="uds-hero uds-hero-sm" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="container uds-hero-container">
-    <h1 class="heading heading-one col-md-8"><span class="highlight-gold highlight-heading-one">Heading 1</span></h1>
-  </div>
+<div class="uds-hero-sm" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+    <div class="hero-content">
+      <h1 class="heading"><span class="highlight-gold highlight-heading-one">Heading 1</span></h1>
+    </div>
 </div>
 `;
 
 export const heroMedium = () => `
-<div class="uds-hero uds-hero-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="container uds-hero-container">
-    <h1 class="heading heading-one col-md-8"><span class="highlight-gold highlight-heading-one">Heading 1</span></h1>
-    <div class="uds-hero-text col-sm-12 col-md-7">
+<div class="uds-hero-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+    <div class="hero-content">
+      <h1 class="heading"><span class="highlight-gold highlight-heading-one">Heading 1</span></h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <a href="#" class="btn btn-gold" />Call to action</a>
     </div>
-  </div>
 </div>
 `;
 
 export const heroLarge = () => `
-<div class="uds-hero uds-hero-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="container uds-hero-container">
-    <h1 class="heading heading-one col-md-8"><span class="highlight-gold highlight-heading-one">Heading 1</span></h1>
-    <div class="uds-hero-text col-sm-12 col-md-7">
+<div class="uds-hero-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+    <div class="hero-content">
+      <h1 class="heading"><span class="highlight-gold highlight-heading-one">Heading 1</span></h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       <a href="#" class="btn btn-gold" />Call to action</a>
     </div>
-  </div>
 </div>
 `;
 
@@ -79,19 +74,19 @@ export const storyHeroLarge = () => `
 `;
 
 export const heroMediumBlackTitle = () => `
-<div class="uds-hero uds-hero-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="container uds-hero-container">
-    <h3 class="col-md-8"><span class="highlight-black">Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle</span></h3>
-    <h1 class="col-md-8"><span class="highlight-white">Title Title Title Title Title Title</span></h1>
+<div class="uds-hero-md" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+  <div class="hero-content">
+    <h3><span class="highlight-black">Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle</span></h3>
+    <h1><span class="highlight-white">Title Title Title Title Title Title</span></h1>
   </div>
 </div>
 `;
 
 export const heroLargeBlackTitle = () => `
-<div class="uds-hero uds-hero-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
-  <div class="container uds-hero-container">
-    <h3 class="col-md-8"><span class="highlight-black">Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle</span></h3>
-    <h1 class="col-md-8"><span class="highlight-white">Title Title Title Title Title Title</span></h1>
+<div class="uds-hero-lg" style="background-image: linear-gradient(180deg, #19191900 0%, #191919c9 100%), url('https://source.unsplash.com/random/1920x1200');">
+  <div class="hero-content">
+    <h3><span class="highlight-black">Subtitle Subtitle Subtitle Subtitle Subtitle Subtitle</span></h3>
+    <h1><span class="highlight-white">Title Title Title Title Title Title</span></h1>
   </div>
 </div>
 `
