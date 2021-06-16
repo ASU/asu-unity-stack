5e8a30a79cb7a458e3c94c695479faec9361a2e9
Michael Samuelson <mlsamuel@asu.edu>
2021-06-16 12:05:35 -0700

    build: reset to pre-GHA fontawesome sourcing
    We were getting many issues in the ES2 deploy logs related to missing FA icons.

--- packages/bootstrap4-theme/gulpfile.js
+++ packages/bootstrap4-theme/gulpfile.js
@@ -151,7 +151,10 @@ gulp.task('copy-assets', function (done) {
 gulp.task('copy-dist-assets', function (done) {
   // Copy font-awesome to dist/
   gulp
-    .src('./src/fontawesome/**/*')
+    // For GitHub Actions
+    //.src('./src/fontawesome/**/*')
+    // For Jenkins pipeline
+    .src('./src/assets/fontawesome/**/*')
     .pipe(gulp.dest(paths.dist + '/assets/fontawesome'));
 
   // Copy global header js to dist/ for use on monorepo index page
