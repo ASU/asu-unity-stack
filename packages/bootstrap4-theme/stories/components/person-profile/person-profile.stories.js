b283bd3a81cd6a2471cf8c36ff2bcc9cddfcdc4c
nicolasBaudon <nicobaudon01@gmail.com>
2021-06-30 09:49:31 -0300

    fix(bootstrap4-theme): accessibility fixes

--- packages/bootstrap4-theme/stories/components/person-profile/person-profile.stories.js
+++ packages/bootstrap4-theme/stories/components/person-profile/person-profile.stories.js
@@ -11,24 +11,26 @@ storiesOf('Components/Person Profile', module)
     <div class='container-xl'>
       <div class='row'>
         <div class='col pl-0 pr-0'>
-          <div class="uds-person-profile"> <!-- Component Start -->
+          <!-- Component Start -->
+          <div class="uds-person-profile">
             <img class='profile-img' src='https://placeimg.com/300/300/people' alt='John Smith' />
             <div class='person'>
               <h3 class='person-name'>John Smith</h3>
-              <h5 class='person-profession'>Regents Professor</h5>
+              <h4 class='person-profession'>Regents Professor</h4>
               <ul class='person-contact-info'>
-                <li><a href="mailto:email@asu.edu">email@asu.edu</a></li>
-                <li><a href="tel:555-555-5555">555-555-5555</a></li>
-                <li><a href="#">1234 Address St. Tempe, AZ 12345</a></li>
+                <li><a href="mailto:email@asu.edu" aria-label="Email user">email@asu.edu</a></li>
+                <li><a href="tel:555-555-5555" aria-label="Call user">555-555-5555</a></li>
+                <li><a href="#" aria-label="See user address">1234 Address St. Tempe, AZ 12345</a></li>
               </ul>
               <p class='person-description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed ultrices nisl, at vestibulum tortor. Phasellus ut interdum turpis, et dignissim nisi. In sed volutpat lectus. Lorem ipsum dolor sit amet.</p>
               <ul class='person-social-medias'>
-                <li><a href="#"><span class='fab fa-facebook-square'/></a></li>
-                <li><a href="#"><span class='fab fa-linkedin'/></a></li>
-                <li><a href="#"><span class='fab fa-twitter-square'/></a></li>
+                <li><a href="#" aria-label="Go to user Facebook profile"><span class='fab fa-facebook-square'/></a></li>
+                <li><a href="#" aria-label="Go to user Linkedin profile"><span class='fab fa-linkedin'/></a></li>
+                <li><a href="#" aria-label="Go to user Twitter profile"><span class='fab fa-twitter-square'/></a></li>
               </ul>
             </div>
-          </div> <!-- Component End -->
+          </div>
+          <!-- Component End -->
         </div>
       </div>
     </div>
