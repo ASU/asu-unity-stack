d4c1e723295864ea3f9f39ed0e90d63e03e03a5a
nicolasBaudon <nicobaudon01@gmail.com>
2021-07-06 13:16:53 -0300

    feat(bootstrap4-theme): gray 7 background sidebar

--- packages/bootstrap4-theme/stories/components/sidebar/sidebar.stories.js
+++ packages/bootstrap4-theme/stories/components/sidebar/sidebar.stories.js
@@ -6,7 +6,9 @@ storiesOf('Components/Sidebar', module)
     happo: false,
   })
 
-  .add('Sidebar Navigation', () => `
+  .add(
+    'White Background Sidebar Navigation',
+    () => `
 
   <div class="container my-5">
 
@@ -15,6 +17,7 @@ storiesOf('Components/Sidebar', module)
       <div class="col-md-4">
 
         <h3>Lorem ipsum</h3>
+        <!-- Component Start -->
         <nav id="sidebar-left" class="sidebar accordion" aria-label="Secondary">
 
           <a class="nav-link" href="#">Default Link</a>
@@ -71,8 +74,8 @@ storiesOf('Components/Sidebar', module)
           <a class="nav-link" href="#">Default Link</a>
           <a class="nav-link" href="#">Last Link</a>
 
-        </nav><!-- end .sidebar -->
-
+        </nav>
+        <!-- Component End -->
       </div><!-- end .col -->
 
       <div class="col-md-8">
@@ -151,6 +154,156 @@ storiesOf('Components/Sidebar', module)
       </div><!-- end .col -->
     </div><!-- end .row -->
 
-  </div>
+  </div>`
+  )
 
-  `)
+  .add(
+    'Gray 7 Background Sidebar Navigation',
+    () => `
+
+  <div class="container my-5 gray-7-bg">
+
+    <div class="row mt-4">
+      <div class="col-md-12 pb-4"><h1><span class="highlight-gold">Take a look at the sidebar</span></h1></div>
+      <div class="col-md-4">
+
+        <h3 class="text-white">Lorem ipsum</h3>
+        <!-- Component Start -->
+        <nav id="sidebar-left" class="sidebar accordion gray-7-bg" aria-label="Secondary">
+
+          <a class="nav-link" href="#">Default Link</a>
+          <a class="nav-link is-active" href="#">Active Link</a>
+
+          <div class="card card-foldable">
+            <div class="card-header">
+                <a id="cardOne" class="collapsed nav-link" href="#cardBodyOne" data-toggle="collapse" data-target="#cardBodyOne" aria-expanded="false" aria-controls="cardBodyOne">Expandable link sections
+                  <span class="fas fa-chevron-down ml-1"></span>
+                </a>
+            </div><!-- end .card-header -->
+            <div id="cardBodyOne" class="collapse card-body" aria-labelledby="cardOne" data-parent=".sidebar">
+              <a href="#" class="nav-link">A Long Link Total that is two or more lines of text</a>
+              <a href="#" class="nav-link">Another link here</a>
+              <a href="#" class="nav-link is-active">One more link</a>
+            </div><!-- end .card-body -->
+          </div><!-- end .card -->
+
+          <div class="card card-foldable">
+            <div class="card-header">
+                  <a id="cardTwo" class="collapsed nav-link" data-toggle="collapse" href="#cardBodyTwo" role="button" aria-expanded="false" aria-controls="cardBodyTwo">There should only be one open section at a time.
+                    <span class="fas fa-chevron-down ml-1"></span>
+                  </a>
+            </div>
+            <div id="cardBodyTwo" class="collapse card-body" aria-labelledby="cardTwo" data-parent=".sidebar">
+              <a href="#" class="nav-link">Work it harder</a>
+              <a href="#" class="nav-link">Make it better</a>
+              <a href="#" class="nav-link">Do it faster</a>
+              <a href="#" class="nav-link is-active">Makes us stronger</a>
+              <a href="#" class="nav-link">More than ever</a>
+              <a href="#" class="nav-link">Hour after</a>
+              <a href="#" class="nav-link">Our work is</a>
+              <a href="#" class="nav-link">Never over</a>
+            </div><!-- end .card-body -->
+          </div><!-- end .card -->
+
+          <a class="nav-link" href="#">Link between cards</a>
+
+          <div class="card card-foldable">
+            <div class="card-header">
+                  <a id="cardThree" class="collapsed nav-link" data-toggle="collapse" href="#cardBodyThree" role="button" aria-expanded="false" aria-controls="cardBodyThree">There be gold inside
+                  <span class="fas fa-chevron-down ml-1"></span>
+                </a>
+            </div>
+            <div id="cardBodyThree" class="collapse card-body" aria-labelledby="cardThree" data-parent=".sidebar">
+              <a href="#" class="nav-link">Crow's nest square-rigged hands chase</a>
+              <a href="#" class="nav-link">Davy Jones' Locker belaying pin Sail ho mizzen</a>
+              <a href="#" class="nav-link">Lugger to go on account loaded to the gunwalls lad</a>
+              <a href="#" class="nav-link is-active">Skysail fluke overhaul hardtack</a>
+              <a href="#" class="nav-link">Sloop flogging chase guns lee</a>
+            </div><!-- end .card-body -->
+          </div><!-- end .card -->
+
+          <a class="nav-link" href="#">Default Link</a>
+          <a class="nav-link" href="#">Last Link</a>
+
+        </nav>
+        <!-- Component End -->
+      </div><!-- end .col -->
+
+      <div class="col-md-8">
+
+
+        <p class="text-white">This story isn't about the page content itself. Rather, you should focus your attention on the sidebar navigation element to the left.</p>
+        <p class="text-white">Foldable cards can be mixed in with static navigation links to create a section of content which behaves like an accordion and reveals navigation items which are nested two levels deep.</p>
+
+        <h2 class="text-white">Ligula aenean leo porttitor eu conseq</h2>
+
+        <p class="text-white">Lorem ipsum dolor sit amet, consectetuer adipiscing
+        elit. Aenean commodo ligula eget dolor. Aenean massa
+        <strong>strong</strong>. Cum sociis natoque penatibus
+        et magnis dis parturient montes, nascetur ridiculus
+        mus. Donec quam felis, ultricies nec, pellentesque
+        eu, pretium quis, sem. Nulla consequat massa quis
+        enim. Donec pede justo, fringilla vel, aliquet nec,
+        vulputate eget, arcu. In enim justo, rhoncus ut,
+        imperdiet a, venenatis vitae, justo. Nullam dictum
+        felis eu pede <a class="external ext" href="#">link</a>
+        mollis pretium. Integer tincidunt. Cras dapibus.
+        Vivamus elementum semper nisi. Aenean vulputate
+        eleifend tellus. Aenean leo ligula, porttitor eu,
+        consequat vitae, eleifend ac, enim. Aliquam lorem ante,
+        dapibus in, viverra quis, feugiat a, tellus. Phasellus
+        viverra nulla ut metus varius laoreet. Quisque rutrum.
+        Aenean imperdiet. Etiam ultricies nisi vel augue.
+        Curabitur ullamcorper ultricies nisi.</p>
+
+        <p class="text-white">Lorem ipsum dolor sit amet, consectetuer adipiscing
+        elit. Aenean commodo ligula eget dolor. Aenean massa.
+        Cum sociis natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Donec quam felis,
+        ultricies nec, pellentesque eu, pretium quis, sem.</p>
+
+        <h2 class="text-white">Aenean commodo ligula eget dolor aenean massa</h2>
+
+        <p class="text-white">Lorem ipsum dolor sit amet, consectetuer adipiscing
+        elit. Aenean commodo ligula eget dolor. Aenean massa.
+        Cum sociis natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Donec quam felis,
+        ultricies nec, pellentesque eu, pretium quis, sem.</p>
+
+        <ul class="uds-list text-white">
+          <li>Lorem ipsum dolor sit amet consectetuer.</li>
+          <li>Aenean commodo ligula eget dolor.</li>
+          <li>Aenean massa cum sociis natoque penatibus.</li>
+        </ul>
+
+        <p class="text-white">Lorem ipsum dolor sit amet, consectetuer adipiscing
+        elit. Aenean commodo ligula eget dolor. Aenean massa.
+        Cum sociis natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Donec quam felis,
+        ultricies nec, pellentesque eu, pretium quis, sem.</p>
+
+        <blockquote class="text-white">
+        Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit. Aenean commodo ligula eget dolor.
+        Aenean massa <strong>strong</strong>. Cum sociis
+        natoque penatibus et magnis dis parturient montes,
+        nascetur ridiculus mus. Donec quam felis, ultricies
+        nec, pellentesque eu, pretium quis, sem. Nulla consequat
+        massa quis enim. Donec pede justo, fringilla vel,
+        aliquet nec, vulputate eget, arcu. In <em>em</em>
+        enim justo, rhoncus ut, imperdiet a, venenatis vitae,
+        justo. Nullam <a class="external ext" href="#">link</a>
+        dictum felis eu pede mollis pretium.
+        </blockquote>
+
+        <p class="text-white">Lorem ipsum dolor sit amet, consectetuer adipiscing
+        elit. Aenean commodo ligula eget dolor. Aenean massa.
+        Cum sociis natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Donec quam felis,
+        ultricies nec, pellentesque eu, pretium quis, sem.</p>
+
+      </div><!-- end .col -->
+    </div><!-- end .row -->
+
+  </div>`
+  );
