8d457ec9736db32ee24c600c27c95f216708e933
nicolasBaudon <nicobaudon01@gmail.com>
2021-07-06 16:20:37 -0300

    fix(components-library): global header various issues

--- packages/components-library/src/components/NavItem/styles.js
+++ packages/components-library/src/components/NavItem/styles.js
@@ -7,7 +7,7 @@ import { focusStyle, hiddenStyle, breakpointForMin } from "../../theme";
 import { Icon } from "../Icons";
 
 const navItemStyles = breakpoint => css`
-  a{
+  a {
     ${focusStyle}
   }
 
@@ -48,7 +48,6 @@ const navItemStyles = breakpoint => css`
     @media (min-width: ${breakpointForMin(breakpoint)}) {
       > a {
         padding: 0.5rem 0;
-        white-space: nowrap;
 
         :visited {
           color: #191919;
@@ -64,13 +63,14 @@ const navItemStyles = breakpoint => css`
 
   .navbutton {
     margin-top: auto;
+    padding-top: 1rem;
 
     @media (min-width: ${breakpointForMin(breakpoint)}) {
       order: 1;
     }
 
     @media (max-width: ${breakpoint}) {
-      margin-top: 0.5rem;
+      margin-top: 1rem;
     }
   }
 `;
@@ -92,7 +92,6 @@ const NavLink = forwardRef(({ onFocus, children, selected, ...props }, ref) => {
 
 const NavIcon = forwardRef(
   ({ children, onFocus, type, alt, selected, ...props }, ref) => {
-
     return (
       <li class="navicon">
         <a
