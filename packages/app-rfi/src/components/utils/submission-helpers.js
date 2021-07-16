f67ad9cb85da8b5826c79b876c46261f935f691f
Michael Samuelson <mlsamuel@asu.edu>
2021-07-16 13:39:38 -0700

    fix(app-rfi): avoid toISOString() date conversion failures
    ERFI-80

--- packages/app-rfi/src/components/utils/submission-helpers.js
+++ packages/app-rfi/src/components/utils/submission-helpers.js
@@ -33,11 +33,20 @@ export function submissionFormFieldPrep(payload) {
   // artifact. Remove.
   delete output.Email;
 
-  // Can't transform the date to iso value during validation as it breaks type
-  // checking in Yup, so doing it here.
-  output.BirthDate = output.BirthDate
-    ? new Date(output.BirthDate).toISOString()
-    : undefined;
+  // Can't transform the BirthDate to iso value during validation as it breaks
+  // type checking in Yup, so doing it here. Also... Yup.date() lets dates
+  // without divider characters through but toISOString() then chokes,
+  // so in those cases, since it is an optional field, we drop the date from
+  // the payload, considering it bad data. The regex test ensures there's 2
+  // of / or - or . characters in the string. They can be mixed.
+  if (new RegExp(/[.|/|-].{2}/).test(output.BirthDate)) {
+    output.BirthDate = output.BirthDate
+      ? new Date(output.BirthDate).toISOString()
+      : undefined;
+  } else {
+    // Is invalid date in the eyes of toISOString(), so drop.
+    output.BirthDate = undefined;
+  }
 
   return output;
 }
