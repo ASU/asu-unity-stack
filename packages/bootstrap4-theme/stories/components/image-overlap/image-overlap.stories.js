37e5f8605a00099fd61fbaf8b294a6935addae06
Mark Lambe <mark.lambe@deptagency.com>
2021-07-27 14:51:37 +0100

    fix(bootstrap4-theme): 🐛 rework image overlap for greater content allowance

--- packages/bootstrap4-theme/stories/components/image-overlap/image-overlap.stories.js
+++ packages/bootstrap4-theme/stories/components/image-overlap/image-overlap.stories.js
@@ -10,7 +10,7 @@ export const contentImageOverlapRight = () => `
           <div class="content-wrapper">
             <h3>This is the content that goes in the box.</h3>
             <p>Instagram tour operator travel sailing flying package. Territory New York City group discount active lifestyle creditcard insurance wellness kayak guide overnight rural lonely planet.</p>
-            <p>Train luxury Paris recommendations nature France sight seeing. Flexibility Amsterdam maps. Pacific lonely planet private jet national insurance taxi tourist attractions. Budget Pacific guide caravan Barcelona place to stay maps gateway diary tour operator money</p>
+
           </div>
         </div>
         <!-- Component End -->
