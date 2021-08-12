287be90540c48a35135b7d1b849d5a869e853232
Michele Dibenedetto <mdibenedetto.dev@gmail.com>
2021-08-12 11:34:43 +0100

    perf(app-degree-pages):  add style to search button on key enter

--- packages/app-degree-pages/src/components/ListingPage/components/SearchBar/index.js
+++ packages/app-degree-pages/src/components/ListingPage/components/SearchBar/index.js
@@ -1,12 +1,17 @@
 // @ts-check
 import { Button } from "@asu-design-system/components-core";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Section = styled.div`
   label[for="search-field"] {
     margin-bottom: 0;
+  }
+
+  &[data-searching="true"] button {
+    transform: scale(1.05);
+    box-shadow: 0px 0px 0px 2px #ffffff, 0px 0px 0px 4px #191919 !important;
   }
 `;
 
@@ -25,13 +30,16 @@ const Section = styled.div`
  */
 
 const SearchBar = ({ value, onChange, onSearch }) => {
+  const [searching, setSearching] = useState(false);
   return (
-    <Section className="container mt-5">
+    <Section className="container mt-5" data-searching={searching}>
       <form
         className="uds-form p-0 col-md-6 col-sm-12"
         onSubmit={e => {
           e.preventDefault();
           onSearch();
+          setSearching(true);
+          setTimeout(() => setSearching(false), 500);
         }}
       >
         <div className="form-group mb-0 mr-2">
