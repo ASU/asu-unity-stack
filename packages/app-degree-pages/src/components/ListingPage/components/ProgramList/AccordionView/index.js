55912aa0236cd30c8bcad1386830e351bb018b9f
nicolasBaudon <nicobaudon01@gmail.com>
2021-08-13 11:18:58 -0300

    feat(app-degree-pages): accordion view adding fields

--- packages/app-degree-pages/src/components/ListingPage/components/ProgramList/AccordionView/index.js
+++ packages/app-degree-pages/src/components/ListingPage/components/ProgramList/AccordionView/index.js
@@ -8,6 +8,7 @@ import {
   degreeDataPropResolverService,
   parseMajorInfoLink,
 } from "../../../../../core/services";
+import { accellerateDegreeLink } from "../../../../../core/services/degree-http-service";
 import { toTitleCase } from "../../../../../core/utils";
 import { degreeListPropTypes } from "../programs-prop-types";
 
@@ -45,6 +46,36 @@ const AccordionView = ({ programs, actionUrls }) => {
    * */
   const cards = programs.map(row => {
     const resolver = degreeDataPropResolverService(row);
+
+    const getRequiredCourses = () => {
+      const isOnline = resolver.isOnline();
+      const directUrl = isOnline
+        ? resolver.getOnlineMajorMapURL()
+        : resolver.getAsuCritTrackUrl();
+
+      return `<a href=${directUrl}>Major Map</a>`;
+    };
+
+    const getAcceleratedConcurrent = () => `<div>
+        ${
+          resolver.getConcurrentDegrees().lenght > 0
+            ? "<div className='cell-container'>concurrent</div>"
+            : ""
+        }
+        ${
+          resolver.getAccelerateDegrees().length > 0
+            ? `<div className="cell-container">
+                <a href=${accellerateDegreeLink(
+                  resolver,
+                  actionUrls.majorInfoUrl
+                )}>
+                  4+1 years
+                </a>
+              </div>`
+            : ""
+        }
+    </div>`;
+
     return {
       content: {
         header: resolver.getMajorDesc(),
@@ -61,12 +92,21 @@ const AccordionView = ({ programs, actionUrls }) => {
           <br />${resolver.getDegree()}
         </li>
         <li>
+          <strong>Required Courses:</strong>
+          <br />${getRequiredCourses()}
+        </li>
+        <li>
           <strong>Campus or location:</strong>
           <br />${resolver.getCampusList().map(toTitleCase).join(", ")}
         </li>
         <li>
-          <strong>College:</strong>
-          <br />${resolver.getCollegeDesc()}
+          <strong>Accelerated/Concurrent:</strong>
+          <br />${getAcceleratedConcurrent()}
+        </li>
+        <li>
+          <strong>College/School:</strong>
+          <br />
+          <a href=${resolver.getCollegeUrl()}>${resolver.getCollegeDesc()}</a>
         </li>
       </ul>`,
       },
