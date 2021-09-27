c22c8f6463fdcb76e40c5847650a3e84d01f51b4
Mark Lambe <mark.lambe@deptagency.com>
2021-09-27 13:23:15 +0100

    fix(bootstrap4-theme): heroes ios height bug

--- packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
+++ packages/bootstrap4-theme/stories/components/heroes/heroes.stories.js
@@ -3,21 +3,99 @@ import { createComponent, createStory } from '../../../helpers/wrapper.js';
 export default createComponent('Heroes');
 
 export const HeroSmall = createStory(
-  <div class="uds-hero-sm">
+  <div>
+    <div class="uds-hero-sm">
+      <img
+        class="hero"
+        src="https://source.unsplash.com/WeYamle9fDM/3840x1080"
+        alt="Sample placeholder image."
+      />
+      <div role="doc-subtitle">
+        <span class="highlight-white">The New American University</span>
+      </div>
+      <h1>
+        <span class="highlight-black">By whom it includes</span>
+      </h1>
+      <a href="#" class="btn btn-maroon">
+        Maroon Call to Action
+      </a>
+    </div>
+
+
+
+
+    <div class="uds-hero-md has-btn-row">
+      <img
+        class="hero"
+        src="https://source.unsplash.com/IydYk-LGOhc/3840x1080"
+        alt="Sample placeholder image."
+      />
+      <h1>
+        <span class="highlight-gold">Gettysburg Address</span>
+      </h1>
+      <div class="content">
+        <p class="text-white">
+          Four score and seven years ago our fathers brought forth upon this
+          continent, a new nation, conceived in Liberty, and dedicated to the
+          proposition that all men are created equal.
+        </p>
+      </div>
+      <div class="btn-row">
+        <a href="#" class="btn btn-default btn-gold">
+          Read the 13th Amendment
+        </a>
+        <a href="#" class="btn btn-default btn-gold">
+          More quotes from Abraham Lincoln
+        </a>
+      </div>
+    </div>
+
+    <div class="uds-hero-lg hide-content">
     <img
       class="hero"
-      src="https://source.unsplash.com/WeYamle9fDM/1920x256"
+      src="https://source.unsplash.com/xyXcGADvAwE/3840x1080"
       alt="Sample placeholder image."
     />
     <div role="doc-subtitle">
-      <span class="highlight-white">The New American University</span>
+      <span class="highlight-black">Mass Ascension</span>
     </div>
-    <h1>
-      <span class="highlight-black">By whom it includes</span>
-    </h1>
+    <h1 class="text-white">The Albuquerque International Balloon Fiesta</h1>
+    <div class="content">
+      <p class="text-white">
+        For nine days in October, the Albuquerque International Balloon Fiesta
+        creates an enchanted world of vibrant balloon-filled skies. Brisk autumn
+        mornings in the Rio Grande Valley create an otherworldly backdrop for
+        our most popular event, Mass Ascension.
+      </p>
+    </div>
     <a href="#" class="btn btn-maroon">
-      Maroon Call to Action
+      Get your tickets
     </a>
+  </div>
+
+
+  <div class="uds-hero-lg hide-content">
+    <img
+      class="hero"
+      src="https://source.unsplash.com/xyXcGADvAwE/3840x1080"
+      alt="Sample placeholder image."
+    />
+    <div role="doc-subtitle">
+      <span class="highlight-black">Mass Ascension</span>
+    </div>
+    <h1 class="text-white">The Albuquerque International Balloon Fiesta</h1>
+    <div class="content">
+      <p class="text-white">
+        For nine days in October, the Albuquerque International Balloon Fiesta
+        creates an enchanted world of vibrant balloon-filled skies. Brisk autumn
+        mornings in the Rio Grande Valley create an otherworldly backdrop for
+        our most popular event, Mass Ascension.
+      </p>
+    </div>
+    <a href="#" class="btn btn-maroon">
+      Get your tickets
+    </a>
+  </div>
   </div>
 );
 
