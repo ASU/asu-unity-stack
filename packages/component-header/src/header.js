73d4a2b1420193bc861967f0359f737575b9965b
nicolasBaudon <nicobaudon01@gmail.com>
2021-10-08 16:51:01 -0300

    fix: anchor menu with header fixes

--- packages/component-header/src/header.js
+++ packages/component-header/src/header.js
@@ -77,7 +77,7 @@ const ASUHeader = ({
       }}
     >
       {/* @ts-ignore */}
-      <Header ref={headerRef} breakpoint={breakpoint}>
+      <Header id="asuHeader" ref={headerRef} breakpoint={breakpoint}>
         <HeaderMain />
       </Header>
     </AppContextProvider>
