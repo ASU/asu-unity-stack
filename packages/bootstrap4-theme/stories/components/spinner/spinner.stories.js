c60fcb0028d653463dc333f97e094a1cab02dbe9
nicolasBaudon <nicobaudon01@gmail.com>
2021-10-19 16:43:32 -0300

    feat(bootstrap4-theme): rotating spinner

---
+++ packages/bootstrap4-theme/stories/components/spinner/spinner.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createComponent, createStory } from '../../../helpers/wrapper';
+export default createComponent('Spinner');
+
+export const Spinner = createStory(
+  <div class="uds-spinner">
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+    <div class="dot"></div>
+  </div>
+);
