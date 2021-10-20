3449e2283d2eac758b3719df95072ee9d1ceacec
Michele Dibenedetto <mdibenedetto.dev@gmail.com>
2021-10-20 12:25:29 +0100

    docs(component-events): update documentation

--- packages/component-events/src/components/CardsListEvents/index.js
+++ packages/component-events/src/components/CardsListEvents/index.js
@@ -6,19 +6,20 @@ import { eventsPropTypes } from "../../core/models/propTypes";
 import { ListView } from "./ListView";
 
 /**
- *
- * @param {import('../../core/models/types').FeedType} props
- * @returns {JSX.Element}
+ * @typedef {import('../../core/types/events-types').FeedType} FeedType
  */
 
-const CardsListEvents = ({ header, ctaButton, dataSource, maxItems }) => {
-  return (
-    // Calling the high order component that fetch the data
-    <BaseFeed {...{ header, ctaButton, dataSource, maxItems }}>
-      <ListView />
-    </BaseFeed>
-  );
-};
+/**
+ *
+ * @param {FeedType} props
+ * @returns {JSX.Element}
+ */
+const CardsListEvents = ({ header, ctaButton, dataSource, maxItems }) => (
+  // Calling the high order component that fetch the data
+  <BaseFeed {...{ header, ctaButton, dataSource, maxItems }}>
+    <ListView />
+  </BaseFeed>
+);
 
 CardsListEvents.propTypes = eventsPropTypes;
 
---
+++ packages/component-events/src/core/types/events-types.js
@@ -0,0 +1,12 @@
+// ts-check
+
+/**
+ * @typedef {import("../../../../components-core/src/core/types/feed-types").FeedType} FeedType
+ * */
+
+/**
+ * This helps VSCODE and JSOC to recognize the syntax
+ * `import(FILE_PATH).EXPORTED_THING`
+ *  @ignore
+ */
+export const JSDOC = "jsdoc";
