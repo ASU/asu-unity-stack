af7332bf7e5dfd857679e0e69f8a8a5c180467b6
Michael Samuelson <mlsamuel@asu.edu>
2021-10-24 11:30:24 -0700

    fix(bootstrap4-theme): undo disable. didn't work as expected

--- packages/bootstrap4-theme/.storybook/percy/main.js
+++ packages/bootstrap4-theme/.storybook/percy/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: [
-    '../../stories/**/*.DISABLEpercy.mdx',
-    '../../stories/**/*.DISABLEpercy.jsx',
-    '../../stories/**/*.DISABLEpercy.[tj]s',
+    '../../stories/**/*.percy.mdx',
+    '../../stories/**/*.percy.jsx',
+    '../../stories/**/*.percy.[tj]s',
   ],
   addons: [
     '@storybook/addon-docs',
