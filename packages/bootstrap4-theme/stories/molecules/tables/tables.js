181de58d9661e70f2df0f6a61d49e2bde87c6fac
Michele Dibenedetto <mdibenedetto.dev@gmail.com>
2021-10-27 10:48:29 +0100

    fix(bootstrap4-theme): fix style and code clean-up

--- packages/bootstrap4-theme/stories/molecules/tables/tables.js
+++ packages/bootstrap4-theme/stories/molecules/tables/tables.js
@@ -6,69 +6,31 @@
  * Because the table scrolls, if they were to be absolutely positioned in the same container as the table, they would scroll with it.
  */
 window.addEventListener('DOMContentLoaded', function () {
+  console.log('>>> table DOMContentLoaded');
   initializeFixedTable();
 });
 
 function initializeFixedTable() {
-  const tableWrappers = document.getElementsByClassName(
-    'uds-table-fixed-wrapper'
-  );
+  const wrapper = document.querySelector('.uds-table-fixed-wrapper');
 
-  tableWrappers.forEach((wrapper) => {
-    const container = wrapper.getElementsByClassName('uds-table-fixed')[0];
-    const previous = wrapper.getElementsByClassName(
-      'scroll-control previous'
-    )[0];
-    const next = wrapper.getElementsByClassName('scroll-control next')[0];
+  const container = wrapper.querySelector('.uds-table-fixed');
+  const previous = wrapper.querySelector('.scroll-control.previous');
+  const next = wrapper.querySelector('.scroll-control.next');
 
-    // If the user leaves the scrollable area, hide the scroll
-    wrapper.addEventListener('mouseleave', function () {
-      previous.classList.remove('show');
-      next.classList.remove('show');
+  // If the user leaves the scrollable area, hide the scroll
+  wrapper.addEventListener('mouseleave', function () {
+    previous.classList.remove('show');
+    next.classList.remove('show');
+  });
+
+  ['click', 'focus'].forEach((event) => {
+    previous.addEventListener(event, function () {
+      // Scroll can't go beyond it's bounds, so don't need to do checks here (once it hits zero, it won't go lower)
+      container.scrollLeft -= 100;
     });
 
-    // If the user mouses over the scrollable area, show the scroll
-    const showScrollWhenHoverTheseElements = container.querySelectorAll(
-      'tr > *:nth-child(n + 2)'
-    );
-
-    // If the user leaves the scrollable area, hide the scroll
-    const hideScrollWhenHoverTheseElements =
-      container.querySelectorAll('tr > *:first-child');
-    for (let i = 0; i < showScrollWhenHoverTheseElements.length; i++) {
-      ['mouseenter', 'focus', 'hover'].forEach((event) => {
-        showScrollWhenHoverTheseElements[i].addEventListener(
-          event,
-          function () {
-            previous.classList.add('show');
-            next.classList.add('show');
-          }
-        );
-      });
-    }
-
-    // If the user leaves the scrollable area, hide the scroll
-    for (let i = 0; i < hideScrollWhenHoverTheseElements.length; i++) {
-      ['mouseenter', 'focus', 'hover'].forEach((event) => {
-        hideScrollWhenHoverTheseElements[i].addEventListener(
-          event,
-          function () {
-            previous.classList.remove('show');
-            next.classList.remove('show');
-          }
-        );
-      });
-    }
-
-    ['click', 'focus'].forEach((event) => {
-      previous.addEventListener(event, function () {
-        // Scroll can't go beyond it's bounds, so don't need to do checks here (once it hits zero, it won't go lower)
-        container.scrollLeft -= 100;
-      });
-
-      next.addEventListener(event, function () {
-        container.scrollLeft += 100;
-      });
+    next.addEventListener(event, function () {
+      container.scrollLeft += 100;
     });
   });
 }
