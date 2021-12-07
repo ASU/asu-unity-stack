043a0178bb6a2883bf3f4ee6cf4820f1c34ec7d1
nicolasBaudon <nicobaudon01@gmail.com>
2021-12-07 14:17:24 -0300

    fix(component-events): change events header button url

--- packages/component-events/src/core/constants/default-props.js
+++ packages/component-events/src/core/constants/default-props.js
@@ -10,7 +10,7 @@ const defaultProps = {
   ctaButton: {
     color: "gold",
     text: "Click to see more events",
-    url: "https://news.asu.edu",
+    url: "https://asuevents.asu.edu/",
   },
   dataSource: {
     url: "https://asuevents.asu.edu/feed-json/",
