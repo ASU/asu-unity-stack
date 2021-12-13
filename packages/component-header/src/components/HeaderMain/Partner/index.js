eedcef51788bc0fc9d861529f34d66f8c4e3106c
George Baev <geobaev@gmail.com>
2021-12-13 10:33:15 +0200

    WS2-1171: Fix Partner logo ALT attribute.

--- packages/component-header/src/components/HeaderMain/Partner/index.js
+++ packages/component-header/src/components/HeaderMain/Partner/index.js
@@ -24,7 +24,7 @@ const Partner = () => {
       >
         <img
           src={partnerLogo?.src ?? starbucksLogo}
-          alt={partnerLogo?.partnerLogo ?? "Asu Starbucks logo"}
+          alt={partnerLogo?.alt ?? "Asu Starbucks logo"}
         />
       </a>
       <a
