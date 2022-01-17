28c6f643ce0691fcc130c9a825f309b9c0c00905
Mark Lambe <mark.lambe@deptagency.com>
2022-01-17 17:17:16 +0100

    feat(bootstrap4-theme): new profile variants

--- packages/bootstrap4-theme/stories/molecules/person-profile/person-profile.templates.js
+++ packages/bootstrap4-theme/stories/molecules/person-profile/person-profile.templates.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import { createComponent, createStory } from '../../../helpers/wrapper.js';
-export default createComponent('Person Profile', 'Molecules', 'Templates');
 import { googleAnalytics as initFunc } from '../../../src/js/googleAnalytics';
+import { Button } from "../../../../components-core/src/components/Button";
 
-export const PersonProfile = createStory(
-  <div class="uds-person-profile">
+
+const extraOptions = {
+  size: {
+    name: 'Size',
+    options: ['micro-profile', 'small-profile', ''],
+    defaultValue: '',
+    control: {
+      type: 'radio',
+      labels: {
+        'micro-profile': 'Micro',
+        'small-profile': 'Small',
+        '': 'Large',
+      },
+    },
+  },
+  fill: {
+    name: 'Fill',
+    control: { type: 'boolean' },
+    defaultValue: false,
+  },
+};
+
+export default createComponent('Person Profile', 'Molecules', 'Templates', extraOptions);
+
+const PersonProfile = props => (
+  <div className={`uds-person-profile ${props.size} ${props.fill ? 'fill' : ''}`}>
     <img
-      class="profile-img"
+      className="profile-img"
       src="https://source.unsplash.com/WLUHO9A_xik/300x300"
       alt="John Smith"
     />
-    <div class="person">
-      <h3 class="person-name">John Smith</h3>
-      <h4 class="person-profession">Regents Professor</h4>
-      <ul class="person-contact-info">
+    <div className="person">
+      <h3 className="person-name">John Smith</h3>
+      <h4 className="person-profession">Regents Professor</h4>
+      {props.size !== "micro-profile" && (
+      <ul className="person-contact-info">
         <li>
           <a
             href="mailto:email@asu.edu"
@@ -63,62 +88,74 @@ export const PersonProfile = createStory(
           </a>
         </li>
       </ul>
-      <p class="person-description">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-        ultrices nisl, at vestibulum tortor. Phasellus ut interdum turpis, et
-        dignissim nisi. In sed volutpat lectus. Lorem ipsum dolor sit amet.
-      </p>
-      <ul class="person-social-medias">
-        <li>
-          <a
-            href="#"
-            aria-label="Go to user Facebook profile"
-            data-ga-event="link"
-            data-ga-action="click"
-            data-ga-name="onclick"
-            data-ga-type="external link"
-            data-ga-region="main content"
-            data-ga-section="john smith"
-            data-ga="facebook icon"
-          >
-            <span class="fab fa-facebook-square" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            aria-label="Go to user Linkedin profile"
-            data-ga-event="link"
-            data-ga-action="click"
-            data-ga-name="onclick"
-            data-ga-type="external link"
-            data-ga-region="main content"
-            data-ga-section="john smith"
-            data-ga="linkedin icon"
-          >
-            <span class="fab fa-linkedin" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            aria-label="Go to user Twitter profile"
-            data-ga-event="link"
-            data-ga-action="click"
-            data-ga-name="onclick"
-            data-ga-type="external link"
-            data-ga-region="main content"
-            data-ga-section="john smith"
-            data-ga="twitter icon"
-          >
-            <span class="fab fa-twitter-square" />
-          </a>
-        </li>
-      </ul>
+      )}
+      {!["micro-profile", "small-profile"].includes(props.size) && (
+      <div>
+        <p className="person-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
+          ultrices nisl, at vestibulum tortor. Phasellus ut interdum turpis, et
+          dignissim nisi. In sed volutpat lectus. Lorem ipsum dolor sit amet.
+        </p>
+        <ul className="person-social-medias">
+          <li>
+            <a
+              href="#"
+              aria-label="Go to user Facebook profile"
+              data-ga-event="link"
+              data-ga-action="click"
+              data-ga-name="onclick"
+              data-ga-type="external link"
+              data-ga-region="main content"
+              data-ga-section="john smith"
+              data-ga="facebook icon"
+            >
+              <span className="fab fa-facebook-square" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              aria-label="Go to user Linkedin profile"
+              data-ga-event="link"
+              data-ga-action="click"
+              data-ga-name="onclick"
+              data-ga-type="external link"
+              data-ga-region="main content"
+              data-ga-section="john smith"
+              data-ga="linkedin icon"
+            >
+              <span className="fab fa-linkedin" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              aria-label="Go to user Twitter profile"
+              data-ga-event="link"
+              data-ga-action="click"
+              data-ga-name="onclick"
+              data-ga-type="external link"
+              data-ga-region="main content"
+              data-ga-section="john smith"
+              data-ga="twitter icon"
+            >
+              <span className="fab fa-twitter-square" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      )}
+      {props.size === "small" && (
+        <Button color="maroon" size="small" label="View Profile" />
+      )}
+      {props.size === "micro" && (
+        <a href="/asu-knowledge-enterprise">ASU Knowledge Enterprise</a>
+      )}
     </div>
-  </div>,
-  { initFunc }
+  </div>
 );
-PersonProfile.args = {
+
+export const PersonProfileTemplate = createStory(PersonProfile, { initFunc })
+PersonProfileTemplate.args = {
   template: 1,
 };
