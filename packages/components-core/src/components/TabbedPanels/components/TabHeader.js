20a7ed5ea083dee5ad727a91d1a173d8536da7e9
Mark Lambe <mark.lambe@deptagency.com>
2022-02-09 12:25:39 +0100

    feat(components-core): add keyboard accessibility to tabbed panels

--- packages/components-core/src/components/TabbedPanels/components/TabHeader.js
+++ packages/components-core/src/components/TabbedPanels/components/TabHeader.js
@@ -1,7 +1,21 @@
 import PropTypes from "prop-types";
 import React from "react";
 
-const TabHeader = ({ id, selected, title, selectTab }) => {
+const TabHeader = ({
+  id,
+  selected,
+  title,
+  selectTab,
+  leftKeyPressed,
+  rightKeyPressed,
+}) => {
+  const func = e => {
+    if (e.keyCode === 37) {
+      leftKeyPressed();
+    } else if (e.keyCode === 39) {
+      rightKeyPressed();
+    }
+  };
   return (
     <a
       className={`nav-item nav-link ${selected ? "active" : ""}`}
@@ -11,6 +25,8 @@ const TabHeader = ({ id, selected, title, selectTab }) => {
       aria-controls={`nav-${id}`}
       aria-selected={selected}
       onClick={e => selectTab(e, id, title)}
+      onKeyDown={func}
+      tabIndex={selected ? "" : "-1"}
     >
       {title}
     </a>
@@ -22,6 +38,8 @@ TabHeader.propTypes = {
   selected: PropTypes.bool.isRequired,
   title: PropTypes.string.isRequired,
   selectTab: PropTypes.func.isRequired,
+  leftKeyPressed: PropTypes.func.isRequired,
+  rightKeyPressed: PropTypes.func.isRequired,
 };
 
 export { TabHeader };
