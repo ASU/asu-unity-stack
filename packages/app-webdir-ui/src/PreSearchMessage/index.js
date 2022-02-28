34894d15e5fe7c7962156ff69572e154c4190d6c
Mark Lambe <mark.lambe@deptagency.com>
2022-02-28 12:39:16 +0100

    feat(app-webdir-ui): new message

--- packages/app-webdir-ui/src/PreSearchMessage/index.js
+++ packages/app-webdir-ui/src/PreSearchMessage/index.js
@@ -1,28 +1,26 @@
 import React from "react";
 
+import { QuickLinks } from "../QuickLinks";
 import { PreSearchMessageTemplate } from "./index.styles";
 
 const PreSearchMessage = () => {
   return (
     <PreSearchMessageTemplate>
       <div className="message-content">
-        <h2>Welcome to iSearch</h2>
+        <h2>Welcome to ASU search</h2>
         <div>
-          ASU’s Google-based search tool allows you to find any existing,
-          accessible page on Arizona State University web sites. Enter a word or
-          phrase into the box above and press the enter or return key to find
-          web pages, people, and departments.
+          <div>
+            Find the information you're looking for from anywhere on <a href="http://asu.edu/">asu.edu.</a>
+          </div>
+          <div className="assisstance-area">
+            <span className="intro-highlight">Need assistance?</span> <br />
+            <a href="https://www.asu.edu/about/contact">
+              Contact us via live chat, phone or email 24 hours a day, seven days a week.
+            </a>
+          </div>
         </div>
       </div>
-      <div className="quick-links">
-        <span>Quick links</span>
-        <a href="https://www.asu.edu/">MyASU</a>
-        <a href="https://www.asu.edu/">ASU directory</a>
-        <a href="https://www.asu.edu/">ASU maps</a>
-        <a href="https://www.asu.edu/">ASU course catalog</a>
-        <a href="https://www.asu.edu/">ASU helpdesk</a>
-        <a href="https://www.asu.edu/">ASU home</a>
-      </div>
+      <QuickLinks />
     </PreSearchMessageTemplate>
   );
 };
