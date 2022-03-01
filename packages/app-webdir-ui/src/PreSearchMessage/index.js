81708f56ec741458eedc3daf46b1129fbc2f14c3
Mark Lambe <mark.lambe@deptagency.com>
2022-03-01 09:45:48 +0000

    feat(app-webdir-ui): new welcome message

--- packages/app-webdir-ui/src/PreSearchMessage/index.js
+++ packages/app-webdir-ui/src/PreSearchMessage/index.js
@@ -4,19 +4,22 @@ import { QuickLinks } from "../QuickLinks";
 import { PreSearchMessageTemplate } from "./index.styles";
 
 const PreSearchMessage = () => {
+  const titleText = "Welcome to ASU search";
+  const descText = "Find the information you're looking for from anywhere on ";
+  const contactText =
+    "Contact us via live chat, phone or email 24 hours a day, seven days a week.";
   return (
     <PreSearchMessageTemplate>
       <div className="message-content">
-        <h2>Welcome to ASU search</h2>
+        <h2>{titleText}</h2>
         <div>
           <div>
-            Find the information you're looking for from anywhere on <a href="http://asu.edu/">asu.edu.</a>
+            {descText}
+            <a href="http://asu.edu/">asu.edu.</a>
           </div>
           <div className="assisstance-area">
             <span className="intro-highlight">Need assistance?</span> <br />
-            <a href="https://www.asu.edu/about/contact">
-              Contact us via live chat, phone or email 24 hours a day, seven days a week.
-            </a>
+            <a href="https://www.asu.edu/about/contact">{contactText}</a>
           </div>
         </div>
       </div>
