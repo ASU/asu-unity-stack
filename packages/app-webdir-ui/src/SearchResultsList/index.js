76c817373f4b08696cae3ef96d28f68907b981aa
Dave Ornelas <davidornelas_11@hotmail.com>
2022-05-23 18:00:41 -0700

    fix(app-webdir-ui/searchresultslist): profiles per page option now reloads when option changes

--- packages/app-webdir-ui/src/SearchResultsList/index.js
+++ packages/app-webdir-ui/src/SearchResultsList/index.js
@@ -109,7 +109,7 @@ const ASUSearchResultsList = ({
 
   useEffect(() => {
     doSearch();
-  }, [term, sort, filters]);
+  }, [term, sort, filters, itemsPerPage]);
 
   function expandClick(text) {
     trackGAEvent({
