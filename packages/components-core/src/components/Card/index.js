c6db66bf4231acbd1daaff2ae304de32061c8f32
Mayco Barale <maycobarale@gmail.com>
2023-01-20 16:46:56 -0300

    feat(components-core): add feature to disable borders on cards

--- packages/components-core/src/components/Card/index.js
+++ packages/components-core/src/components/Card/index.js
@@ -42,6 +42,7 @@ export const Card = ({
   linkLabel,
   linkUrl,
   tags,
+  bordered,
 }) => {
   return (
     <BaseCard
@@ -60,6 +61,7 @@ export const Card = ({
       linkLabel={linkLabel}
       linkUrl={linkUrl}
       tags={tags}
+      bordered={bordered}
     />
   );
 };
@@ -138,6 +140,10 @@ Card.propTypes = {
       onClick: PropTypes.func,
     })
   ),
+  /**
+   * Remove card borders
+   */
+  bordered: PropTypes.bool,
 };
 
 Card.defaultProps = {
@@ -155,6 +161,7 @@ Card.defaultProps = {
   linkLabel: undefined,
   linkUrl: undefined,
   tags: undefined,
+  bordered: true,
 };
 
 /*
@@ -176,6 +183,7 @@ const BaseCard = ({
   linkLabel,
   linkUrl,
   tags,
+  bordered,
 }) => {
   const cardClass = classNames("card", "cards-components", {
     [`card-degree`]: type === "degree",
@@ -183,6 +191,7 @@ const BaseCard = ({
     [`card-story`]: type === "story",
     [`w-${width.replace("%", "")}`]: width !== "100%",
     [`card-horizontal`]: horizontal,
+    [`borderless`]: !bordered,
   });
 
   return (
@@ -271,6 +280,7 @@ BaseCard.propTypes = {
       onClick: PropTypes.func,
     })
   ),
+  bordered: PropTypes.bool,
 };
 
 BaseCard.defaultProps = {
@@ -288,6 +298,7 @@ BaseCard.defaultProps = {
   linkLabel: undefined,
   linkUrl: undefined,
   tags: undefined,
+  bordered: false,
 };
 
 const CardContent = ({
