e183f748db7c044b8ba18ca369a84bc80a20f10a
Scott Williams <stwilli2@asu.edu>
2023-03-30 11:52:01 -0700

    build(unity-bootstrap-theme): still needs work

---
+++ packages/unity-bootstrap-theme/src/js/index.js
@@ -0,0 +1,5 @@
+const test = {
+  NODE_ENV: process.env.NODE_ENV,
+  BUNDLE_NAME: process.env.BUNDLE_NAME,
+  BUNDLE_VERSION: process.env.BUNDLE_VERSION
+}
--- packages/unity-bootstrap-theme/webpack.config.js
+++ packages/unity-bootstrap-theme/webpack.config.js
@@ -1,103 +1,132 @@
-const autoprefixer = require('autoprefixer');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const ESLintPlugin = require('eslint-webpack-plugin');
+const webpack = require("webpack");
+const autoprefixer = require("autoprefixer");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 // const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
-const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 const nodeExternals = require("webpack-node-externals");
 
-const isDev = false;
+const pkg = require("./package.json");
+
+const env = process.env.NODE_ENV || "development";
+const mode = env === "production" ? "production" : "development";
 
 const paths = {
-  js: path.resolve(__dirname, 'src/js'),
-  css: path.resolve(__dirname, 'dist/css'),
-  img: path.resolve(__dirname, 'dist/img'),
-  imgsrc: path.resolve(__dirname, 'src/img'),
-  sass: path.resolve(__dirname, 'src/scss'),
+  js: path.resolve(__dirname, "src/js"),
+  css: path.resolve(__dirname, "dist/css"),
+  img: path.resolve(__dirname, "dist/img"),
+  imgsrc: path.resolve(__dirname, "src/img"),
+  sass: path.resolve(__dirname, "src/scss"),
 };
 
-module.exports = {
-  mode: isDev ? 'development' : 'production',
+const devtool = "source-map";
+
+/* */
+const jsConfig = {
+  name: "JS",
+  mode,
+  devtool,
   entry: {
-    'bootstrap-asu': path.resolve(paths.js, 'index.js'),
+    "bootstrap-asu": [
+      path.resolve(paths.js, "index.js"),
+      path.resolve(paths.js, "googleAnalytics.js"),
+    ],
   },
-  externalsPresets: { node: true },
-  externals: [nodeExternals(), 'commonjs sharp'],
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: '[name].js',
-    library: 'bootstrapASU',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
-    globalObject: 'this',
+    path: path.resolve(__dirname, "dist/js"),
+    filename: "[name].js",
+    library: "bootstrapASU",
+    libraryTarget: "umd",
+    libraryExport: "default",
+    globalObject: "this",
     pathinfo: true,
   },
-  devtool: false,
-  resolve: {
-    extensions: ['.js'],
-  },
+  externalsPresets: { node: true },
+  externals: [nodeExternals(), "commonjs sharp"],
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: env,
+      BUNDLE_NAME: pkg.name.substr(1),
+      BUNDLE_VERSION: pkg.version,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: paths.imgsrc, to: paths.img }],
+    }),
+  ].filter(Boolean),
   optimization: {
-    minimize: !isDev,
+    minimize: env === "production",
     minimizer: [
-      new CssMinimizerPlugin(),
       new TerserPlugin({
-        parallel: true,
-        extractComments: true,
         terserOptions: {
-          compress: {
-            drop_console: true,
-            drop_debugger: true,
-          },
+          warnings: true,
         },
       }),
     ],
   },
+};
+
+const cssConfig = {
+  name: "CSS and Images",
+  mode,
+  devtool,
+  entry: [path.resolve(paths.sass, "unity-bootstrap-theme.scss")],
+  output: {
+    path: path.resolve(__dirname, "./.tmp"),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: path.join("..", "dist", "css", "unity-bootstrap-theme.css"),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: paths.imgsrc, to: paths.img }],
+    }),
+    // new webpack.LoaderOptionsPlugin({
+    //   options: {
+    //     postcss: [
+    //       autoprefixer()
+    //     ]
+    //   }
+    // }),
+  ].filter(Boolean),
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: paths.js,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        include: paths.sass,
+        test: /\.s?css$/,
         use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
+          { loader: MiniCssExtractPlugin.loader },
+          {
+            loader: "css-loader",
+            options: {
+              // importLoaders: 1,
+              // sourceMap: true
+            },
+          },
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     postcssOptions: {
+          //       plugins: [ 'autoprefixer']
+          //     },
+          //     sourceMap: true
+          //   }
+          // },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          /* There is an issue with Sass changing image urls
+            .bg.morse-code-white {
+              background-image: url(../../.tmp/1fbe75652170f51c1e1d.png);
+            }
+          */
         ],
       },
-      // {
-      //   test: /\.(png|jpe?g|gif)$/i,
-      //   include: paths.imgsrc,
-      //   type: 'asset/resource',
-      //   generator: {
-      //     filename: 'img/[name][ext]',
-      //   },
-      //   use: [
-      //     ImageMinimizerPlugin()
-      //   ],
-      // },
-    ],
+    ].filter(Boolean),
   },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        postcss: [
-          autoprefixer()
-        ]
-      }
-    }),
-    new MiniCssExtractPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [
-          { from: paths.imgsrc, to: paths.img }
-      ]
-  })
-  ],
 };
+module.exports = [jsConfig, cssConfig];
