d4be61a906c332e0dd99dccdef67ac02f2033371
david ornelas <davidornelas_11@hotmail.com>
2023-04-18 01:17:07 -0700

    fix(unity-bootstrap-theme): added container within navbar in examples

--- packages/unity-bootstrap-theme/stories/organisms/global-header/global-header.templates.stories.js
+++ packages/unity-bootstrap-theme/stories/organisms/global-header/global-header.templates.stories.js
@@ -100,6 +100,7 @@ export const Basic = (
           <div class="row">
             <div id="header-main" class="col-12">
               <div class="navbar navbar-expand-xl">
+                <div class="container-fluid">
                 <a class="navbar-brand" href="#" data-ga-header="asu logo">
                   <img
                     class="vert"
@@ -321,6 +322,7 @@ export const Basic = (
                     </div>
                   </div>
                 </div>
+                </div>
               </div>
             </div>
           </div>
@@ -433,6 +435,7 @@ export const DropDownMenus = (
           <div class="row">
             <div id="header-main" class="col-12">
               <div class="navbar navbar-expand-xl">
+                <div class="container-fluid">
                 <a class="navbar-brand" href="#" data-ga-header="asu logo">
                   <img
                     class="vert"
@@ -1413,6 +1416,7 @@ export const DropDownMenus = (
                     </div>
                   </div>
                 </div>
+                </div>
               </div>
             </div>
           </div>
@@ -1524,6 +1528,7 @@ export const NoNavigation = (
           <div class="row">
             <div id="header-main" class="col-12">
               <div class="navbar navbar-expand-xl">
+                <div class="container-fluid">
                 <a class="navbar-brand" href="#" data-ga-header="asu logo">
                   <img
                     class="vert"
@@ -1651,6 +1656,7 @@ export const NoNavigation = (
                     </div>
                   </div>
                 </div>
+                </div>
               </div>
             </div>
           </div>
@@ -1759,6 +1765,7 @@ export const NoNavigationAndWithButtons = (
           <div class="row">
             <div id="header-main" class="col-12">
               <div class="navbar navbar-expand-xl">
+                <div class="container-fluid">
                 <a class="navbar-brand" href="#" data-ga-header="asu logo">
                   <img
                     class="vert"
@@ -1904,6 +1911,7 @@ export const NoNavigationAndWithButtons = (
                     </div>
                   </div>
                 </div>
+                </div>
               </div>
             </div>
           </div>
@@ -2015,6 +2023,7 @@ export const ScrolledState = (
           <div class="row">
             <div id="header-main" class="col-12">
               <div class="navbar navbar-expand-xl">
+                <div class="container-fluid">
                 <a class="navbar-brand" href="#" asu-ga="asu logo">
                   <img
                     class="vert"
@@ -2340,6 +2349,7 @@ export const ScrolledState = (
                       </div>
                     </div>
                   </div>
+                </div>
                 </div>
               </div>
             </div>
