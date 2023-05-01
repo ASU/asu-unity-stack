ded73449c032dc43a23ff1211ee18b5cb36c0774
Scott Williams <stwilli2@asu.edu>
2023-05-01 10:56:09 -0700

    build(component-header): fix build-storybook

--- packages/component-header/.storybook/main.js
+++ packages/component-header/.storybook/main.js
@@ -3,6 +3,11 @@ const PROJECT_DIR = path.resolve(__dirname, "../");
 const common = require("../webpack/webpack.common");
 
 module.exports = {
+  addons: [
+    "@storybook/addon-controls",
+    "@storybook/addon-viewport",
+    "@storybook/addon-a11y",
+  ],
   stories: ["../src/*.stories.js"],
   framework: "@storybook/react",
   core: {
