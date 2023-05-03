addf1d283ba4b4ddc2043422130d56e808e8350c
Scott Williams <stwilli2@asu.edu>
2023-05-03 10:50:01 -0700

    chore(storybook): fix storybook

--- packages/component-footer/.storybook/main.js
+++ packages/component-footer/.storybook/main.js
@@ -13,6 +13,18 @@ module.exports = {
     builder: "webpack5",
   },
   webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        { loader: "css-loader", options: { importLoaders: 1 } },
+        {
+          loader: "sass-loader",
+          options: {},
+        },
+      ],
+    });
+
     return {
       ...config,
       resolve: {
