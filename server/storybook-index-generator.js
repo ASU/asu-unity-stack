02c4cca5b12b51194036e8496d0992e3f45acbf7
david ornelas <davidornelas_11@hotmail.com>
2023-05-09 14:18:06 -0700

    fix: added new css in storybook index page

--- server/storybook-index-generator.js
+++ server/storybook-index-generator.js
@@ -17,7 +17,9 @@ module.exports = generateHTML = packages => `
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="./@asu/bootstrap4-theme/css/bootstrap-asu.css">
+    <link rel="stylesheet" href="./@asu/unity-bootstrap-theme/css/unity-bootstrap-theme.css">
+    <link rel="stylesheet" href="./@asu/unity-bootstrap-theme/css/unity-bootstrap-header.css">
+    <link rel="stylesheet" href="./@asu/unity-bootstrap-theme/css/unity-bootstrap-footer.css">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
     <script type="text/javascript" src="./@asu/bootstrap4-theme/js/bootstrap.min.js"></script>
