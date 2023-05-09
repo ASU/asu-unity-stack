75c9c82660e9f583d4ca70712609e509d2f7a396
Scott Williams <stwilli2@asu.edu>
2023-05-08 18:38:00 -0700

    fix(unity-bootstrap-theme):sort storybook

--- packages/unity-bootstrap-theme/.storybook/preview.js
+++ packages/unity-bootstrap-theme/.storybook/preview.js
@@ -5,6 +5,39 @@ import "../../../node_modules/@fortawesome/fontawesome-free/scss/fontawesome.scs
 import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 
 export const parameters = {
+  options: {
+    storySort: {
+      order: [
+        'Design',
+        [
+          'Colors',
+          'Typography',
+          'Layout',
+          'Icons',
+          'Backgrounds',
+          'Focus States',
+        ],
+        'Atoms',
+        'Molecules',
+        'Organisms',
+        'Components',
+        'Docs',
+        [
+          'Global Header',
+          [
+            'Header top',
+            'Header main',
+            'Navbar options',
+            'No navigation',
+            'Additional considerations',
+          ],
+          'Content Sections',
+          'Hero',
+        ],
+      ],
+    },
+  },
+  layout: 'fullscreen',
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
