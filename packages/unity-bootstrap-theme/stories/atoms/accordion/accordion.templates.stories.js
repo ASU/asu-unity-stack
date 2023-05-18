83e4d4853fdea73b8f806a299dbf3636d6334e9f
mlsamuelson <mlsamuel@asu.edu>
2023-05-18 14:04:53 -0700

    fix(unity-bootstrap-theme): add missing foldable card accordion wrapper

--- packages/unity-bootstrap-theme/stories/atoms/accordion/accordion.templates.stories.js
+++ packages/unity-bootstrap-theme/stories/atoms/accordion/accordion.templates.stories.js
@@ -7,52 +7,54 @@ const FoldableCardElement = (
   linkID = null,
   bodyID = null
 ) => (
-  <div className="card accordion-item">
-    <div className="accordion-header">
-      <h3>
-        <a
-          id={`${linkID}`}
-          className="collapsed"
-          data-bs-toggle="collapse"
-          data-bs-target={bodyID ? `#${bodyID}` : ""}
-          href={bodyID ? `#${bodyID}` : ""}
-          role="button"
-          aria-expanded="false"
-          aria-controls={`${bodyID}`}
-          data-ga="This card unfolds"
-          data-ga-name="onclick"
-          data-ga-event="collapse"
-          data-ga-type="click"
-          data-ga-region="main content"
-          data-ga-section="default"
-        >
-          This card unfolds.
-          <span className="fas fa-chevron-up"></span>
-        </a>
-      </h3>
-    </div>
-    <div
-      id={`${bodyID}`}
-      className="collapse accordion-body"
-      aria-labelledby={`${linkID}`}
-      data-bs-parent={accordionID ? `#${accordionID}` : ""}
-    >
-      <h4>This is a quaternary headline</h4>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud
-      </p>
-      <h5>This is a level five headline. There's a fancy word for that too.</h5>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor
-      </p>
+  <div className="accordion">
+    <div className="card accordion-item">
+      <div className="accordion-header">
+        <h3>
+          <a
+            id={`${linkID}`}
+            className="collapsed"
+            data-bs-toggle="collapse"
+            data-bs-target={bodyID ? `#${bodyID}` : ""}
+            href={bodyID ? `#${bodyID}` : ""}
+            role="button"
+            aria-expanded="false"
+            aria-controls={`${bodyID}`}
+            data-ga="This card unfolds"
+            data-ga-name="onclick"
+            data-ga-event="collapse"
+            data-ga-type="click"
+            data-ga-region="main content"
+            data-ga-section="default"
+          >
+            This card unfolds.
+            <span className="fas fa-chevron-up"></span>
+          </a>
+        </h3>
+      </div>
+      <div
+        id={`${bodyID}`}
+        className="collapse accordion-body"
+        aria-labelledby={`${linkID}`}
+        data-bs-parent={accordionID ? `#${accordionID}` : ""}
+      >
+        <h4>This is a quaternary headline</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud
+        </p>
+        <h5>This is a level five headline. There's a fancy word for that too.</h5>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor
+        </p>
+      </div>
     </div>
   </div>
 );
