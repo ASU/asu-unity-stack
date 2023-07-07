29dc6dd6b1c266a307e2717dbc35c6a75d7c51fa
david ornelas <davidornelas_11@hotmail.com>
2023-07-07 16:37:54 -0700

    refactor(app-webdir-ui): removed unused imports

--- packages/app-webdir-ui/src/FacultyRankComponent/index.js
+++ packages/app-webdir-ui/src/FacultyRankComponent/index.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import { TabbedPanels, Tab } from "@asu/components-core";
 import { engineNames, engines } from "../helpers/search";
