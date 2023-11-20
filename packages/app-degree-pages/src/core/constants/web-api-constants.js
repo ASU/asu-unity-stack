7c968c61ee2dc4f2c081e52125db69a33968ce98
david ornelas <davidornelas_11@hotmail.com>
2023-11-20 16:28:58 -0700

    feat(app-degree-pages): updated default api endpoint data

--- packages/app-degree-pages/src/core/constants/web-api-constants.js
+++ packages/app-degree-pages/src/core/constants/web-api-constants.js
@@ -6,20 +6,10 @@ const DOMAIN_URL = "https://degrees.apps.asu.edu";
  * @type {import("../types/listing-page-types").ProgramListDataSource}
  */
 const listingPageDefaultDataSource = {
-  endpoint: "https://degrees.apps.asu.edu/t5/service",
-  method: "findAllDegrees",
-  init: "false",
-  fields:
-    `graduateApplyDates,planDeadlines,AsuDegSrchFlg,` +
-    "CollegeAcadOrg,CollegeAcadOrgJoint,DepartmentCode," +
-    "Descr100,Institution,AcadPlan," +
-    "Degree,DegreeDescr,DegreeDescrlong," +
-    "concurrentDegreeMajorMaps,managedOnlineCampus,onlineMajorMapURL," +
-    "AsuCritTrackUrl,AsuOfficeLoc,CampusStringArray,AcadPlan," +
-    "accelerateDegrees,concurrentDegrees," +
-    "CollegeDescr100,CollegeUrl,EmailAddr,DescrlongExtns,AsuProgramFee," +
-    "AsuLangReqFlag,asuAcadpLrfText," +
-    "asuMathReqFlag,additionalMathReqCourse,asuAcadpMrfText,MathIntensity,",
+  endpoint: "https://api.myasuplat-dpl.asu.edu/api/codeset/acad-plans",
+  filter: "activeInDegreeSearch",
+  include: "*",
+  degreeType: "UG",
 };
 
 /**
@@ -27,34 +17,8 @@ const listingPageDefaultDataSource = {
  */
 const detailPageDefaultDataSource = {
   acadPlan: null,
-  endpoint: "https://degrees.apps.asu.edu/t5/service",
-  method: "findDegreeByAcadPlan",
-  init: "false",
-  fields:
-    `graduateApplyDates,planDeadlines,AsuDegSrchFlg,AsuCustomText,Degree,` +
-    `marketText,DescrlongExtns,concurrentDegreeMajorMaps,onlineMajorMapURL,` +
-    `ChangeMajor,AsuCritTrackUrl,SubPlnMajorMaps,SubPln,` +
-    // at a glance
-    `Descr100,CollegeDescr100,CollegeUrl,` +
-    `AsuOfficeLoc,CampusStringArray,campusWue,MinMathReq,MathIntensity,` +
-    `degreeMajorMap,` +
-    // Example Careers
-    "careerData," +
-    // flexible degree options
-    `accelerateDegrees,accelerateDegreeMajorMaps,` +
-    `concurrentDegrees,concurrentDegreeMajorMaps,` +
-    // career opportunity
-    `AsuCareerOpp,` +
-    // program contact
-    `DepartmentName,PlanUrl,EmailAddr,Phone,CollegeDescr100,` +
-    // application requirement
-    `DescrlongExtn5,TransferAdmission,gradAdditionalRequirements,AdmissionsDegRequirements,` +
-    `AdmissionsDegRequirements,` +
-    // Global opportunity
-    `globalExp,` +
-    // attend online
-    `CurriculumUrl,managedOnlineCampus,` +
-    `planCatDescr`,
+  endpoint: "https://api.myasuplat-dpl.asu.edu/api/codeset/acad-plan",
+  include: "*",
 };
 
 export {
