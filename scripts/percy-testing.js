870de1ec0a7c96f820e06aa860003f227aefac6b
david ornelas <davidornelas_11@hotmail.com>
2024-02-07 01:55:28 -0700

    build: edit jenkinsfile to pull puppeteer image

--- scripts/percy-testing.js
+++ scripts/percy-testing.js
@@ -1,32 +1,36 @@
 const { exec } = require('child_process');
-const fs = require('fs');
+const fs = require('fs').promises; // Use the Promise-based version of fs
 const path = require('path');
 
 const storybookDir = path.join(__dirname, '..', 'build/@asu');
 
-fs.readdir(storybookDir, { withFileTypes: true }, (err, files) => {
-  if (err) {
-    console.error("Error reading the storybook directory:", err);
-    return;
-  }
-
-    const NUMBER_OF_PACKAGES = files.length || 0; // TODO: Do we want to include all packages?
-
-    files.forEach((dirent, index) => {
-        if (dirent.isDirectory()) {
-            const packageDir = path.join(storybookDir, dirent.name);
-            console.log(`Capturing snapshots for ${dirent.name}`);
-
-            exec(`percy storybook --shard-count=${NUMBER_OF_PACKAGES} --shard-index=${index} ${packageDir}`, (err, stdout, stderr) => {
-                if (err) {
-                    console.error(`Error capturing snapshots for ${dirent.name}:`, err);
-                    return;
-                }
-                console.log(stdout);
-                if (stderr) {
-                    console.error(stderr);
-                }
-            });
-        }
-    });
+const execPromise = (cmd) => new Promise((resolve, reject) => {
+  exec(cmd, (err, stdout, stderr) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    if (stderr) {
+      console.error(stderr);
+    }
+    resolve(stdout);
+  });
 });
+
+const captureSnapshots = async () => {
+  try {
+    const files = await fs.readdir(storybookDir, { withFileTypes: true });
+    for (const dirent of files) {
+      if (dirent.isDirectory()) {
+        const packageDir = path.join(storybookDir, dirent.name);
+        console.log(`Capturing snapshots for ${dirent.name}`);
+        const stdout = await execPromise(`percy storybook --dry-run --verbose ${packageDir}`);
+        console.log(stdout);
+      }
+    }
+  } catch (err) {
+    console.error("Error reading the storybook directory or capturing snapshots:", err);
+  }
+};
+
+captureSnapshots();
