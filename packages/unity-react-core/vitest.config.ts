5321c070271e6ac77504d1cb629958d0e68983ec
Scott Williams <stwilli2@asu.edu>
2024-10-03 11:39:32 -0700

    fix alias for tests

--- packages/unity-react-core/vitest.config.ts
+++ packages/unity-react-core/vitest.config.ts
@@ -1,10 +1,16 @@
 /// <reference types="vitest/config" />
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from "path";
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
+  },
+  resolve: {
+    alias: {
+      "@shared": resolve(__dirname, "./../../shared"),
+    }
   },
 })
