802ac20501ab9444b3a472daa68cdeed31b5c03a
david ornelas <davidornelas_11@hotmail.com>
2024-10-08 17:44:42 -0700

    docs: update examples to use React18

--- packages/unity-react-core/vite.config.js
+++ packages/unity-react-core/vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
       output: {
         globals: {
           "react": "React",
-          "react-dom": "ReactDOM",
+          "render": "ReactDOM.render",
         },
       },
     },
