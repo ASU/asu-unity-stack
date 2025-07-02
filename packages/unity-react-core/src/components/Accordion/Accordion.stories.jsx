e10dc26f01bcbdb7b64cedf3258ab15494401dd1
Scott Williams <5209283+scott-williams-az@users.noreply.github.com>
2025-07-02 10:29:42 -0700

    fix(unity-react-core): fix long accordion scroll issue
    # Conflicts:
    #	packages/unity-react-core/src/components/Accordion/Accordion.jsx
    #	packages/unity-react-core/src/components/Accordion/AccordionCard/AccordionCard.jsx

--- packages/unity-react-core/src/components/Accordion/Accordion.stories.jsx
+++ packages/unity-react-core/src/components/Accordion/Accordion.stories.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-plusplus */
 /* eslint react/jsx-props-no-spreading: "off" */
 import React from "react";
 
+import { getLoremSentences } from "../../../../../shared/constants/strings";
 import { Accordion } from "./Accordion";
 
 export default {
@@ -18,13 +20,28 @@ including rules on the use of Call-to-Action buttons and tags.
 View component examples and source code below.
 
 This story includes another components for demostration purposes.
-        `,
+
+## Bootstrap HTML
+
+The \`.accordion\` class is a bootstrap5 class that is used to create an accordion.
+
+- The accordions will conform to the width of the surrounding container.
+- There is a recommended character limit of 75 characters for the text within the header of a foldable card.
+
+The recommended markup for an accordion begins from the following pattern.
+
+***Note:*** The \`.accordion-body\` class must be wrapped in a div with the \`.collapse\` class, or else the accordion will have a jerky animation when opening and closing.
+`,
       },
     },
   },
 };
 
-const Template = args => <Accordion {...args} />;
+const Template = args => (
+  <>
+    <Accordion {...args} />
+  </>
+);
 
 export const Default = Template.bind({});
 Default.args = {
@@ -49,6 +66,75 @@ Default.args = {
     },
   ],
   openedCard: 3,
+};
+
+let i = 0;
+export const LargeContent = {
+  render: args => (
+    <>
+      <div className="container" style={{ paddingTop: "50vh" }}>
+        <p>{getLoremSentences(12, i++)}</p>
+        <p>{getLoremSentences(22, i++)}</p>
+        <p>{getLoremSentences(10, i++)}</p>
+        <Accordion {...args} />
+        <br />
+        <p>{getLoremSentences(35, i++)}</p>
+        <p>{getLoremSentences(22, i++)}</p>
+        <p>{getLoremSentences(12, i++)}</p>
+        <p>{getLoremSentences(42, i++)}</p>
+        <p>{getLoremSentences(19, i++)}</p>
+        <p>{getLoremSentences(12, i++)}</p>
+        <p>{getLoremSentences(10, i++)}</p>
+        <p>{getLoremSentences(35, i++)}</p>
+        <p>{getLoremSentences(22, i++)}</p>
+        <p>{getLoremSentences(12, i++)}</p>
+        <p>{getLoremSentences(42, i++)}</p>
+        <p>{getLoremSentences(19, i++)}</p>
+        <p>{getLoremSentences(12, i++)}</p>
+        <p>{getLoremSentences(10, i++)}</p>
+      </div>
+    </>
+  ),
+  args: {
+    cards: [
+      {
+        content: {
+          header: "Accordion Card 1",
+          body: "<h4>Quatrenary Headline</h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p><h5>This is a level five headline. There's a fancy word for that too.</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>",
+        },
+      },
+      {
+        content: {
+          header: "Accordion Card 2, Large Content",
+          body: `<h4>Quatrenary Headline</h4>
+          <p>${getLoremSentences(35, i++)}</p>
+          <p>${getLoremSentences(22, i++)}</p>
+          <p>${getLoremSentences(12, i++)}</p>
+          <p>${getLoremSentences(42, i++)}</p>
+          <p>${getLoremSentences(19, i++)}</p>
+          <p>${getLoremSentences(12, i++)}</p>
+          <p>${getLoremSentences(10, i++)}</p>
+          <p>${getLoremSentences(35, i++)}</p>
+          <p>${getLoremSentences(22, i++)}</p>
+          <p>${getLoremSentences(12, i++)}</p>
+          <p>${getLoremSentences(42, i++)}</p>
+          <p>${getLoremSentences(19, i++)}</p>
+          <p>${getLoremSentences(12, i++)}</p>
+          <p>${getLoremSentences(10, i++)}</p>
+          `,
+        },
+      },
+      {
+        content: {
+          header: "Accordion Card 3",
+          body: `<h4>Quatrenary Headline</h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p><h5>This is a level five headline. There's a fancy word for that too.</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
+          <p>${getLoremSentences(35, i++)}</p>
+          <p>${getLoremSentences(22, i++)}</p>`,
+        },
+      },
+    ],
+    openedCard: 2,
+  },
 };
 export const ColorCombinations = Template.bind({});
 ColorCombinations.args = {
