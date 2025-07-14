f58b6bb3e617c1c12ca9ed9d9b2a8f4e7ba4cfb2
david ornelas <davidornelas_11@hotmail.com>
2025-07-14 10:22:54 -0700

    fix(component-header-footer): fix social icon focus styles

--- packages/component-header-footer/src/footer/index.styles.js
+++ packages/component-header-footer/src/footer/index.styles.js
@@ -164,7 +164,7 @@ const StyledFooter = styled.footer`
 
         &:last-child {
           @media (min-width: 768px) {
-            padding-right: 0;
+            /* padding-right: 0; */
           }
         }
       }
