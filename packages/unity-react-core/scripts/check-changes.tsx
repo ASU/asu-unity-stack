40a97a5d77941bd2f007030f5188dd425e941107
Ojas Atkar <oatkar@asu.edu>
2025-09-11 12:31:35 -0700

    feat(unity-react-core): changes

--- packages/unity-react-core/scripts/check-changes.tsx
+++ packages/unity-react-core/scripts/check-changes.tsx
@@ -54,6 +54,120 @@ function convertToHTML(OLD_DATE){
              openedCard: 3,
       }
     },
+    AnchorMenu: {
+      reactComponent: AnchorMenu,
+      props: {
+        items: [
+        {
+          text: "Title Case is Required",
+          targetIdName: "first-container",
+          icon: ["fas", "link"],
+        },
+        { text: "Second Container", targetIdName: "second-container" },
+        { text: "Third Container", targetIdName: "third-container" },
+        {
+          text: "This is the Last",
+          targetIdName: "fourth-container",
+          icon: ["fas", "link"],
+        },
+      ],
+    }
+    },
+    Article: {
+      reactComponent: Article,
+      props: {defaultArgs: {
+        type: "news",
+        articleUrl: "https://example.com",
+        headerImageUrl: "",
+        title:
+          "Clarisse Machanguana takes her skill set to the next level at ASU Thunderbird",
+        publicationDate: "March 18, 2021",
+        authorName: "Jimena Garrison",
+        authorTitle: "Media Relations and Strategic Communications",
+        authorEmail: "jgarris6@asu.edu",
+        authorPhone: "480-727-4058",
+        breadcrumbs: [
+          {
+            title: "Home",
+            url: "/",
+          },
+          {
+            title: "Second nav item",
+            url: "/events",
+          },
+          {
+            title: "Current page",
+            url: "/events/current-article",
+            active: true,
+          },
+        ],
+        body: "<p>After 34 years in the game of basketball, Clarisse Machanguana retired. Her eponymous philanthropic foundation remains her only connection to the sport, although the effect of the global game has left imprints in many aspects of her life.</p><p>Playing basketball took her to Portugal and then the U.S., where she attended Old Dominion University in Virginia to study criminal justice. When she realized that sports could be a microcosm of life and values, she decided to create a way to coach sports while teaching life skills in her home country of Mozambique. She started the <a href='http://machanguanafoundation.rf.gd/'>Clarisse Machanguana Foundation</a> in 2014 with the goal of empowering Mozambican youth through health, education and sports programs.</p><p>Now she’s taking her leadership game to the next level at ASU’s Thunderbird School of Global Management, earning her Master of Global Management degree with a nonprofit management concentration. Machanguana is honing her skills as a global professional in and out of the classroom to propel her foundation even further.</p><p>Here she reflects on the experiences that brought her to Thunderbird and ASU.</p><p><strong>Question: Why basketball?</strong></p><p><strong>Answer:</strong> I started at age 6, and because it was popular in my area and I was tall — now 6-feet-5-inches tall, to be exact — people kept telling me I should play. Basketball took me everywhere. I had a scholarship to play in Portugal and the U.S., and later on in Spain, France, Brazil, South Korea and Italy. Basketball became a passport and a school for me, and a source of amazing friendships. I played from age 6 to 40. My foundation now partners with the Department of Education. We collaborate with teachers and teach them to coach life skills and basketball.</p><p>I advocate social causes that are challenging for youth and transform them into opportunities, giving them tools to lift themselves out of the poverty they see. They use the skills like respecting your opponent and perseverance that can be applied in life as well as sports. When you wake up and all you see is poverty, you start to believe that mindset of limitations and scarcity. We give young people something else to believe in, a vision of a better life.</p>",
+      },
+    }
+    },
+    Breadcrumbs: {
+      reactComponent: Breadcrumbs,
+      props: {
+        linkItems: [
+        {
+          label: "Home",
+          active: false,
+          href: "#",
+        },
+        {
+          label: "Library",
+          active: false,
+          href: "#",
+        },
+        {
+          label: "Data",
+          active: true,
+          href: "#",
+        },
+      ],
+    }
+    },
+    Card: {
+      reactComponenet: Card,
+      props: {
+        type: "default",
+        horizontal: false,
+        clickable: false,
+        image: "https://picsum.photos/300/200",
+        imageAltText: "An example image",
+        title: "Default title",
+        body: "<span style='font-weight: bold;'>(Bold!) Body copy goes here.</span> Limit to 5 lines max. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua eiusmod tempo.",
+        buttons: [
+          {
+            color: "maroon",
+            size: "default",
+            label: "CTA button",
+          },
+          {
+            color: "gold",
+            size: "small",
+            label: "Link Button",
+            href: "/",
+            target: "_top",
+          },
+        ],
+        tags: [
+          { color: "gray", label: "tag1", href: "/#example-link" },
+          { color: "gray", label: "tag2", href: "/#example-link" },
+          { color: "gray", label: "tag3", href: "/#example-link" },
+        ],
+        linkUrl: "/#",
+        linkLabel: "Link",
+      }
+    },
+    Pagination: {
+      reactComponent: Pagination,
+      props: {
+        type: "default",
+        background: "white",
+        totalPages: 4,
+      }
+    }
   }
 
 
