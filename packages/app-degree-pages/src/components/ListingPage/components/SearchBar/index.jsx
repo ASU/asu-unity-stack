08d8f48c0d6aecbf916f89ab627ea9cb3a51a3f5
david ornelas <davidornelas_11@hotmail.com>
2026-02-27 14:00:48 -0700

    fix(app-degree-pages): add search landmark accessibility to listing page

--- packages/app-degree-pages/src/components/ListingPage/components/SearchBar/index.jsx
+++ packages/app-degree-pages/src/components/ListingPage/components/SearchBar/index.jsx
@@ -54,49 +54,51 @@ const SearchBar = ({ value, onChange, onSearch }) => {
       data-searching={searching}
       data-testid="search-bar"
     >
-      <form
-        data-testid="search-bar-form"
-        className="uds-form p-0 col-md-6 col-sm-12"
-        onSubmit={e => {
-          e.preventDefault();
-          onSearch();
-          setSearching(true);
-          timeoutId = setTimeout(() => setSearching(false), 500);
-          clearTimeout(timeoutId);
-        }}
-      >
-        <div className="form-group mb-0 me-2">
-          <label htmlFor="search-field">Search</label>
+      <search>
+        <form
+          data-testid="search-bar-form"
+          className="uds-form p-0 col-md-6 col-sm-12"
+          onSubmit={e => {
+            e.preventDefault();
+            onSearch();
+            setSearching(true);
+            timeoutId = setTimeout(() => setSearching(false), 500);
+            clearTimeout(timeoutId);
+          }}
+        >
+          <div className="form-group mb-0 me-2">
+            <label htmlFor="search-field">Search</label>
 
-          <div className="d-flex row align-items-baseline g-3">
-            <div className="col-sm-12 col-md-6 align-self-end">
-              <input
-                data-testid="search-field"
-                id="search-field"
-                value={value}
-                type="text"
-                className="form-control"
-                placeholder="Search degree programs"
-                onChange={e => {
-                  onChange(e.target.value);
-                  trackSearchEvent(e.target.value);
-                }}
-              />
-            </div>
+            <div className="d-flex row align-items-baseline g-3">
+              <div className="col-sm-12 col-md-6 align-self-end">
+                <input
+                  data-testid="search-field"
+                  id="search-field"
+                  value={value}
+                  type="text"
+                  className="form-control"
+                  placeholder="Search degree programs"
+                  onChange={e => {
+                    onChange(e.target.value);
+                    trackSearchEvent(e.target.value);
+                  }}
+                />
+              </div>
 
-            <div className="col-sm-12 col-md-6 mt-2 mt-sm-0">
-              <Button
-                data-testid="search-button"
-                color="maroon"
-                label="Search now"
-                ariaLabel="Search now"
-                size="default"
-                onClick={onSearch}
-              />
+              <div className="col-sm-12 col-md-6 mt-2 mt-sm-0">
+                <Button
+                  data-testid="search-button"
+                  color="maroon"
+                  label="Search now"
+                  ariaLabel="Search now"
+                  size="default"
+                  onClick={onSearch}
+                />
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </search>
     </Section>
   );
 };
