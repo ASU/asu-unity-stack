317b46c312dbbd677f9f8008d851d7ed94d35e12
asu-jenkins-devops <jenkins_app@asu.edu>
2026-03-12 17:47:57 +0000

    Updates

---
+++ @asu/app-rfi/assets/preview-Dz-xy8UU.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./DocsRenderer-NNNQARDV-BqZZDRNY.js","./iframe-DswhyOpI.js","./index-DqL-iMyr.js","./react-18-BFBtyJRr.js","./index-Cu4lwwaE.js","./extends-CF3RwP-h.js","./_getPrototype-QNyG8S32.js","./index-DrFu-skq.js"])))=>i.map(i=>d[i]);
+import{_ as t}from"./iframe-DswhyOpI.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-BqZZDRNY.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5,6,7]),import.meta.url);return new r}}};export{_ as parameters};
---
+++ @asu/unity-react-core/assets/ImageCarousel.stories-gFbZ9Jda.js
@@ -0,0 +1,36 @@
+import{j as r}from"./jsx-runtime-VU7BY01E.js";import"./index-BebQxa19.js";import{i as o}from"./props-mock-B1jwmRCT.js";import{I}from"./ImageCarousel-iBoIkIzA.js";import"./_commonjsHelpers-NT8qMgGI.js";import"./Layout-BJiG6VbP.js";import"./index-X6w4Ppxe.js";import"./index-BMjp-peT.js";import"./index-DZ0CU6KP.js";import"./GaEventWrapper-CYSgpRo7.js";const C=()=>o.map(e=>({...e,content:`
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+      commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+      penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      Donec quam felis, ultricies nec, pellentesque eu, pretium …
+  `})),f=()=>o.map((e,i)=>({...e,title:`Content ${i+1}`,content:`<p>Body copy goes here. Limit to 5 lines max. Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua eiusmod tempo.</p>
+    <p>Another line of content</p>`})),_={title:"Components/Image Carousel",component:I,globals:{framework:"react"},tags:["!bootstrap"],args:{imageItems:[],perView:1},argTypes:{imageItems:{table:{disable:!0}},perView:{control:{type:"range",min:1,max:3,step:1}}}},y="800px",m=({imageItems:e,perView:i})=>r.jsx("div",{style:{},children:r.jsx(I,{perView:i,maxWidth:y,imageItems:e})}),t=m.bind({});t.args={imageItems:o};const a=m.bind({});a.args={imageItems:C()};const s=m.bind({});s.args={imageItems:f()};var p,n,l;t.parameters={...t.parameters,docs:{...(p=t.parameters)==null?void 0:p.docs,source:{originalSource:`({
+  imageItems,
+  perView
+}) => <div style={{
+  // setting display flex the 'maxHeight' gets applied but the carousel squashes
+  // display: "flex",
+  // maxHeight: text("Max Height", maxHeight, GROUP_STYLE),
+}}>
+    <ImageCarousel perView={perView} maxWidth={maxWidth} imageItems={imageItems} />
+  </div>`,...(l=(n=t.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var g,u,d;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`({
+  imageItems,
+  perView
+}) => <div style={{
+  // setting display flex the 'maxHeight' gets applied but the carousel squashes
+  // display: "flex",
+  // maxHeight: text("Max Height", maxHeight, GROUP_STYLE),
+}}>
+    <ImageCarousel perView={perView} maxWidth={maxWidth} imageItems={imageItems} />
+  </div>`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var c,h,x;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`({
+  imageItems,
+  perView
+}) => <div style={{
+  // setting display flex the 'maxHeight' gets applied but the carousel squashes
+  // display: "flex",
+  // maxHeight: text("Max Height", maxHeight, GROUP_STYLE),
+}}>
+    <ImageCarousel perView={perView} maxWidth={maxWidth} imageItems={imageItems} />
+  </div>`,...(x=(h=s.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};const E=["ImageCarouselDefault","ImageCarouselWithCaption","ImageCarouselWithMoreCaptionContent"];export{t as ImageCarouselDefault,a as ImageCarouselWithCaption,s as ImageCarouselWithMoreCaptionContent,E as __namedExportsOrder,_ as default};
