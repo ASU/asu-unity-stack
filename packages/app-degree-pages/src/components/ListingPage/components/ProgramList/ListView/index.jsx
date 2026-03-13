3bf0914193dfcc3f162e09bdc9730f8375dd0d20
Michael Webber <mawebbe1@asu.edu>
2026-03-13 09:45:01 -0700

    fix(app-degree-pages): added space for "First Required Math Course:" and "Math Intensity:"

--- packages/app-degree-pages/src/components/ListingPage/components/ProgramList/ListView/index.jsx
+++ packages/app-degree-pages/src/components/ListingPage/components/ProgramList/ListView/index.jsx
@@ -79,7 +79,7 @@ const renderExtraInfo = ({ resolver }) => (
     {resolver.hasMathReqFlag() && (
       <div>
         <div>
-          <strong>First Required Math Course:</strong>
+          <strong>First Required Math Course: </strong>
           <span>{resolver.getMinMathReq()}</span>
           {resolver.getOtherMathReqCourse() && (
             <span
@@ -94,7 +94,7 @@ const renderExtraInfo = ({ resolver }) => (
 
     {resolver.getMathIntensity() && (
       <div>
-        <strong>Math Intensity:</strong>
+        <strong>Math Intensity: </strong>
         {resolver.getMathIntensity()}
       </div>
     )}
