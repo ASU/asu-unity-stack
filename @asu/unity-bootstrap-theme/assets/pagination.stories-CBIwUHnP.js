159bbe11504782e49a49d5b80ac078da335977f4
asu-jenkins-devops <jenkins_app@asu.edu>
2026-03-20 22:34:12 +0000

    Updates

---
+++ @asu/unity-bootstrap-theme/assets/pagination.stories-CBIwUHnP.js
@@ -0,0 +1,21 @@
+import{M as a}from"./chunk-HLWAVYOI-Btpc0oF5.js";import{j as e}from"./jsx-runtime-DAFs00V8.js";import{useMDXComponents as s}from"./index-0_jiJ7ii.js";import"./iframe-BZp8msEB.js";import"../sb-preview/runtime.js";import"./index-RYns6xqu.js";import"./react-18-CWbnM-_i.js";import"./index-Cu4lwwaE.js";import"./index-HRfTNzeJ.js";import"./index-DrFu-skq.js";function o(t){const n=Object.assign({h1:"h1",h2:"h2",p:"p",strong:"strong",code:"code",ol:"ol",li:"li",a:"a"},s(),t.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Molecules/Pagination/Readme"}),`
+`,e.jsx(n.h1,{id:"custom-pagination-component",children:"Custom Pagination Component"}),`
+`,e.jsx(n.h2,{id:"introduction",children:"Introduction"}),`
+`,e.jsx(n.p,{children:"This component provides a flexible and customizable way to implement pagination on your website or application. It is designed to be highly adaptable to your specific design and accessibility needs."}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Note"}),": If you are considering implementing this Pagination component over the one available in the ",e.jsx(n.code,{children:"unity-react-core"})," package, please be aware that you will need to handle the appropriate JavaScript to manage and update accessibility attributes based on the current page. The default behavior might not include this specific feature."]}),`
+`,e.jsx(n.h2,{id:"getting-started",children:"Getting Started"}),`
+`,e.jsx(n.p,{children:"To get started with using this Pagination component, follow these steps:"}),`
+`,e.jsxs(n.ol,{children:[`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Installation"}),": Download the necessary files for the Pagination component and include them in your project."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"Usage"}),": Integrate the Pagination component into your desired web page or application. Use the provided HTML structure to render the pagination UI."]}),`
+`]}),`
+`,e.jsxs(n.li,{children:[`
+`,e.jsxs(n.p,{children:[e.jsx(n.strong,{children:"JavaScript"}),": Make sure to include the required JavaScript to handle the dynamic updates of the accessibility attributes based on the current page. This is crucial for ensuring that the Pagination component is fully accessible to all users."]}),`
+`]}),`
+`]}),`
+`,e.jsx(n.h2,{id:"accessibility-guidelines",children:"Accessibility Guidelines"}),`
+`,e.jsx(n.p,{children:"The pager stories here are static, markup-only references. Developers will need to provide interactivity that updates markup accordingly as users interact with the pager."}),`
+`,e.jsxs(n.p,{children:["When implementing the Pagination component, it's essential to adhere to accessibility guidelines. We recommend following the ",e.jsx(n.a,{href:"https://design-system.w3.org/components/pagination.html",target:"_blank",rel:"nofollow noopener noreferrer",children:"W3C Design System's Pagination Component Guidelines"}),". These guidelines provide valuable insights into creating a component that is usable and navigable for all users, including those who rely on assistive technologies."]})]})}function r(t={}){const{wrapper:n}=Object.assign({},s(),t.components);return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}const c=()=>{throw new Error("Docs-only story")};c.parameters={docsOnly:!0};const i={title:"Molecules/Pagination/Readme",tags:["stories-mdx"],includeStories:["__page"]};i.parameters=i.parameters||{};i.parameters.docs={...i.parameters.docs||{},page:r};const b=["__page"];export{b as __namedExportsOrder,c as __page,i as default};
---
+++ @asu/unity-bootstrap-theme/assets/syntaxhighlighter-MJWPISIS-CIMrO3BU.js
@@ -0,0 +1,1 @@
+import{S as c,c as l,s as n}from"./chunk-HLWAVYOI-Btpc0oF5.js";import"./iframe-BZp8msEB.js";import"../sb-preview/runtime.js";import"./index-RYns6xqu.js";import"./react-18-CWbnM-_i.js";import"./index-Cu4lwwaE.js";import"./index-HRfTNzeJ.js";import"./index-DrFu-skq.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
