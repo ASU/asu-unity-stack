3a3fce879afe929d335f260c541adacc551aa841
david ornelas <davidornelas_11@hotmail.com>
2026-03-25 14:08:30 -0700

    chore(unity-bootstrap-theme): jS linting

--- packages/unity-bootstrap-theme/.storybook/main.js
+++ packages/unity-bootstrap-theme/.storybook/main.js
@@ -7,14 +7,15 @@ function getAbsolutePath(value) {
 
 export default {
   staticDirs: ["../dist"],
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     fileURLToPath(import.meta.resolve("./local-addon")),
     fileURLToPath(import.meta.resolve("../../../.storybook-config/index.js")),
-    fileURLToPath(import.meta.resolve("../../../.storybook-config/dataLayerListener/index.js")),
+    fileURLToPath(
+      import.meta.resolve(
+        "../../../.storybook-config/dataLayerListener/index.js"
+      )
+    ),
     "@whitespace/storybook-addon-html",
     getAbsolutePath("@storybook/addon-docs"),
   ],
