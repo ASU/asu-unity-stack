99b588fcfa3d35259fe8a128fd2c66bc7a4618e0
asu-jenkins-devops <jenkins_app@asu.edu>
2026-04-09 20:03:19 +0000

    Updates

---
+++ @asu/unity-react-core/assets/HighlyRanked.stories-B3nXOy88.js
@@ -0,0 +1,72 @@
+import{m as l}from"./iframe-DfaYDBZy.js";import"./preload-helper-Dp1pzeXC.js";const p={title:"Components/HighlyRanked",component:l},e={args:{title:"ASU is highly ranked",description:"ASU consistently ranks among the best in the nation, recognized for our commitment to excellence and showcasing our dedication to providing quality education and impactful research.",ctaText:"See more ASU rankings",ctaUrl:"https://www.asu.edu/rankings",rankings:[{value:"400+",title:"'prestigious faculty'",description:"National Academies-honored faculty"},{value:"#2",title:"in the U.S. for employability",description:"among public universities"},{value:"83",title:"top-ranked programs",description:"Ranked in the top 25 in the U.S., including 38 in the top 10"},{value:"$6.1",title:"billion",description:"FY24 economic impact on the state's gross product"},{value:"Top 10",title:"worldwide among universities granted U.S. patents",description:"For two years"},{value:"270+",title:"athletic championships",description:"National and conference titles"},{value:"620,000+",title:"alumni",description:"Leading, shaping, changing our world"},{value:"Association of American Universities (AAU) member",title:"",description:"Along with Harvard, Stanford and MIT",headingLevel:"h2"},{value:"Top producer of elite scholars",title:"",description:"For 10 consecutive years",headingLevel:"h2"}]}},i={args:{...e.args,rankings:[{value:"400+",title:"'prestigious faculty'",description:"National Academies-honored faculty",headingLevel:"h1"},{value:"#2",title:"in the U.S. for employability",description:"among public universities",headingLevel:"h3"},{value:"83",title:"top-ranked programs",description:"Ranked in the top 25 in the U.S., including 38 in the top 10",headingLevel:"h4"},{value:"$6.1",title:"billion",description:"FY24 economic impact on the state's gross product",headingLevel:"h5"}]}};var t,n,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    title: "ASU is highly ranked",
+    description: "ASU consistently ranks among the best in the nation, recognized for our commitment to excellence and showcasing our dedication to providing quality education and impactful research.",
+    ctaText: "See more ASU rankings",
+    ctaUrl: "https://www.asu.edu/rankings",
+    rankings: [{
+      value: "400+",
+      title: "'prestigious faculty'",
+      description: "National Academies-honored faculty"
+    }, {
+      value: "#2",
+      title: "in the U.S. for employability",
+      description: "among public universities"
+    }, {
+      value: "83",
+      title: "top-ranked programs",
+      description: "Ranked in the top 25 in the U.S., including 38 in the top 10"
+    }, {
+      value: "$6.1",
+      title: "billion",
+      description: "FY24 economic impact on the state's gross product"
+    }, {
+      value: "Top 10",
+      title: "worldwide among universities granted U.S. patents",
+      description: "For two years"
+    }, {
+      value: "270+",
+      title: "athletic championships",
+      description: "National and conference titles"
+    }, {
+      value: "620,000+",
+      title: "alumni",
+      description: "Leading, shaping, changing our world"
+    }, {
+      value: "Association of American Universities (AAU) member",
+      title: "",
+      description: "Along with Harvard, Stanford and MIT",
+      headingLevel: "h2"
+    }, {
+      value: "Top producer of elite scholars",
+      title: "",
+      description: "For 10 consecutive years",
+      headingLevel: "h2"
+    }]
+  }
+}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};var o,r,s;i.parameters={...i.parameters,docs:{...(o=i.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    rankings: [{
+      value: "400+",
+      title: "'prestigious faculty'",
+      description: "National Academies-honored faculty",
+      headingLevel: "h1"
+    }, {
+      value: "#2",
+      title: "in the U.S. for employability",
+      description: "among public universities",
+      headingLevel: "h3"
+    }, {
+      value: "83",
+      title: "top-ranked programs",
+      description: "Ranked in the top 25 in the U.S., including 38 in the top 10",
+      headingLevel: "h4"
+    }, {
+      value: "$6.1",
+      title: "billion",
+      description: "FY24 economic impact on the state's gross product",
+      headingLevel: "h5"
+    }]
+  }
+}`,...(s=(r=i.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};const u=["Default","MixedValueSizes"];export{e as Default,i as MixedValueSizes,u as __namedExportsOrder,p as default};
---
+++ @asu/unity-react-core/assets/Tooltip.stories-jyHQ68qU.js
@@ -0,0 +1,25 @@
+import{u as s,j as e,b as f}from"./iframe-DfaYDBZy.js";import"./preload-helper-Dp1pzeXC.js";const j={title:"Components/Tooltip",component:s,parameters:{docs:{description:{component:`TODO
+https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/tooltip_role
+Keyboard interactions [Escape] should close the tooltip
+
+alignment of tooltip assumes the trigger is icon button
+
+probably limit the triggers to something with a visual inidicator (like button or link)`}}}},i={title:"Header",content:"Content"},T=n=>e.jsx(s,{...n}),t={render:T.bind({}),args:{...i,triggerElement:e.jsx(f,{icon:["fas","info"]})}},r={render:n=>e.jsxs("div",{children:["This is a ",e.jsx(s,{...n})," sentence."]}),args:{...i,triggerElement:e.jsx("a",{href:"javascript:void(0);",children:"Tooltiptrigger"})}},o={render:T.bind({}),args:{...i,triggerElement:e.jsx("span",{children:"Tooltiptrigger"})}};var a,l,p;t.parameters={...t.parameters,docs:{...(a=t.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: tooltipTemplate.bind({}),
+  args: {
+    ...defaultProps,
+    triggerElement: <ButtonIconOnly icon={["fas", "info"]} />
+  }
+}`,...(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var c,d,g;r.parameters={...r.parameters,docs:{...(c=r.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: args => <div>This is a <Tooltip {...args} /> sentence.</div>,
+  args: {
+    ...defaultProps,
+    triggerElement: <a href="javascript:void(0);">Tooltiptrigger</a>
+  }
+}`,...(g=(d=r.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};var m,u,h;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: tooltipTemplate.bind({}),
+  args: {
+    ...defaultProps,
+    triggerElement: <span>Tooltiptrigger</span>
+  }
+}`,...(h=(u=o.parameters)==null?void 0:u.docs)==null?void 0:h.source}}};const v=["Icon","link","text"];export{t as Icon,v as __namedExportsOrder,j as default,r as link,o as text};
