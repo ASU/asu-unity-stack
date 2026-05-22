0d95f8d5e3f93068a886499d80290c5843cf0eb7
Scott Williams <5209283+scott-williams-az@users.noreply.github.com>
2026-05-22 09:44:59 -0700

    chore(app-rfi): remove console log

--- packages/app-rfi/src/core/utils/google-analytics.js
+++ packages/app-rfi/src/core/utils/google-analytics.js
@@ -2,9 +2,6 @@
 
 /* dataLayer event push to GA. */
 export function pushDataLayerEventToGa(value) {
-  // eslint-disable-next-line no-console
-  console.log(`Pushing event ${value.event} to GA dataLayer`);
-  console.log(value);
   // PUSH TO GA.
   if (typeof dataLayer !== "undefined") {
     dataLayer.push(value);
