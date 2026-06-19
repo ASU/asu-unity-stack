75c54e8e824287fc13e66c3311ddfe797958d1c7
Scott Williams <5209283+scott-williams-az@users.noreply.github.com>
2026-06-18 17:59:35 -0700

    fix(component-header-footer): col divider bg fix

--- packages/component-header-footer/src/header/components/HeaderMain/NavbarContainer/DropdownItem/index.js
+++ packages/component-header-footer/src/header/components/HeaderMain/NavbarContainer/DropdownItem/index.js
@@ -210,6 +210,11 @@ const DropdownItem = ({
   if (buttons.length > 0) {
     heightOffset += 64;
   }
+  // linear gradient background start
+  // Calculate the span for each column and the background
+  // style for the dropdown, vertical border lines on the columns
+  // do not render past the overflow. This solution uses a linear
+  // gradient background to simulate the borders.
   /** @type {number[]} */
   let spans = [];
   let spanbackgroundStyle = "";
@@ -239,6 +244,7 @@ const DropdownItem = ({
 
     spanbackgroundStyle = `linear-gradient(to right, ${gradientStops})`;
   }
+  // linear gradient background end
 
   return (
     <DropdownWrapper
