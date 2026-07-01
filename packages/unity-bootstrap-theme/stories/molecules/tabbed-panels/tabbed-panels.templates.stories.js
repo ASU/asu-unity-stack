bd5a11dab952809af0548a56a63ddfc2350b2c03
david ornelas <davidornelas_11@hotmail.com>
2026-06-30 17:05:02 -0700

    fix(unity-react-core): address tabbed panel pr comments

--- packages/unity-bootstrap-theme/stories/molecules/tabbed-panels/tabbed-panels.templates.stories.js
+++ packages/unity-bootstrap-theme/stories/molecules/tabbed-panels/tabbed-panels.templates.stories.js
@@ -1,6 +1,16 @@
 export const TabbedPanels = args => {
   return (
-    <div>See updated HTML output in unity-react-core Tabbed panels story.</div>
+    <div>
+      See updated HTML output in the{" "}
+      <a
+        href="../unity-react-core/index.html?path=/docs/components-tabbedpanels--docs"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        unity-react-core Tabbed panels story
+      </a>
+      .
+    </div>
   );
 };
 
