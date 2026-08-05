a072968b2398c68a88564f2409aea2757dda526e
Michael Webber <mawebbe1@asu.edu>
2026-08-05 10:24:06 -0700

    feat(unity-react-core): modal auto focus and disabled tab to content outside the modal

--- packages/unity-bootstrap-theme/src/js/modals.js
+++ packages/unity-bootstrap-theme/src/js/modals.js
@@ -1,22 +1,101 @@
 import { EventHandler } from "./bootstrap-helper";
 
+function openModal() {
+  document.getElementById("uds-modal")?.classList.add("open");
+  let closeModalButton = document.getElementById("closeModalButton");
+  setTimeout(() => {
+    if (closeModalButton) {
+      // Wait for dom to update before setting focus
+      closeModalButton?.focus();
+    }
+  }, 200);
+
+  // const mainContent = document.getElementById("main-content");
+  // mainContent.setAttribute("inert", "");
+  // const mainContentChildren = mainContent.children;
+  // let mainContentChildrenArray = Array.from(mainContentChildren);
+  // for (let i = 0; i < mainContentChildrenArray.length; i++) {
+  //   mainContentChildrenArray[i].setAttribute("inert", "");
+  // }
+
+  // Disable navigation to everything accept for the modal content
+  // Source: https://stackoverflow.com/questions/4195616/how-to-set-the-focus-on-a-javascript-modal-window
+  const focusableElements =
+    'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+  const modal = document.getElementsByClassName("uds-modal-container")[0];
+  const firstFocusableElement = modal?.querySelectorAll(focusableElements)[0];
+  const focusableContent = modal?.querySelectorAll(focusableElements);
+  const lastFocusableElement = focusableContent
+    ? focusableContent[focusableContent?.length - 1]
+    : undefined;
+
+  const handleTabKey = e => {
+    let isTabPressed = e.key === "Tab"; // || e.keyCode === 9;
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (e.shiftKey) {
+      // if shift key pressed for shift + tab combination
+      if (document.activeElement === firstFocusableElement) {
+        lastFocusableElement?.focus(); // add focus for the last focusable element
+        e.preventDefault();
+      }
+    } else {
+      // if tab key is pressed
+      if (document.activeElement === lastFocusableElement) {
+        // if focused has reached to last focusable element then focus first focusable element after pressing tab
+        firstFocusableElement?.focus(); // add focus for the first focusable element
+        e.preventDefault();
+      }
+    }
+  };
+
+  if (lastFocusableElement && firstFocusableElement) {
+    document.addEventListener("keydown", handleTabKey);
+    // firstFocusableElement?.focus();
+    return () => document.removeEventListener("keydown", handleTabKey);
+  }
+}
+
+function closeModal() {
+  document.getElementById("uds-modal").classList.remove("open");
+
+  let openModalButton = document.getElementById("openModalButton");
+  setTimeout(() => {
+    if (openModalButton) {
+      // Wait for dom to update before setting focus
+      openModalButton?.focus();
+    }
+  }, 200);
+
+  // const mainContent = document.getElementById("main-content");
+  // mainContent?.removeAttribute("inert");
+  // const mainContentChildren = mainContent.children;
+  // let mainContentChildrenArray = Array.from(mainContentChildren);
+  // for (let i = 0; i < mainContentChildrenArray.length; i++) {
+  //   mainContentChildrenArray[i].removeAttribute("inert");
+  // }
+}
+
 function initModals() {
   document
     .getElementById("openModalButton")
     ?.addEventListener("click", function () {
-      document.getElementById("uds-modal")?.classList.add("open");
-      document.getElementById("closeModalButton")?.focus();
+      openModal();
     });
 
   document
     .getElementById("closeModalButton")
     ?.addEventListener("click", function () {
-      document.getElementById("uds-modal").classList.remove("open");
+      closeModal();
     });
 
   document?.addEventListener("keydown", function (event) {
-    event.key === "Escape" &&
-      document.getElementById("uds-modal")?.classList.remove("open");
+    if (event.key === "Escape") {
+      closeModal();
+    }
   });
 }
 
--- packages/unity-react-core/src/components/Modal/Modal.tsx
+++ packages/unity-react-core/src/components/Modal/Modal.tsx
@@ -4,10 +4,6 @@ import { ButtonIconOnly } from "../ButtonIconOnly/ButtonIconOnly";
 import { GaEventWrapper } from "../GaEventWrapper/GaEventWrapper";
 import { useBaseSpecificFramework } from "../GaEventWrapper/useBaseSpecificFramework";
 import classNames from "classnames";
-/**
- *
- * TODO: Should we be using bootstrap's built in modal functionality?
- */
 
 const defaultGaData = {
   name: "onclick",
@@ -57,9 +53,19 @@ export const Modal: React.FC<ModalProps> = ({ open, gaData }) => {
   }, [openState, setOpen]);
 
   useEffect(() => {
-    if (!openState) return;
+    if (!openState) {
+      let openModalButton = document.getElementById("openModalButtonR");
+      setTimeout(() => {
+        if (openModalButton) {
+          // Wait for dom to update before setting focus
+          openModalButton?.focus();
+        }
+      }, 200);
+      return;
+    }
 
-    //source: https://stackoverflow.com/questions/4195616/how-to-set-the-focus-on-a-javascript-modal-window
+    // Disable navigation to everything accept for the modal content
+    // Source: https://stackoverflow.com/questions/4195616/how-to-set-the-focus-on-a-javascript-modal-window
     const focusableElements =
       'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
     const modal = document.getElementsByClassName("uds-modal-container")[0];
@@ -94,27 +100,36 @@ export const Modal: React.FC<ModalProps> = ({ open, gaData }) => {
 
     if (lastFocusableElement && firstFocusableElement) {
       document.addEventListener("keydown", handleTabKey);
-      (firstFocusableElement as HTMLElement)?.focus();
+      setTimeout(() => {
+        if (firstFocusableElement) {
+          // Wait for dom to update before setting focus
+          (firstFocusableElement as HTMLElement)?.focus();
+        }
+      }, 200);
       return () => document.removeEventListener("keydown", handleTabKey);
     }
   }, [openState]);
 
   const modalTitle = "Content";
 
-  return (
-    <div className="container-fluid">
-      <button
-        type="button"
-        // data-bs-toggle={isBootstrap && "modal"}
-        // data-bs-target={isBootstrap && "#uds-modal"}
-        onClick={isReact ? handleOpen : undefined}
-        id="openModalButton"
-        className="btn btn-dark"
-      >
-        Show modal
-      </button>
-
-      {(openState || isBootstrap) && (
+  if (isBootstrap) {
+    return (
+      <div className="container-fluid">
+        {/* Disable main content on modal open */}
+        <div id="main-content">
+          <button
+            autoFocus
+            inert={isBootstrap ? undefined : openState ?? false}
+            type="button"
+            // data-bs-toggle={isBootstrap && "modal"}
+            // data-bs-target={isBootstrap && "#uds-modal"}
+            onClick={isReact ? handleOpen : undefined}
+            id="openModalButton"
+            className="btn btn-dark"
+          >
+            Show modal
+          </button>
+        </div>
         <div
           id="uds-modal"
           role="dialog"
@@ -125,9 +140,10 @@ export const Modal: React.FC<ModalProps> = ({ open, gaData }) => {
           <div className="uds-modal-container">
             <GaEventWrapper gaData={{ ...defaultGaData, ...gaData }}>
               <ButtonIconOnly
+                autoFocus
                 // @ts-ignore
                 id="closeModalButton"
-                onClick={isReact ? handleClose : undefined}
+                // onClick={isReact ? handleClose : undefined}
                 // data-bs-dismiss={isBootstrap && "modal"}
                 className="uds-modal-close-btn"
                 icon={["fas", "times"]}
@@ -144,7 +160,60 @@ export const Modal: React.FC<ModalProps> = ({ open, gaData }) => {
             </button>
           </div>
         </div>
-      )}
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return (
+      <div className="container-fluid">
+        {/* Disable main content on modal open */}
+        <div id="main-content-r" inert={openState ?? undefined}>
+          <button
+            autoFocus
+            inert={openState ?? undefined}
+            type="button"
+            // data-bs-toggle={isBootstrap && "modal"}
+            // data-bs-target={isBootstrap && "#uds-modal"}
+            onClick={isReact ? handleOpen : undefined}
+            id="openModalButtonR"
+            className="btn btn-dark"
+          >
+            Show modal
+          </button>
+        </div>
+
+        {openState && (
+          <div
+            id="uds-modal"
+            role="dialog"
+            aria-modal="true"
+            aria-label={modalTitle}
+            className={classNames("uds-modal", { open: openState })}
+          >
+            <div className="uds-modal-container">
+              <GaEventWrapper gaData={{ ...defaultGaData, ...gaData }}>
+                <ButtonIconOnly
+                  autoFocus
+                  // @ts-ignore
+                  id="closeModalButtonR"
+                  onClick={handleClose}
+                  // data-bs-dismiss={isBootstrap && "modal"}
+                  className="uds-modal-close-btn"
+                  icon={["fas", "times"]}
+                />
+              </GaEventWrapper>
+              <h1>{modalTitle}</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod incididuntåç ut labore et dolore magna aliqua eiusmod
+                tempo.
+              </p>
+              <button type="button" className="btn btn-primary">
+                button
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 };
